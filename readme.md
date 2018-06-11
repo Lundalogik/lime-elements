df1a98aa108b86e984162c34823d2f513ea5f3e6
Adrian Schmidt <adrian.schmidt@lime.tech>
2018-06-11 13:52:20 +0200

    Adds Open Sans font for development

--- readme.md
+++ readme.md
@@ -3,3 +3,9 @@
 This package will include basic components like buttons, loading-spinners, etcetera.
 
 To start the dev-server, run `SASS_PATH=node_modules npm start`.
+
+To build the dist folder, run `SASS_PATH=node_modules npm run build`.
+
+## Requirements
+
+The "Open Sans" font is included for development purposes, but is not included in the published package. This font should be supplied by the consuming application. If not supplied, texts will fall back to suitable alternatives.
--- stencil.config.js
+++ stencil.config.js
@@ -2,25 +2,26 @@ const less = require('@stencil/less');
 const sass = require('@stencil/sass');
 
 exports.config = {
-  namespace: 'lime-elements',
-  outputTargets:[
-    {
-      type: 'dist'
-    },
-    {
-      type: 'www',
-      serviceWorker: false
-    }
-  ],
-  plugins: [
-    less({
-      injectGlobalPaths: ['node_modules/lime-less-variables/src/lime-less-variables.less']
-    }),
-    sass()
-  ]
+    copy: [{ src: 'dev-assets' }],
+    namespace: 'lime-elements',
+    outputTargets:[
+        {
+            type: 'dist'
+        },
+        {
+            type: 'www',
+            serviceWorker: false
+        }
+    ],
+    plugins: [
+        less({
+            injectGlobalPaths: ['node_modules/lime-less-variables/src/lime-less-variables.less']
+        }),
+        sass()
+    ]
 };
 
 exports.devServer = {
-  root: 'www',
-  watchGlob: '**/**'
+    root: 'www',
+    watchGlob: '**/**'
 }
