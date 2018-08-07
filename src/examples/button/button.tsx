9836c8f9ef8ec65b2fcc36905e75b0250e3f4109
Adrian Schmidt <adrian.schmidt@lime.tech>
2018-08-07 14:27:54 +0200

    first step

--- src/examples/button/button.tsx
+++ src/examples/button/button.tsx
@@ -1,17 +1,41 @@
-import { Component } from '@stencil/core';
+import { Component, State } from '@stencil/core';
+import { default as i18n } from 'roddeh-i18n';
 
 @Component({
     shadow: true,
     tag: 'limel-example-button',
 })
 export class ButtonExample {
+    @State()
+    private timesClicked = 0;
+
+    public componentWillLoad() {
+        i18n.translator.add({
+            "values":{
+                "%n comments":[
+                    [0, 0, "You haven't clicked at all yet!"],
+                    [1, 1, "%n click"],
+                    [2, null, "%n clicks"]
+                ]
+            }
+        });
+    }
+
     public render() {
         return (
             <limel-button-group reverse-order={true}>
-                <limel-button label="Save" primary={true} />
+                <limel-button
+                    label={i18n("%n comments", this.timesClicked)}
+                    primary={true}
+                    onClick={() => {this.clickHandler();}}
+                />
                 <limel-button label="Cancel" />
                 <limel-button label="disabled" disabled={true} />
             </limel-button-group>
         );
+    }
+
+    private clickHandler() {
+        this.timesClicked += 1;
     }
 }
