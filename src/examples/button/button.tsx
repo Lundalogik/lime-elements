668765a35e7a1be91a2c866d3a834fe47428380d
Katja Bregenzer <katja.bregenzer@lime.tech>
2018-08-08 11:39:00 +0200

    feat(limel-example-button): use language service to translate labels
    crm-feature #471

--- src/examples/button/button.tsx
+++ src/examples/button/button.tsx
@@ -1,15 +1,30 @@
 import { Component } from '@stencil/core';
+import { LanguageService } from '../../components/language-selector/language.service';
 
 @Component({
     shadow: true,
     tag: 'limel-example-button',
 })
 export class ButtonExample {
+    private labelSave: string;
+    private labelCancel: string;
+    private languageService: LanguageService;
+
+    public componentWillLoad() {
+        this.languageService = new LanguageService();
+        this.labelSave = this.languageService.getTranslation(
+            'limel.button.save'
+        );
+        this.labelCancel = this.languageService.getTranslation(
+            'limel.button.cancel'
+        );
+    }
+
     public render() {
         return (
             <limel-button-group reverse-order={true}>
-                <limel-button label="Save" primary={true} />
-                <limel-button label="Cancel" />
+                <limel-button label={this.labelSave} primary={true} />
+                <limel-button label={this.labelCancel} />
                 <limel-button label="disabled" disabled={true} />
             </limel-button-group>
         );
