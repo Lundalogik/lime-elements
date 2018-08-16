ca12cf322db80b1d47695fdbabf3e3dd2d4cee87
Johan Groth <johan.groth@lime.tech>
2018-08-16 10:22:45 +0200

    feat(limel-list): add list component

---
+++ src/examples/list/list.tsx
@@ -0,0 +1,21 @@
+import { Component } from '@stencil/core';
+import { ListItem, ListSeparator } from '../../components/list/list-item';
+
+@Component({
+    tag: 'limel-example-list',
+    shadow: true,
+})
+export class ListExample {
+    private items: Array<ListItem | ListSeparator> = [
+        { text: 'King of Tokyo', id: 1 },
+        { text: 'Smash Up!', id: 2 },
+        { text: 'Pandemic', id: 3 },
+        { separator: true },
+        { text: 'Catan', id: 4 },
+        { text: 'Ticket ot Ride', id: 5 },
+    ];
+
+    public render() {
+        return <limel-list items={this.items} />;
+    }
+}
