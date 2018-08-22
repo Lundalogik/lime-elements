89016717b5cc69afb84c98eaabeb3f6adf5907c8
Adrian Schmidt <adrian.schmidt@lime.tech>
2018-08-22 09:38:28 +0200

    feat(stenciljs): update stenciljs to v0.11.4
    See changelog for StencilJS at https://github.com/ionic-team/stencil/blob/master/CHANGELOG.md#-0114-2018-08-12

--- src/examples/props.tsx
+++ src/examples/props.tsx
@@ -1,4 +1,5 @@
 import { Component, Prop, State } from '@stencil/core';
+import * as showdown from 'showdown';
 
 @Component({
     tag: 'limel-props',
@@ -9,84 +10,25 @@ export class Props {
     public name: string;
 
     @State()
-    private props = [];
+    private propsTableHtml = '';
 
-    @State()
-    private events = [];
+    private converter = new showdown.default.Converter({
+        tables: true,
+        simpleLineBreaks: true,
+        underline: true,
+    });
 
-    public componentDidLoad() {
+    public componentWillLoad() {
         const type = this.name.replace('limel-', '');
         const url = `/stencil/components/${type}/readme.md`;
 
         this.fetchData(url).then(data => {
-            const lines = data.split('\n').filter(row => !!row.trim());
-            const { props, events } = this.parseLines(lines);
-
-            this.props = props;
-            this.events = events;
+            this.propsTableHtml = this.converter.makeHtml(data);
         });
     }
 
     public render() {
-        return [this.renderPropsTable(), this.renderEventsTable()];
-    }
-
-    private renderPropsTable() {
-        if (!this.props.length) {
-            return;
-        }
-
-        return [
-            <h2>Properties</h2>,
-            <table>
-                <thead>
-                    <tr>
-                        <th>Property</th>
-                        <th>Type</th>
-                        <th>Description</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.props.map(prop => {
-                        return (
-                            <tr>
-                                <td>{prop.name}</td>
-                                <td>{prop.type}</td>
-                                <td>{prop.description}</td>
-                            </tr>
-                        );
-                    })}
-                </tbody>
-            </table>,
-        ];
-    }
-
-    private renderEventsTable() {
-        if (!this.events.length) {
-            return;
-        }
-
-        return [
-            <h2>Events</h2>,
-            <table>
-                <thead>
-                    <tr>
-                        <th>Event</th>
-                        <th>Description</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.events.map(event => {
-                        return (
-                            <tr>
-                                <td>{event.name}</td>
-                                <td>{event.description}</td>
-                            </tr>
-                        );
-                    })}
-                </tbody>
-            </table>,
-        ];
+        return <div innerHTML={this.propsTableHtml} />;
     }
 
     private fetchData(url) {
@@ -95,82 +37,18 @@ export class Props {
                 .getReader()
                 .read()
                 .then(({ value }) => {
-                    return new TextDecoder('utf-8').decode(value);
+                    const text = new TextDecoder('utf-8').decode(value);
+                    return this.stripBuiltWithStencilLine(text);
                 });
         });
     }
 
-    private parseLines(lines) {
-        const props = [];
-        const events = [];
-        let i = 0;
-        let line;
-        let mode = null;
-        let currentObject;
+    private stripBuiltWithStencilLine(data) {
+        return data.replace(
+            `----------------------------------------------
 
-        do {
-            line = lines[i++];
-
-            if (!line || line.includes('---------')) {
-                break;
-            }
-
-            if (line.includes('## Properties')) {
-                mode = 'props';
-                continue;
-            }
-
-            if (line.includes('## Attributes')) {
-                mode = 'attr';
-                continue;
-            }
-
-            if (line.includes('## Events')) {
-                mode = 'events';
-                continue;
-            }
-
-            currentObject = this.editObject(
-                line,
-                currentObject,
-                props,
-                events,
-                mode
-            );
-        } while (true); // tslint:disable-line
-
-        return { props: props, events: events };
-    }
-
-    private editObject(line, currentObject, props, events, mode) {
-        if (line.includes('##')) {
-            currentObject = {
-                name: line.replace('####', '').trim(),
-            };
-
-            if (mode === 'props') {
-                props.push(currentObject);
-            } else if (mode === 'events') {
-                events.push(currentObject);
-            }
-            return currentObject;
-        }
-
-        if (currentObject) {
-            if (mode === 'props') {
-                if (!('type' in currentObject)) {
-                    currentObject.type = line;
-                    return currentObject;
-                }
-
-                currentObject.description = line;
-                return currentObject;
-            }
-
-            if (mode === 'events') {
-                currentObject.description = line;
-                return currentObject;
-            }
-        }
+*Built with [StencilJS](https://stenciljs.com/)*`,
+            ''
+        );
     }
 }
