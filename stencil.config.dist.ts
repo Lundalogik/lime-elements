af2be4b6d47826981922804de1a1e0c21830726c
Adrian Schmidt <adrian.schmidt@lime.tech>
2018-09-20 09:44:24 +0200

    build(stencil.config.ts): make the dev-config the default one
    The build crashes on Windows when specifying a custom config, so we make the config for the
    dev-build the default one, and the config for the dist-build the custom one. `npm start` will now
    work on Windows, while developers on Windows will need to use the Docker container if they wish to
    run the dist-build (`npm run build`).

---
+++ stencil.config.dist.ts
@@ -0,0 +1,16 @@
+import { Config } from '@stencil/core';
+import { OutputTargetDist } from '@stencil/core/dist/declarations/output-targets';
+import { sass } from '@stencil/sass';
+
+const targetDist: OutputTargetDist = {
+    type: 'dist',
+};
+
+export const config: Config = {
+    namespace: 'lime-elements',
+    outputTargets: [ targetDist ],
+    plugins: [
+        sass(),
+    ],
+    excludeSrc: ['/test/', '**/.spec.', '**/examples/**', '**/dev-assets/**'],
+};
