5b1e06117c0ba5f7a8eacc684355abdfc73995da
Johan Groth <johan.groth@lime.tech>
2018-09-24 15:19:17 +0200

    feat(snackbar): new snackbar component

---
+++ src/components/snackbar/snackbar.tsx
@@ -0,0 +1,101 @@
+import { Component, Prop, Element, Event, EventEmitter, Method } from '@stencil/core';
+import { MDCSnackbar } from '@lime-material/snackbar';
+
+@Component({
+    tag: 'limel-snackbar',
+    shadow: true,
+    styleUrl: 'snackbar.scss',
+})
+export class Snackbar {
+    /**
+     * The text message to display.
+     */
+    @Prop()
+    public message: string;
+
+    /**
+     * The amount of time in milliseconds to show the snackbar.
+     */
+    @Prop()
+    public timeout: number;
+
+    /**
+     * The text to display for the action button.
+     */
+    @Prop()
+    public actionText: string;
+
+    /**
+     * Whether to show the snackbar with space for multiple lines of text
+     */
+    @Prop()
+    public multiline: boolean;
+
+    @Element()
+    private host: HTMLElement;
+
+    /**
+     * Emitted when the action button is pressed
+     */
+    @Event()
+    private action: EventEmitter;
+
+    /**
+     * Emitted when the snackbar hides itself
+     */
+    @Event()
+    private hide: EventEmitter;
+
+    private mdcSnackbar: MDCSnackbar;
+
+    public componentDidLoad() {
+        this.mdcSnackbar = new MDCSnackbar(this.host.shadowRoot.querySelector('.mdc-snackbar'));
+
+        this.mdcSnackbar.listen('MDCSnackbar:hide', () => {
+            this.hide.emit();
+        });
+    }
+
+    public componentDidUnload() {
+        this.mdcSnackbar.destroy();
+    }
+
+    /**
+     * Show the snackbar
+     */
+    @Method()
+    public async show() {
+        const config = {
+            message: this.message,
+            multiline: !!this.multiline,
+            actionOnBottom: true
+        };
+
+        if (this.actionText) {
+            config['actionText'] = this.actionText;
+            config['actionHandler'] = () => {
+                this.action.emit();
+            };
+        }
+
+        if (this.timeout) {
+            config['timeout'] = this.timeout;
+        }
+
+        this.mdcSnackbar.show(config);
+    }
+
+    public render() {
+        return (
+            <div class="mdc-snackbar"
+                aria-live="assertive"
+                aria-atomic="true"
+                aria-hidden="true">
+                <div class="mdc-snackbar__text"></div>
+                <div class="mdc-snackbar__action-wrapper">
+                    <button type="button" class="mdc-snackbar__action-button"></button>
+                </div>
+            </div>
+        );
+    }
+}
