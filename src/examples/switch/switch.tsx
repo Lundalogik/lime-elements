00131cd6773b459bd54bf73c9ad952b37cfe240e
Adrian Schmidt <adrian.schmidt@lime.tech>
2018-09-27 11:52:08 +0200

    docs(limel-switch): improve example

--- src/examples/switch/switch.tsx
+++ src/examples/switch/switch.tsx
@@ -1,4 +1,4 @@
-import { Component } from '@stencil/core';
+import { Component, State } from '@stencil/core';
 
 @Component({
     shadow: true,
@@ -6,38 +6,69 @@ import { Component } from '@stencil/core';
     tag: 'limel-example-switch',
 })
 export class SwitchExample {
+    @State()
+    private valueOne = false;
+    @State()
+    private valueTwo = true;
+    @State()
+    private valueThree = false;
+    @State()
+    private valueFour = true;
+    @State()
+    private toggleExampleValue = false;
+
     public render() {
         return [
-            <limel-switch
-                label="False - Enabled"
-                value={false}
-                onChange={event => {
-                    console.log(event);
-                }}
-            />,
-            <limel-switch
-                label="True - Enabled"
-                value={true}
-                onChange={event => {
-                    console.log(event);
-                }}
-            />,
-            <limel-switch
-                label="False - Disabled"
-                value={false}
-                disabled={true}
-                onChange={event => {
-                    console.log(event);
-                }}
-            />,
-            <limel-switch
-                label="True - Disabled"
-                value={true}
-                disabled={true}
-                onChange={event => {
-                    console.log(event);
-                }}
-            />,
+            <section>
+                <h3>Basic usage</h3>
+                <limel-switch
+                    label={`${this.valueOne} - Enabled`}
+                    value={this.valueOne}
+                    onChange={event => {
+                        this.valueOne = event.detail;
+                    }}
+                />
+                <limel-switch
+                    label={`${this.valueTwo} - Enabled`}
+                    value={this.valueTwo}
+                    onChange={event => {
+                        this.valueTwo = event.detail;
+                    }}
+                />
+                <limel-switch
+                    label={`${this.valueThree} - Disabled`}
+                    value={this.valueThree}
+                    disabled={true}
+                    onChange={event => {
+                        this.valueThree = event.detail;
+                    }}
+                />
+                <limel-switch
+                    label={`${this.valueFour} - Disabled`}
+                    value={this.valueFour}
+                    disabled={true}
+                    onChange={event => {
+                        this.valueFour = event.detail;
+                    }}
+                />
+            </section>,
+            <section>
+                <h3>Updating the value from outside the component</h3>
+                <limel-button
+                    label="Toggle"
+                    primary={true}
+                    onClick={() => {
+                        this.toggleExampleValue = !this.toggleExampleValue;
+                    }}
+                />
+                <limel-switch
+                    label={`Current value: ${this.toggleExampleValue.toString()}`}
+                    value={this.toggleExampleValue}
+                    onChange={event => {
+                        this.toggleExampleValue = event.detail;
+                    }}
+                />
+            </section>,
         ];
     }
 }
