89899007977d2b2d21b36dac8937bd9ab69a8291
Adrian Schmidt <adrian.schmidt@lime.tech>
2018-10-01 14:44:00 +0200

    docs(limel-example): add support for showing scss source in example

--- src/examples/example.tsx
+++ src/examples/example.tsx
@@ -1,6 +1,7 @@
 import { Component, Element, Prop, State } from '@stencil/core';
 import prism from 'prismjs';
 import 'prismjs/components/prism-jsx.js'; // tslint:disable-line:no-submodule-imports
+import 'prismjs/components/prism-scss.js'; // tslint:disable-line:no-submodule-imports
 import 'prismjs/components/prism-tsx.js'; // tslint:disable-line:no-submodule-imports
 
 @Component({
@@ -15,16 +16,30 @@ export class Example {
     private root: HTMLElement;
 
     @State()
-    private code: string;
+    private tsxCode: string;
+
+    @State()
+    private scssCode: string;
 
     public componentWillLoad() {
         const type = this.name.replace('limel-example-', '');
-        const url = `/stencil/examples/${type}/${type}.tsx`;
+        const tsxUrl = `/stencil/examples/${type}/${type}.tsx`;
+        const scssUrl = `/stencil/examples/${type}/${type}.scss`;
 
-        this.fetchData(url).then(data => {
-            this.code = prism.highlight(data, prism.languages.tsx);
-            const element = this.root.querySelector('.code code');
-            element.innerHTML = this.code;
+        this.fetchData(tsxUrl).then(data => {
+            this.tsxCode = prism.highlight(data, prism.languages.tsx);
+            const element = this.root.querySelector('.code.language-tsx code');
+            element.innerHTML = this.tsxCode;
+        });
+
+        this.fetchData(scssUrl).then(data => {
+            if (data !== 'Not Found') {
+                this.scssCode = prism.highlight(data, prism.languages.scss);
+                const element = this.root.querySelector(
+                    '.code.language-scss code'
+                );
+                element.innerHTML = this.scssCode;
+            }
         });
     }
 
@@ -35,8 +50,19 @@ export class Example {
             <div class="example">
                 <ExampleComponent />
             </div>,
-            <div class="code">
-                <pre class="react-prism react-prism language-jsx">
+            <div class="code language-tsx">
+                <pre class="react-prism react-prism language-tsx">
+                    <code />
+                </pre>
+            </div>,
+            <div
+                class={`
+                    code
+                    language-scss
+                    ${this.scssCode ? '' : 'hidden'}
+                `}
+            >
+                <pre class="react-prism react-prism language-scss">
                     <code />
                 </pre>
             </div>,
