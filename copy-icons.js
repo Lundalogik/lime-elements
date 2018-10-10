Lundalogik/lime-elements

commit 011bae03f711fa5f0ef4f597fa59944b876aa2af

build(docz): make it so the documentation can run in docker

copy-icons.js
@@ -44,7 +44,7 @@ function copyFiles(path) {
 const env = process.env.NODE_ENV || 'dev';
 const paths = {
     prod: 'dist/collection',
-    dev: '.docz/public/stencil',
+    dev: '.docz/dist/stencil',
 };
 const basePath = paths[env];
 

Dockerfile.docs
@@ -1,0 +1,8 @@
+FROM node:8-alpine
+WORKDIR /lime
+COPY .npmrc ./
+COPY package*.json ./
+RUN npm install
+COPY . .
+RUN npm run docz:build
+CMD npm run docz:serve

Makefile
@@ -1,4 +1,10 @@
 DOCKER_IMAGE = lime-elements
+DOCKER_DOCS_IMAGE = lime-elements-docs
+
+.PHONY: build-docs
+build-docs:
+	@# Builds the docs image for lime-elements.
+	docker build --file Dockerfile.docs --pull -t $(DOCKER_DOCS_IMAGE) .
 
 .PHONY: build
 build:

src/index.html
@@ -1,26 +1,27 @@
 <!DOCTYPE html>
-<html dir="ltr" lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="{{ description }}">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ title }}</title>
-    <base href="/stencil/">
-    <link rel="stylesheet" type="text/css" href="https://unpkg.com/codemirror@5.39.2/lib/codemirror.css" />
-    <link href="/stencil/dev-assets/fonts.css" rel="stylesheet" />
-    <script src="/stencil/build/lime-elements.js"></script>
+<html dir="ltr" lang="{{ lang }}">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="{{ description }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <base href="/stencil/">
+    <title>{{ title }}</title>
+    {{ head }}
+</head>
+
+<body>
     <style>
         html {
             font-size: 62.5%;
         }
     </style>
-</head>
-<body>
+    <link rel="stylesheet" type="text/css" href="https://unpkg.com/codemirror@5.39.2/lib/codemirror.css" />
+    <link href="/stencil/dev-assets/fonts.css" rel="stylesheet" />
+    <script src="/stencil/build/lime-elements.js"></script>
     <div id="root" />
-    <script src="/static/js/runtime~app.js"></script>
-    <script src="/static/js/vendors.js"></script>
-    <script src="/static/js/app.js"></script>
+    {{ footer }}
 </body>
+
 </html>

stencil.config.docs.ts
@@ -15,6 +15,7 @@ export const config: Config = {
         { src: 'examples/**/*.scss' },
         { src: 'components/**/*.md' },
     ],
+    hashFileNames: false,
     namespace: 'lime-elements',
     outputTargets: [targetWww],
     plugins: [

commit 26685f9b0f5a14a762953589aded1193b936367e

build(docz): fix docz when running `npm start`
issue introduced in commit 011bae03f711fa5f0ef4f597fa59944b876aa2af

.dockerignore
@@ -1,3 +1,6 @@
 Dockerfile
 Makefile
 node_modules
+.docz/
+dist/
+.stencil/

copy-icons.js
@@ -44,7 +44,8 @@ function copyFiles(path) {
 const env = process.env.NODE_ENV || 'dev';
 const paths = {
     prod: 'dist/collection',
-    dev: '.docz/dist/stencil',
+    dev: '.docz/public/stencil',
+    docz: '.docz/dist/stencil',
 };
 const basePath = paths[env];
 

stencil.config.ts
@@ -5,7 +5,7 @@ import { sass } from '@stencil/sass';
 const targetWww: OutputTargetWww = {
     type: 'www',
     serviceWorker: null,
-    dir: '.docz/dist/stencil',
+    dir: '.docz/public/stencil',
 };
 
 export const config: Config = {