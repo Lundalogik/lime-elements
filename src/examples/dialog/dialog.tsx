e5a2bfb05a051d9d4ec7c5dc2ea85318ad99d6b2
Adrian Schmidt <adrian.schmidt@lime.tech>
2018-10-15 10:33:16 +0200

    docs(limel-dialog): update docs to show different types of dialogs

--- src/examples/dialog/dialog.tsx
+++ src/examples/dialog/dialog.tsx
@@ -1,92 +1,118 @@
 import { Component, State } from '@stencil/core';
 
+const MIN_NAME_LENGTH = 5;
+const MIN_AGE = 20;
+const MAX_AGE = 50;
+
 @Component({
-    shadow: true,
     tag: 'limel-example-dialog',
+    shadow: true,
+    styleUrl: 'dialog.scss',
 })
 export class DialogExample {
     @State()
-    public dialogOpen = false;
+    private basicDialogOpen = false;
     @State()
-    public isValid = false;
+    private headingDialogOpen = false;
 
     @State()
-    public age: number;
+    private name = '';
+
     @State()
-    public name = '';
+    private age: string;
 
     public render() {
         return [
-            <limel-button
-                primary={true}
-                label="Open"
-                onClick={() => {
-                    this.dialogOpen = true;
-                }}
-            />,
-
-            <limel-dialog
-                open={this.dialogOpen}
-                onClose={() => {
-                    this.dialogOpen = false;
-                }}
-            >
-                <form>
-                    <label>Name: </label>
-                    <input
-                        tabindex="0"
-                        type="text"
-                        value={this.name}
-                        onInput={this.handleChangeName}
-                    />
-                    <br />
-                    <label>Age: </label>
-                    <input
-                        type="text"
-                        value={this.age}
-                        onInput={this.handleChangeColor}
-                    />
-                </form>
-                <limel-button-group reverse-order={true} slot="button">
-                    <limel-button
-                        primary={true}
-                        label="Save"
-                        disabled={!this.isValid}
-                        onClick={this.submitForm}
-                    />
-                    <limel-button label="Cancel" onClick={this.closeDialog} />
-                </limel-button-group>
-            </limel-dialog>,
+            <section>
+                <h3>Basic Usage</h3>
+                <limel-button
+                    primary={true}
+                    label="Open"
+                    onClick={() => {
+                        this.basicDialogOpen = true;
+                    }}
+                />
+                <limel-dialog
+                    open={this.basicDialogOpen}
+                    onClose={() => {
+                        this.basicDialogOpen = false;
+                    }}
+                >
+                    <p>This is a simple alert-dialog.</p>
+                    <limel-button-group slot="button">
+                        <limel-button
+                            label="Ok"
+                            onClick={() => {
+                                this.basicDialogOpen = false;
+                            }}
+                        />
+                    </limel-button-group>
+                </limel-dialog>
+            </section>,
+            <section>
+                <h3>With Heading</h3>
+                <limel-button
+                    primary={true}
+                    label="Open"
+                    onClick={() => {
+                        this.headingDialogOpen = true;
+                    }}
+                />
+                <limel-dialog
+                    heading="Registration"
+                    open={this.headingDialogOpen}
+                    onClose={() => {
+                        this.headingDialogOpen = false;
+                    }}
+                >
+                    <form>
+                        <p>
+                            <limel-text-field
+                                label="Name"
+                                value={this.name}
+                                required={true}
+                                invalid={!this.nameValid()}
+                                onChange={event => {
+                                    this.name = event.detail;
+                                }}
+                            />
+                        </p>
+                        <p>
+                            <limel-text-field
+                                label="Age"
+                                value={this.age}
+                                required={true}
+                                invalid={!this.ageValid()}
+                                onChange={event => {
+                                    this.age = event.detail;
+                                }}
+                            />
+                        </p>
+                    </form>
+                    <limel-button-group class="reverse-order" slot="button">
+                        <limel-button
+                            primary={true}
+                            label="Save"
+                            disabled={!this.nameValid() || !this.ageValid()}
+                            onClick={this.submitForm}
+                        />
+                        <limel-button
+                            label="Cancel"
+                            onClick={this.closeDialog}
+                        />
+                    </limel-button-group>
+                </limel-dialog>
+            </section>,
         ];
     }
 
-    /**
-     *
-     */
-    public handleChangeName = event => {
-        this.name = event.target.value;
-        this.validate();
-    };
+    private nameValid() {
+        return this.name.length >= MIN_NAME_LENGTH;
+    }
 
-    /**
-     *
-     */
-    public handleChangeColor = event => {
-        this.age = event.target.value;
-        this.validate();
-    };
-
-    /**
-     *
-     */
-    public validate = () => {
-        const MIN_NAME_LENGTH = 5;
-        const MIN_AGE = 20;
-        const MAX_AGE = 50;
-        this.isValid =
-            this.name.length >= MIN_NAME_LENGTH &&
-            (this.age > MIN_AGE && this.age < MAX_AGE);
-    };
+    private ageValid() {
+        return +this.age > MIN_AGE && +this.age < MAX_AGE;
+    }
 
     /**
      *
@@ -100,6 +126,6 @@ export class DialogExample {
      *
      */
     public closeDialog = () => {
-        this.dialogOpen = false;
+        this.headingDialogOpen = false;
     };
 }
