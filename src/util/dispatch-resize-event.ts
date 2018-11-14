1d508275f9899c7394cb4728b8e0c1eb819cf27f
Katja Bregenzer <katja.bregenzer@lime.tech>
2018-11-14 14:39:59 +0100

    refactor(limel-dialog): move dispatchResizeEvent to util
    #192

---
+++ src/util/dispatch-resize-event.ts
@@ -0,0 +1,11 @@
+export const dispatchResizeEvent = () => {
+    /*
+     * The shorthand (`window.dispatchEvent(new Event('resize'));`)
+     * causes compiler errors, so we go the long way around.
+     * See https://stackoverflow.com/a/1818513/280972
+     * /Ads
+     */
+    const resizeEvent = window.document.createEvent('UIEvents');
+    resizeEvent.initUIEvent('resize', true, false, window, 0);
+    window.dispatchEvent(resizeEvent);
+};
