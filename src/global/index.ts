fa400ce52d1bea8285648a11f941e53a37e20d73
Hanna Hultgren <hanna.hultgren@lundalogik.se>
2018-12-17 00:50:27 +0100

    feat(translations): adds translations support

--- src/global/index.ts
+++ src/global/index.ts
@@ -1,7 +1,9 @@
 import config from './config';
 import iconCache from './icon-cache';
+import translations from './translations';
 
 declare var Context: any;
 
 Context.config = config;
 Context.iconCache = iconCache;
+Context.translations = translations;
---
+++ src/translations/no.ts
@@ -0,0 +1,5 @@
+export default {
+    'date-picker.month.heading': 'Måned',
+    'date-picker.quarter.heading': 'Kvartal',
+    'date-picker.year.heading': 'År',
+};
