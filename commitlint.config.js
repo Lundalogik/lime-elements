c086c07d0d79a11416beb4fddbf1f6f195aabc2c
Adrian Schmidt <adrian.schmidt@lime.tech>
2019-02-12 11:35:05 +0100

    chore(commitlint): update config to fail if type is not correct

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,5 +3,22 @@ module.exports = {
     rules: {
         'header-max-length': [2, 'always', 100],
         'scope-case': [0, 'always', 'lower-case'],
+        'type-enum': [
+            2,
+            'always',
+            [
+                'feat',
+                'fix',
+                'docs',
+                'style',
+                'refactor',
+                'perf',
+                'test',
+                'build',
+                'ci',
+                'chore',
+                'revert',
+            ],
+        ],
     },
 };
