6f43c979187c605029706b97fef0354633c837e8
Adrian Schmidt <adrian.schmidt@lime.tech>
2019-02-20 11:54:48 +0100

    fix(limel-date-picker): enable moving caret with keyboard
    fix #306

--- postinstall.js
+++ postinstall.js
@@ -1,19 +1,5 @@
 const fs = require('fs');
 
-// Copy pre-built version of flatpickr
-// TODO: remove this when a new version of flatpickr has been released
-fs.exists('src/dev-assets/hack/flatpickr.js', () => {
-    fs.copyFile(
-        'src/dev-assets/hack/flatpickr.js',
-        'node_modules/flatpickr/dist/flatpickr.js',
-        () => {
-            console.log(
-                'copied src/dev-assets/hack/flatpickr.js to node_modules'
-            );
-        }
-    );
-});
-
 // Remove type definitions for jsx-dom
 fs.exists('node_modules/jsx-dom/jsx-dom.d.ts', () => {
     fs.unlink('node_modules/jsx-dom/jsx-dom.d.ts', () => {
--- src/examples/date-picker/date-picker-formatted.tsx
+++ src/examples/date-picker/date-picker-formatted.tsx
@@ -6,29 +6,40 @@ import { Component, State } from '@stencil/core';
 })
 export class DatePickerFormattedExample {
     @State()
-    private value = new Date('2018-11-04');
-
-    public handleChange(event) {
-        this.value = event.detail;
-    }
+    private valueNo = new Date('2018-11-04');
+    private valueFi = new Date('2018-11-04');
 
     public render() {
         return [
             <limel-date-picker
                 language="no"
                 type="datetime"
-                label="localized Date"
-                value={this.value}
-                onChange={this.handleChange}
+                label="Localized date"
+                value={this.valueNo}
+                onChange={this.handleChangeNo}
             />,
+            <p style={{ 'font-size': 'small' }}>
+                Value: <code>{this.valueNo.toString()}</code>
+            </p>,
             <limel-date-picker
                 language="fi"
                 format="YYYY-MM-DD"
                 type="datetime"
                 label="Date with custom format"
-                value={this.value}
-                onChange={this.handleChange}
+                value={this.valueFi}
+                onChange={this.handleChangeFi}
             />,
+            <p style={{ 'font-size': 'small' }}>
+                Value: <code>{this.valueFi.toString()}</code>
+            </p>,
         ];
+    }
+
+    private handleChangeNo(event) {
+        this.valueNo = event.detail;
+    }
+
+    private handleChangeFi(event) {
+        this.valueFi = event.detail;
     }
 }
