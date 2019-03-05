ee08a044e3dd29cbe84beef84fe484826716cb42
Adrian Schmidt <adrian.schmidt@lime.tech>
2019-03-05 14:19:03 +0100

    build(postinstall.js): remove postinstall script that is no longer needed

--- postinstall.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-// Remove type definitions for jsx-dom
-fs.exists('node_modules/jsx-dom/jsx-dom.d.ts', () => {
-    fs.unlink('node_modules/jsx-dom/jsx-dom.d.ts', () => {
-        console.log('jsx-dom.d.ts removed!');
-    });
-});
