14f8e37ce44d521aa193d8d1508df298232c272d
Johan Groth <johan.groth@lime.tech>
2019-03-06 14:05:39 +0100

    docs(button): improved documentation and examples

--- src/examples/button/button.tsx
+++ src/examples/button/button.tsx
@@ -1,117 +1,75 @@
-import { Component, Element } from '@stencil/core';
+import { Component, State } from '@stencil/core';
 
 @Component({
     tag: 'limel-example-button',
     shadow: true,
+    styleUrl: 'button.scss',
 })
 export class ButtonExample {
-    @Element()
-    private hostElement: HTMLElement;
-
-    private buttonWithClickHandler;
-
-    public componentDidLoad() {
-        this.buttonWithClickHandler = this.hostElement.shadowRoot.querySelector(
-            '#buttonWithClickHandler'
-        );
-    }
+    @State()
+    private props = {
+        label: 'My button',
+        primary: true,
+        outlined: false,
+        icon: null,
+        disabled: false,
+        loading: false,
+    };
 
     public render() {
-        return [
-            <section>
-                <h3>Default</h3>
-                <p>
-                    <limel-button label="Primary" primary={true} />
-                </p>
-                <p>
-                    <limel-button label="Secondary" />
-                </p>
-            </section>,
-            <section>
-                <h3>Disabled</h3>
-                <p>
-                    <limel-button
-                        label="Primary"
-                        primary={true}
-                        disabled={true}
-                    />
-                </p>
-                <p>
-                    <limel-button label="Secondary" disabled={true} />
-                </p>
-            </section>,
-            <section>
-                <h3>Loading</h3>
-                <p>
-                    <limel-button
-                        label="Primary"
-                        primary={true}
-                        loading={true}
-                    />
-                </p>
-                <p>
-                    <limel-button label="Secondary" loading={true} />
-                </p>
-            </section>,
-            <section>
-                <h3>Disabled &amp; loading</h3>
-                <p>
-                    <limel-button
-                        label="Primary"
-                        primary={true}
-                        disabled={true}
-                        loading={true}
-                    />
-                </p>
-                <p>
-                    <limel-button
-                        label="Secondary"
-                        disabled={true}
-                        loading={true}
-                    />
-                </p>
-            </section>,
-            <section>
-                <h3>With click handler</h3>
-                <p>
-                    The click handler in this example sets the attributes{' '}
-                    <code>loading</code> and <code>disabled</code> to{' '}
-                    <code>true</code>. After 1 second, the <code>loading</code>{' '}
-                    attribute is set to <code>false</code> again. After another
-                    4 seconds, the button is once again enabled.
-                </p>
-                <p>
-                    When the <code>loading</code> attribute changes from{' '}
-                    <code>true</code> to <code>false</code>, the button
-                    automatically displays a checkmark icon for 2 seconds. Note
-                    that our click handler isn't actually involved in this.
-                </p>
-                <p>
-                    <limel-button
-                        id="buttonWithClickHandler"
-                        label="Click me!"
-                        primary={true}
-                        onClick={() => {
-                            this.onClick();
-                        }}
-                    />
-                </p>
-            </section>,
-        ];
+        return [<limel-button {...this.props} />, this.renderControls()];
     }
 
-    private onClick() {
-        const button = this.buttonWithClickHandler;
-        button.setAttribute('disabled', 'true');
-        button.setAttribute('loading', 'true');
+    private renderControls() {
+        const controls = [
+            {
+                property: 'label',
+                label: 'Label',
+                value: 'My button',
+            },
+            {
+                property: 'primary',
+                label: 'Primary',
+                value: true,
+            },
+            {
+                property: 'outlined',
+                label: 'Outlined',
+                value: true,
+            },
+            {
+                property: 'icon',
+                label: 'Icon',
+                value: 'filled_message',
+            },
+            {
+                property: 'disabled',
+                label: 'Disabled',
+                value: true,
+            },
+            {
+                property: 'loading',
+                label: 'Loading',
+                value: true,
+            },
+        ];
 
-        const TIME_LOADING = 1000;
-        const TIME_DISABLED = 4000;
-        setTimeout(() => {
-            button.setAttribute('loading', 'false');
-            setTimeout(() => {
-                button.setAttribute('disabled', 'false');
-            }, TIME_DISABLED);
-        }, TIME_LOADING);
+        return controls.map(control => {
+            return (
+                <limel-switch
+                    label={control.label}
+                    value={!!this.props[control.property]}
+                    onChange={event => {
+                        this.props = {
+                            ...this.props,
+                            ...{
+                                [control.property]:
+                                    (event.detail && control.value) || null,
+                            },
+                        };
+                    }}
+                />
+            );
+        });
     }
 }
