eb706e7d83e67e80ca120410b1ab712d167be664
Johan Groth <johan.groth@lime.tech>
2019-03-18 15:36:30 +0100

    fix(icon): define icon sizes

---
+++ src/components/icon/icon.ts
@@ -0,0 +1,1 @@
+export type IconSize = 'x-small' | 'small' | 'medium' | 'large';
--- src/interface.d.ts
+++ src/interface.d.ts
@@ -3,3 +3,4 @@ export * from './components';
 export * from './components/list/list-item';
 export * from './components/select/option';
 export * from './components/picker/searcher';
+export * from './components/icon/icon';
