fc6a0c4ef840c39c480d1ae059ae1a1285ecb37f
Johan Groth <johan.groth@lime.tech>
2019-03-19 14:20:48 +0100

    docs(events): clarify docs about input and event handling

--- src/components/input-field/input-field.tsx
+++ src/components/input-field/input-field.tsx
@@ -50,11 +50,6 @@ export class InputField {
 
     /**
      * The value of the field.
-     * Please note that the value of this property is *not* updated by the
-     * component itself when the user enters text. Instead, the new value is
-     * available via the `change` event. If the new value is accepted (the
-     * normal case), the consumer must update this property accordingly. If
-     * the value is not updated, the change is, in effect, rejected.
      */
     @Prop({ reflectToAttr: true })
     public value: string;
@@ -99,8 +94,6 @@ export class InputField {
 
     /**
      * Emitted when the input value is changed.
-     * The new value is available via the `detail` property on the event
-     * object.
      */
     @Event()
     private change: EventEmitter<string>;
