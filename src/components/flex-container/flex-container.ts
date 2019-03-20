7f8c481e604d3cdac7cb77c9760333ae0a858e73
Johan Groth <johan.groth@lime.tech>
2019-03-20 15:20:33 +0100

    feat(flex container): new flex container component

---
+++ src/components/flex-container/flex-container.ts
@@ -0,0 +1,9 @@
+export type FlexContainerDirection = 'horizontal' | 'vertical';
+export type FlexContainerAlign = 'start' | 'end' | 'center' | 'stretch';
+export type FlexContainerJustify =
+    | 'start'
+    | 'end'
+    | 'center'
+    | 'space-between'
+    | 'space-around'
+    | 'space-evenly';
