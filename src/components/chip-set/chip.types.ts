21b5318ddade450ab0e2d883569de608f11d0748
Adrian Schmidt <adrian.schmidt@lime.tech>
2019-03-21 14:10:34 +0100

    docs(chip): clean up docs for typings

--- src/components/chip-set/chip.types.ts
+++ src/components/chip-set/chip.types.ts
@@ -1,36 +1,36 @@
 export interface Chip<T = any> {
     /**
-     * Id of the chip. Needs to be unique
+     * ID of the chip. Must be unique.
      */
     id: number | string;
 
     /**
-     * Text to display inside the chip
+     * Text to display inside the chip.
      */
     text: string;
 
     /**
-     * Name of the icon to use. Not valid for `filter`
+     * Name of the icon to use. Not valid for `filter`.
      */
     icon?: string;
 
     /**
-     * Background color of the icon. Overrides `--icon-background-color`
+     * Background color of the icon. Overrides `--icon-background-color`.
      */
     iconColor?: string;
 
     /**
-     * Wether the chip should be removable. Not valid for `choice`
+     * Whether the chip should be removable. Not valid for `choice`.
      */
     removable?: boolean;
 
     /**
-     * Wether the chip is selected or not. Only valid for `choice` and `filter`
+     * Whether the chip is selected. Only valid for `choice` and `filter`.
      */
     selected?: boolean;
 
     /**
-     * Value of the chip
+     * Value of the chip.
      */
     value?: T;
 }
