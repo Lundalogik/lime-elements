4eafbbcfabd91ecbb6141f280d1f4e5f10c7a77f
Johan Groth <johan.groth@lime.tech>
2019-06-10 16:00:57 +0200

    feat(list): add property for setting size on icon

--- src/components/list/list-renderer-config.ts
+++ src/components/list/list-renderer-config.ts
@@ -1,7 +1,10 @@
+import { IconSize } from '../icon/icon.types';
 import { ListType } from './list.types';
+
 export interface ListRendererConfig {
     isMenu?: boolean;
     isOpen?: boolean;
     badgeIcons?: boolean;
     type?: ListType;
+    iconSize?: IconSize;
 }
