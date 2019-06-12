a790438a0c9bef9ecbfb59584c9f75d8ac3f0efa
Adrian Schmidt <adrian.schmidt@lime.tech>
2019-06-12 10:02:32 +0200

    chore(semantic-release): move release-config from package.json to release.config.js

---
+++ release.config.js
@@ -0,0 +1,19 @@
+module.export = {
+    branch: 'master',
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/changelog',
+        '@semantic-release/npm',
+        '@semantic-release/git',
+        '@semantic-release/github',
+        [
+            '@semantic-release/exec',
+            {
+                publishCmd:
+                    'npm run docz:publish -- --v=${nextRelease.version} --dryRun=${options.dryRun}',
+            },
+        ],
+    ],
+    npmPublish: true,
+};
