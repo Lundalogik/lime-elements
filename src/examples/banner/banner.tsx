110d2ebfa0688aa7cc2f96236e13ec63e555c523
Katja Bregenzer <katja.bregenzer@lime.tech>
2019-08-08 16:35:56 +0200

    feat(limel-banner): add component

---
+++ src/examples/banner/banner.tsx
@@ -0,0 +1,67 @@
+import { Component, Element, h, State } from '@stencil/core';
+
+@Component({
+    tag: 'limel-example-banner',
+    shadow: true,
+    styleUrl: 'banner.scss',
+})
+export class BannerExample {
+    @Element()
+    private host: HTMLElement;
+
+    @State()
+    private disabled = false;
+
+    private message = 'This is a non-blocking but also non-transient message';
+
+    private banner: HTMLLimelBannerElement;
+
+    constructor() {
+        this.openBanner = this.openBanner.bind(this);
+        this.closeBanner = this.closeBanner.bind(this);
+        this.someCustomAction = this.someCustomAction.bind(this);
+    }
+
+    public componentDidLoad() {
+        this.banner = this.host.shadowRoot.querySelector('limel-banner');
+    }
+
+    public render() {
+        return [
+            <limel-button
+                primary={true}
+                disabled={this.disabled}
+                label="Show Banner"
+                onClick={this.openBanner}
+            />,
+            <limel-banner message={this.message} icon="exclamation_mark">
+                <limel-flex-container
+                    justify="end"
+                    align="stretch"
+                    slot="buttons"
+                >
+                    <limel-button
+                        label="Some Action"
+                        onClick={this.someCustomAction}
+                    />
+                    <limel-button label="Close" onClick={this.closeBanner} />
+                </limel-flex-container>
+            </limel-banner>,
+        ];
+    }
+
+    private openBanner() {
+        this.banner.open();
+        this.disabled = true;
+    }
+
+    private closeBanner() {
+        this.banner.close();
+        this.disabled = false;
+    }
+
+    private someCustomAction() {
+        alert('Triggered an action of some sort');
+        this.closeBanner();
+    }
+}
