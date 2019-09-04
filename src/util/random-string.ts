a6e817c2096380358cea37ca5dafa0a038d2cd1c
Adrian Schmidt <adrian.schmidt@lime.tech>
2019-09-04 11:59:02 +0200

    fix(util/random-string): make sure the string always begins with a letter, not a digit

--- src/util/random-string.ts
+++ src/util/random-string.ts
@@ -1,7 +1,13 @@
 export const createRandomString = () => {
     const USE_HEX = 36;
     const SKIP_LEADING_ZERODOT = 2;
+    const ASCII_A = 97;
+    const NUMBER_OF_LETTERS = 26;
+
     return (
+        String.fromCharCode(
+            ASCII_A + Math.floor(Math.random() * NUMBER_OF_LETTERS)
+        ) +
         Math.random()
             .toString(USE_HEX)
             .substring(SKIP_LEADING_ZERODOT) +
