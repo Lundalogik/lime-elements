08e66af792426d411b3f573c42fc429df69e9672
Adrian Schmidt <adrian.schmidt@lime.tech>
2019-09-11 15:14:16 +0200

    fix(snackbar): supplies its own x-icon, independent of icon-set used

--- src/components/snackbar/snackbar.tsx
+++ src/components/snackbar/snackbar.tsx
@@ -152,8 +152,15 @@ export class Snackbar {
             return;
         }
 
+        const svgData = `<svg width="32" height="32" x="0px" y="0px" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg">
+    <line fill="none" id="svg_1" stroke="currentColor" stroke-width="2" x1="8" x2="24" y1="8" y2="24"/>
+    <line fill="none" id="svg_2" stroke="currentColor" stroke-width="2" x1="24" x2="8" y1="8" y2="24"/>
+</svg>`;
+
         return (
-            <limel-icon-button class="mdc-snackbar__dismiss" icon="multiply" />
+            <div class="mdc-snackbar__dismiss">
+                <button class={`mdc-icon-button`} innerHTML={svgData} />
+            </div>
         );
     }
 }
