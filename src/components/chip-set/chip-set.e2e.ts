74927fead1f2d78fb9643c1e2585eb068aabf344
Adrian Schmidt <adrian.schmidt@lime.tech>
2019-10-02 12:38:35 +0200

    fix(chip-set): remove extra empty space below chips when picker is very narrow (hide the input)
    fix #455

--- src/components/chip-set/chip-set.e2e.ts
+++ src/components/chip-set/chip-set.e2e.ts
@@ -2,12 +2,15 @@ import { E2EElement, E2EPage, newE2EPage } from '@stencil/core/testing';
 
 describe('limel-chip-set', () => {
     let page: E2EPage;
+    let chipSet: E2EElement;
+    let chips: E2EElement[];
+    let spy;
 
     describe('basic chip set', () => {
         beforeEach(async () => {
             page = await createPage(`<limel-chip-set></limel-chip-set>`);
 
-            const chipSet = await page.find('limel-chip-set');
+            chipSet = await page.find('limel-chip-set');
             chipSet.setProperty('value', [
                 {
                     id: '1',
@@ -18,33 +21,26 @@ describe('limel-chip-set', () => {
                     text: 'Apple',
                 },
             ]);
-
             await page.waitForChanges();
+
+            chips = await page.findAll('limel-chip-set >>> .mdc-chip');
+
+            spy = await chipSet.spyOnEvent('interact');
         });
 
-        it('renders the chips', async () => {
-            const chips: E2EElement[] = await page.findAll(
-                'limel-chip-set >>> .mdc-chip'
-            );
-
+        it('renders the chips', () => {
             expect(chips.length).toEqual(2);
             expect(chips[0]).toEqualText('Lime');
             expect(chips[1]).toEqualText('Apple');
         });
 
         describe('when a chip is clicked', () => {
-            let spy;
-
             beforeEach(async () => {
-                const chipSet: E2EElement = await page.find('limel-chip-set');
-                spy = await chipSet.spyOnEvent('interact');
-                const chip: E2EElement = await page.find(
-                    'limel-chip-set >>> .mdc-chip'
-                );
-                await chip.click();
+                await chips[0].click();
             });
 
             it('emits an interact event', () => {
+                expect(spy).toHaveReceivedEventTimes(1);
                 expect(spy).toHaveReceivedEventDetail({
                     id: '1',
                     text: 'Lime',
@@ -59,7 +55,7 @@ describe('limel-chip-set', () => {
                 `<limel-chip-set type="choice"></limel-chip-set>`
             );
 
-            const chipSet = await page.find('limel-chip-set');
+            chipSet = await page.find('limel-chip-set');
             chipSet.setProperty('value', [
                 {
                     id: '1',
@@ -73,13 +69,13 @@ describe('limel-chip-set', () => {
             ]);
 
             await page.waitForChanges();
+
+            chips = await page.findAll('limel-chip-set >>> .mdc-chip');
+
+            spy = await chipSet.spyOnEvent('change');
         });
 
-        it('renders the chips correctly', async () => {
-            const chips: E2EElement[] = await page.findAll(
-                'limel-chip-set >>> .mdc-chip'
-            );
-
+        it('renders the chips correctly', () => {
             expect(chips.length).toEqual(2);
             expect(chips[0]).toEqualText('Lime');
             expect(chips[1]).toEqualText('Apple');
@@ -89,18 +85,12 @@ describe('limel-chip-set', () => {
         });
 
         describe('when the first chip is clicked', () => {
-            let spy;
-
             beforeEach(async () => {
-                const chipSet: E2EElement = await page.find('limel-chip-set');
-                spy = await chipSet.spyOnEvent('change');
-                const chip: E2EElement = await page.find(
-                    'limel-chip-set >>> .mdc-chip'
-                );
-                await chip.click();
+                await chips[0].click();
             });
 
             it('emits a change event', () => {
+                expect(spy).toHaveReceivedEventTimes(1);
                 expect(spy).toHaveReceivedEventDetail({
                     id: '1',
                     text: 'Lime',
@@ -108,32 +98,17 @@ describe('limel-chip-set', () => {
                 });
             });
 
-            it('deselects the first chip', async () => {
-                const chips: E2EElement[] = await page.findAll(
-                    'limel-chip-set >>> .mdc-chip'
-                );
-
+            it('deselects the first chip', () => {
                 expect(chips[0]).not.toHaveClass('mdc-chip--selected');
             });
 
-            it('does not select the second chip', async () => {
-                const chips: E2EElement[] = await page.findAll(
-                    'limel-chip-set >>> .mdc-chip'
-                );
-
+            it('does not select the second chip', () => {
                 expect(chips[1]).not.toHaveClass('mdc-chip--selected');
             });
         });
 
         describe('when the second chip is clicked', () => {
-            let spy;
-
             beforeEach(async () => {
-                const chipSet: E2EElement = await page.find('limel-chip-set');
-                spy = await chipSet.spyOnEvent('change');
-                const chips: E2EElement[] = await page.findAll(
-                    'limel-chip-set >>> .mdc-chip'
-                );
                 await chips[1].click();
             });
 
@@ -152,18 +127,10 @@ describe('limel-chip-set', () => {
             });
 
             it('deselects the first chip', async () => {
-                const chips: E2EElement[] = await page.findAll(
-                    'limel-chip-set >>> .mdc-chip'
-                );
-
                 expect(chips[0]).not.toHaveClass('mdc-chip--selected');
             });
 
             it('selects the second chip', async () => {
-                const chips: E2EElement[] = await page.findAll(
-                    'limel-chip-set >>> .mdc-chip'
-                );
-
                 expect(chips[1]).toHaveClass('mdc-chip--selected');
             });
         });
@@ -175,7 +142,7 @@ describe('limel-chip-set', () => {
                 `<limel-chip-set type="filter"></limel-chip-set>`
             );
 
-            const chipSet = await page.find('limel-chip-set');
+            chipSet = await page.find('limel-chip-set');
             chipSet.setProperty('value', [
                 {
                     id: '1',
@@ -189,13 +156,13 @@ describe('limel-chip-set', () => {
             ]);
 
             await page.waitForChanges();
+
+            chips = await page.findAll('limel-chip-set >>> .mdc-chip');
+
+            spy = await chipSet.spyOnEvent('change');
         });
 
-        it('renders the chips correctly', async () => {
-            const chips: E2EElement[] = await page.findAll(
-                'limel-chip-set >>> .mdc-chip'
-            );
-
+        it('renders the chips correctly', () => {
             expect(chips.length).toEqual(2);
             expect(chips[0]).toEqualText('Lime');
             expect(chips[1]).toEqualText('Apple');
@@ -205,55 +172,35 @@ describe('limel-chip-set', () => {
         });
 
         describe('when the first chip is clicked', () => {
-            let spy;
-
             beforeEach(async () => {
-                const chipSet: E2EElement = await page.find('limel-chip-set');
-                spy = await chipSet.spyOnEvent('change');
-                const chip: E2EElement = await page.find(
-                    'limel-chip-set >>> .mdc-chip'
-                );
-                await chip.click();
+                await chips[0].click();
             });
 
             it('emits a change event', () => {
-                expect(spy).toHaveReceivedEventDetail({
+                expect(spy).toHaveReceivedEventTimes(1);
+                expect(spy.events[0].detail).toEqual({
                     id: '1',
                     text: 'Lime',
                     selected: false,
                 });
             });
 
-            it('deselects the first chip', async () => {
-                const chips: E2EElement[] = await page.findAll(
-                    'limel-chip-set >>> .mdc-chip'
-                );
-
+            it('deselects the first chip', () => {
                 expect(chips[0]).not.toHaveClass('mdc-chip--selected');
             });
 
-            it('does not select the second chip', async () => {
-                const chips: E2EElement[] = await page.findAll(
-                    'limel-chip-set >>> .mdc-chip'
-                );
-
+            it('does not select the second chip', () => {
                 expect(chips[1]).not.toHaveClass('mdc-chip--selected');
             });
         });
 
         describe('when the second chip is clicked', () => {
-            let spy;
-
             beforeEach(async () => {
-                const chipSet: E2EElement = await page.find('limel-chip-set');
-                spy = await chipSet.spyOnEvent('change');
-                const chips: E2EElement[] = await page.findAll(
-                    'limel-chip-set >>> .mdc-chip'
-                );
                 await chips[1].click();
             });
 
             it('emits a change event', () => {
+                expect(spy).toHaveReceivedEventTimes(1);
                 expect(spy).toHaveReceivedEventDetail({
                     id: '2',
                     text: 'Apple',
@@ -261,26 +208,19 @@ describe('limel-chip-set', () => {
                 });
             });
 
-            it('deselects the first chip', async () => {
-                const chips: E2EElement[] = await page.findAll(
-                    'limel-chip-set >>> .mdc-chip'
-                );
-
+            it('deselects the first chip', () => {
                 expect(chips[0]).toHaveClass('mdc-chip--selected');
             });
 
-            it('selects the second chip', async () => {
-                const chips: E2EElement[] = await page.findAll(
-                    'limel-chip-set >>> .mdc-chip'
-                );
-
+            it('selects the second chip', () => {
                 expect(chips[1]).toHaveClass('mdc-chip--selected');
             });
         });
     });
 
     describe('input chip set', () => {
-        let chipSet: E2EElement;
+        let firstChipRemoveButton;
+        let secondChipRemoveButton;
 
         beforeEach(async () => {
             page = await createPage(
@@ -301,27 +241,23 @@ describe('limel-chip-set', () => {
             ]);
 
             await page.waitForChanges();
+
+            chips = await page.findAll('limel-chip-set >>> .mdc-chip');
+
+            firstChipRemoveButton = await chips[0].find('div[role="button"]');
+            secondChipRemoveButton = await chips[1].find('div[role="button"]');
         });
 
-        it('renders the chips correctly', async () => {
-            const chips: E2EElement[] = await page.findAll(
-                'limel-chip-set >>> .mdc-chip'
-            );
-
+        it('renders the chips correctly', () => {
             expect(chips.length).toEqual(2);
             expect(chips[0]).toEqualText('Lime');
             expect(chips[1]).toEqualText('Apple');
 
-            let button = await chips[0].find('div[role="button"]');
-            expect(button).toBeTruthy();
-
-            button = await chips[1].find('div[role="button"]');
-            expect(button).toBeFalsy();
+            expect(firstChipRemoveButton).toBeTruthy();
+            expect(secondChipRemoveButton).toBeFalsy();
         });
 
         describe('when typing in the input field', () => {
-            let spy;
-
             beforeEach(async () => {
                 spy = await chipSet.spyOnEvent('input');
 
@@ -334,23 +270,21 @@ describe('limel-chip-set', () => {
             });
 
             it('emits an input event', () => {
-                expect(spy).toHaveReceivedEventDetail('Banana');
+                expect(spy).toHaveReceivedEventTimes(6);
+                expect(spy.events[0].detail).toEqual('B');
+                expect(spy.events[5].detail).toEqual('Banana');
             });
         });
 
         describe('when a chip is clicked', () => {
-            let spy;
-
             beforeEach(async () => {
                 spy = await chipSet.spyOnEvent('interact');
-                const chip: E2EElement = await page.find(
-                    'limel-chip-set >>> .mdc-chip'
-                );
-                await chip.click();
+                await chips[0].click();
             });
 
-            it('emits a change event where the removed chip is not present', () => {
-                expect(spy).toHaveReceivedEventDetail({
+            it('emits an interact event with the clicked chip', () => {
+                expect(spy).toHaveReceivedEventTimes(1);
+                expect(spy.events[0].detail).toEqual({
                     id: '1',
                     text: 'Lime',
                     removable: true,
@@ -359,18 +293,14 @@ describe('limel-chip-set', () => {
         });
 
         describe('when a chip delete button is clicked', () => {
-            let spy;
-
             beforeEach(async () => {
                 spy = await chipSet.spyOnEvent('change');
-                const button: E2EElement = await page.find(
-                    'limel-chip-set >>> .mdc-chip div[role="button"]'
-                );
-                await button.click();
+                await firstChipRemoveButton.click();
             });
 
             it('emits a change event where the removed chip is not present', () => {
-                expect(spy).toHaveReceivedEventDetail([
+                expect(spy).toHaveReceivedEventTimes(1);
+                expect(spy.events[0].detail).toEqual([
                     {
                         id: '2',
                         text: 'Apple',
@@ -383,22 +313,22 @@ describe('limel-chip-set', () => {
             beforeEach(async () => {
                 chipSet.setAttribute('disabled', true);
                 await page.waitForChanges();
+
+                firstChipRemoveButton = await chips[0].find(
+                    'div[role="button"]'
+                );
+                secondChipRemoveButton = await chips[1].find(
+                    'div[role="button"]'
+                );
             });
 
             it('renders the chips without delete-buttons', async () => {
-                const chips: E2EElement[] = await page.findAll(
-                    'limel-chip-set >>> .mdc-chip'
-                );
-
                 expect(chips.length).toEqual(2);
                 expect(chips[0]).toEqualText('Lime');
                 expect(chips[1]).toEqualText('Apple');
 
-                let button = await chips[0].find('div[role="button"]');
-                expect(button).toBeFalsy();
-
-                button = await chips[1].find('div[role="button"]');
-                expect(button).toBeFalsy();
+                expect(firstChipRemoveButton).toBeFalsy();
+                expect(secondChipRemoveButton).toBeFalsy();
             });
         });
 
@@ -406,22 +336,22 @@ describe('limel-chip-set', () => {
             beforeEach(async () => {
                 chipSet.setAttribute('readonly', true);
                 await page.waitForChanges();
+
+                firstChipRemoveButton = await chips[0].find(
+                    'div[role="button"]'
+                );
+                secondChipRemoveButton = await chips[1].find(
+                    'div[role="button"]'
+                );
             });
 
             it('renders the chips without delete-buttons', async () => {
-                const chips: E2EElement[] = await page.findAll(
-                    'limel-chip-set >>> .mdc-chip'
-                );
-
                 expect(chips.length).toEqual(2);
                 expect(chips[0]).toEqualText('Lime');
                 expect(chips[1]).toEqualText('Apple');
 
-                let button = await chips[0].find('div[role="button"]');
-                expect(button).toBeFalsy();
-
-                button = await chips[1].find('div[role="button"]');
-                expect(button).toBeFalsy();
+                expect(firstChipRemoveButton).toBeFalsy();
+                expect(secondChipRemoveButton).toBeFalsy();
             });
         });
     });
