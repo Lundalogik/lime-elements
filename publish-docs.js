Lundalogik/lime-elements

commit 5e84e341aaf383c1a5269f98a8282194f1f341e5

docs: add version and link to changelog on landing-page

publish-docs.js
@@ -158,6 +158,13 @@ function build() {
         };
         replace.sync(options4);
 
+        const options5 = {
+            files: ['src/index.mdx'],
+            from: /<version>/g,
+            to: `${version}`,
+        };
+        replace.sync(options5);
+
         shell.exec('git diff --name-status');
     } catch (error) {
         shell.echo('Error occurred:', error);
@@ -320,7 +327,7 @@ function publish() {
 function teardown(finished) {
     if (finished || cleanOnFail) {
         shell.exec(
-            'git checkout doczrc.js src/examples/example.tsx src/examples/props.tsx src/index.html stencil.config.docs.ts'
+            'git checkout doczrc.js src/examples/example.tsx src/examples/props.tsx src/index.html src/index.mdx stencil.config.docs.ts'
         );
         shell.echo('Removing docs repo clone in docsDist.');
         shell.exec('rm -rf docsDist');

src/index.mdx
@@ -6,6 +6,8 @@ menu: Home
 
 # Lime Elements
 
+Version [<version>](https://github.com/Lundalogik/lime-elements/blob/v<version>/CHANGELOG.md). Please see the [changelog](https://github.com/Lundalogik/lime-elements/blob/master/CHANGELOG.md) for information about all versions.
+
 This is the documentation for Lime Elements. Under *Components*, you will find documentation for each available component, along with examples of use.
 
 ## Colors

commit e39a0dddefa4742827a48c58ae4f07a62147c0a6

chore: fix compile issue in docz-app when running in dev
The problem was introduced in 5e84e341aaf383c1a5269f98a8282194f1f341e5

publish-docs.js
@@ -160,7 +160,7 @@ function build() {
 
         const options5 = {
             files: ['src/index.mdx'],
-            from: /<version>/g,
+            from: /<version\\>/g,
             to: `${version}`,
         };
         replace.sync(options5);

src/index.mdx
@@ -6,7 +6,7 @@ menu: Home
 
 # Lime Elements
 
-Version [<version>](https://github.com/Lundalogik/lime-elements/blob/v<version>/CHANGELOG.md). Please see the [changelog](https://github.com/Lundalogik/lime-elements/blob/master/CHANGELOG.md) for information about all versions.
+Version [<version\>](https://github.com/Lundalogik/lime-elements/blob/v<version\>/CHANGELOG.md). Please see the [changelog](https://github.com/Lundalogik/lime-elements/blob/master/CHANGELOG.md) for information about all versions.
 
 This is the documentation for Lime Elements. Under *Components*, you will find documentation for each available component, along with examples of use.
 