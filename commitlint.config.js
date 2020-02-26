0c1371c62fe358089798f3198e9eb79245c6c5c9
Adrian Schmidt <adrian.schmidt@lime.tech>
2020-02-26 13:32:21 +0100

    ci(commitlint): disable subject-case rule

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,6 +3,7 @@ module.exports = {
     rules: {
         'header-max-length': [2, 'always', 100],
         'scope-case': [0, 'always', 'lower-case'],
+        'subject-case': [0, 'always', 'lower-case'],
         'type-enum': [
             2,
             'always',
