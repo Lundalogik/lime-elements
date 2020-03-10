b5cdcd3a27a0a9ae51daa1be7f9563a2c6f6674a
Adrian Schmidt <adrian.schmidt@lime.tech>
2020-03-10 13:43:58 +0100

    build(stencil): remove broken type imports from stencil config files

--- stencil.config.docs.ts
+++ stencil.config.docs.ts
@@ -1,27 +1,26 @@
 import { Config } from '@stencil/core';
-import { OutputTargetWww } from '@stencil/core/dist/declarations/output-targets';
 import { sass } from '@stencil/sass';
-
-const targetWww: OutputTargetWww = {
-    type: 'www',
-    serviceWorker: null,
-    dir: '.docz/dist/stencil',
-    baseUrl: '/',
-    copy: [
-        { src: 'components/**/examples/**/*.tsx' },
-        { src: 'components/**/examples/**/*.scss' },
-        { src: 'components/**/*.md' },
-        {
-            src: '../node_modules/@lundalogik/lime-icons8/assets/',
-            dest: 'assets/',
-        },
-    ],
-};
 
 export const config: Config = {
     hashFileNames: false,
     namespace: 'lime-elements',
-    outputTargets: [targetWww],
+    outputTargets: [
+        {
+            type: 'www',
+            serviceWorker: null,
+            dir: '.docz/dist/stencil',
+            baseUrl: '/',
+            copy: [
+                { src: 'components/**/examples/**/*.tsx' },
+                { src: 'components/**/examples/**/*.scss' },
+                { src: 'components/**/*.md' },
+                {
+                    src: '../node_modules/@lundalogik/lime-icons8/assets/',
+                    dest: 'assets/',
+                },
+            ],
+        },
+    ],
     commonjs: {
         namedExports: {
             'node_modules/react-dom/index.js': [
--- stencil.config.ts
+++ stencil.config.ts
@@ -1,35 +1,30 @@
 import { Config } from '@stencil/core';
-import {
-    OutputTargetDist,
-    OutputTargetWww,
-} from '@stencil/core/dist/declarations/output-targets';
 import { sass } from '@stencil/sass';
 import doczStarter from './rollup-plugin-docz-starter';
-
-const targetDist: OutputTargetDist = {
-    type: 'dist',
-    copy: [{ src: 'style/' }],
-};
-
-const targetWww: OutputTargetWww = {
-    type: 'www',
-    serviceWorker: null,
-    dir: '.docz/public/stencil',
-    copy: [
-        { src: 'components/**/examples/**/*.tsx' },
-        { src: 'components/**/examples/**/*.scss' },
-        { src: 'components/**/*.md' },
-        {
-            src: '../node_modules/@lundalogik/lime-icons8/assets/',
-            dest: 'assets/',
-        },
-    ],
-};
 
 export const config: Config = {
     hashFileNames: false,
     namespace: 'lime-elements',
-    outputTargets: [targetDist, targetWww],
+    outputTargets: [
+        {
+            type: 'dist',
+            copy: [{ src: 'style/' }],
+        },
+        {
+            type: 'www',
+            serviceWorker: null,
+            dir: '.docz/public/stencil',
+            copy: [
+                { src: 'components/**/examples/**/*.tsx' },
+                { src: 'components/**/examples/**/*.scss' },
+                { src: 'components/**/*.md' },
+                {
+                    src: '../node_modules/@lundalogik/lime-icons8/assets/',
+                    dest: 'assets/',
+                },
+            ],
+        },
+    ],
     commonjs: {
         namedExports: {
             'node_modules/react-dom/index.js': [
