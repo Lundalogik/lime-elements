afeedd1f370eaf37526572f08e8308883c6941b5
Bryce Sampson <sampson.bryce@protonmail.com>
2020-04-16 11:06:16 +0200

    chore(schema-field): added support for resetting fields when a dependency changes
    This change fixes issues with dependent fields having potentially
    differnt schemas depending on the dependent variables value. https://github.com/rjsf-team/react-jsonschema-form/issues/1701
    
    BREAKING CHANGE: data in the form will be wiped out when changing a
    dependent value

--- src/components/form/fields/schema-field.ts
+++ src/components/form/fields/schema-field.ts
@@ -2,8 +2,11 @@ import { LimeElementsAdapter } from '../adapter';
 import JSONSchemaField from 'react-jsonschema-form/lib/components/fields/SchemaField';
 import React from 'react';
 import { FieldProps } from './types';
-import { isEqual } from 'lodash-es';
-import { getDefaultFormState } from 'react-jsonschema-form/lib/utils';
+import { union, isEqual, isPlainObject } from 'lodash-es';
+import {
+    retrieveSchema,
+    getDefaultFormState
+} from 'react-jsonschema-form/lib/utils';
 
 const hasOverridenField = (schema): boolean => {
     return Boolean(schema.lime?.overrides?.field?.name);
@@ -18,48 +21,164 @@ const getOverridenField = (
     return { name: name, props: props };
 };
 
-export const SchemaField = (props: FieldProps) => {
-    const handleChange = event => {
-        event.stopPropagation();
-        props.onChange(event.detail);
-    };
+const equals = (a, b) => a === b;
 
-    let prevFormData;
-    let curFormData = props.formData;
+export class SchemaField extends React.Component<FieldProps> {
+    currentFormData = {};
 
-    while (!isEqual(prevFormData, curFormData)) {
-        console.log('FORM DATA DOES NOT MATCH', prevFormData, curFormData);
-        prevFormData = curFormData;
+    constructor(props: FieldProps) {
+        super(props);
 
-        curFormData = getDefaultFormState(
-            props.schema,
-            curFormData,
-            props.registry.definitions
+        this.handleChange = this.handleChange.bind(this);
+        this.detectDependencyAffectingChanges = this.detectDependencyAffectingChanges.bind(
+            this
         );
     }
 
-    const fieldProps = {
-        ...props,
-        formData: curFormData
-    };
+    handleChange(event) {
+        event.stopPropagation();
 
-    if (hasOverridenField(props.schema)) {
-        console.log('OVERRIDEN SCHEMA FIELD', props);
+        console.log('Schema Field Change', event.detail);
+        this.detectDependencyAffectingChanges(event.detail);
+    }
 
-        const { name, props: overridenFieldProps } = getOverridenField(
-            props.schema
+    detectDependencyAffectingChanges(data) {
+        console.log(
+            'Schema Field detecting dependency changes',
+            data,
+            this.props,
+            isPlainObject(data)
         );
-        return React.createElement(LimeElementsAdapter, {
-            name: name,
-            elementProps: {
-                fieldProps: fieldProps,
-                ...overridenFieldProps
-            },
-            events: {
-                change: handleChange
+
+        // Dependencies only exist on object types
+        if (isPlainObject(data)) {
+            console.log('Schema Field comparing', this.currentFormData, data);
+
+            // Get the keys that have changed between the current data and the new data
+            // If the current data is undefined, all the keys have changed
+            const changedKeys = this.currentFormData
+                ? this.getDifferentKeys(this.currentFormData, data)
+                : Object.keys(data);
+
+            console.log('Schema Field changed keys', changedKeys);
+
+            // Get the new schema that is calculated for the new data
+            const newSchema =
+                '$ref' in this.props.schema
+                    ? retrieveSchema(
+                          this.props.schema,
+                          this.props.registry.definitions,
+                          data
+                      )
+                    : this.props.schema;
+
+            console.log('Schema Field changed got schema', newSchema);
+
+            // For each key that was changed in the data, remove to calculate the
+            // test schema that is generated without that key. We do this because by
+            // doing so we can see which keys change whether or not that data exists
+            // and therefore can determine the dependencies of that field
+            for (const key of changedKeys) {
+                // Get test schema with changed key removed
+                // const testData = { ...data, [key]: undefined };
+                const currentSchema =
+                    '$ref' in this.props.schema
+                        ? retrieveSchema(
+                              this.props.schema,
+                              this.props.registry.definitions,
+                              this.currentFormData
+                          )
+                        : this.props.schema;
+                console.log(
+                    'Schema Field changed got test schema with key removed',
+                    key,
+                    currentSchema,
+                    this.currentFormData
+                );
+
+                // Get the properties that change when the key is removed.
+                // This should return us all keys that are dependent on the key
+                // that we just removed from the data
+                const dependentPropertyKeys = this.getDifferentKeys(
+                    newSchema.properties,
+                    currentSchema.properties,
+                    isEqual
+                );
+
+                console.log(
+                    'Schema Field properties dependent on',
+                    key,
+                    dependentPropertyKeys
+                );
+
+                // Reset keys that are dependent on the changed value
+                // the values for these dependent fields will be repopulated
+                // with defaults during the next render
+                for (const dependentPropertyKey of dependentPropertyKeys) {
+                    delete data[dependentPropertyKey];
+                    // const { [dependentPropertyKey]: _, ...newData } = data;
+                    // data = newData;
+                }
             }
+
+            console.log('Schema Field new data', data);
+        }
+
+        this.props.onChange(data);
+    }
+
+    getDifferentKeys(a: object, b: object, equalsFunc = equals): any[] {
+        const keys = union(Object.keys(a), Object.keys(b));
+        return keys.filter(key => {
+            return !equalsFunc(b[key], a[key]);
         });
     }
 
-    return React.createElement(JSONSchemaField, fieldProps);
-};
+    render() {
+        let prevFormData;
+        this.currentFormData = this.props.formData;
+
+        while (!isEqual(prevFormData, this.currentFormData)) {
+            console.log(
+                'FORM DATA DOES NOT MATCH',
+                prevFormData,
+                this.currentFormData
+            );
+            prevFormData = this.currentFormData;
+
+            this.currentFormData = getDefaultFormState(
+                this.props.schema,
+                this.currentFormData,
+                this.props.registry.definitions
+            );
+        }
+
+        console.log('FORM DATA CALCULATED WITH DEFAULTS', this.currentFormData);
+
+        const fieldProps = {
+            ...this.props,
+            formData: this.currentFormData,
+            onChange: this.detectDependencyAffectingChanges
+        };
+
+        if (hasOverridenField(this.props.schema)) {
+            console.log('OVERRIDEN SCHEMA FIELD', this.props);
+
+            const { name, props: overridenFieldProps } = getOverridenField(
+                this.props.schema
+            );
+            return React.createElement(LimeElementsAdapter, {
+                name: name,
+                elementProps: {
+                    fieldProps: fieldProps,
+                    ...overridenFieldProps
+                },
+                events: {
+                    change: this.handleChange
+                }
+            });
+        }
+
+        return React.createElement(JSONSchemaField, fieldProps);
+    }
+}
