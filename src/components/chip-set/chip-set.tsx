2d5db9d2492b0776f4448ea54f10326ecce13c3b
Kiarokh <kiarokh@icloud.com>
2020-05-05 14:17:12 +0200

    fix(chip-set): import styles directly from limel-input-field

--- src/components/chip-set/chip-set.tsx
+++ src/components/chip-set/chip-set.tsx
@@ -287,7 +287,7 @@ export class ChipSet {
 
     private renderInputChips() {
         return (
-            <label
+            <div
                 class={{
                     'mdc-text-field': true,
                     'mdc-text-field--disabled': this.readonly || this.disabled,
@@ -330,7 +330,7 @@ export class ChipSet {
                     {this.label}
                 </label>
                 <div class="mdc-line-ripple" />
-            </label>
+            </div>
         );
     }
 
