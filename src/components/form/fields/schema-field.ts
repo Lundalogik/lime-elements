721c0e158693e3d02a5e9cc085faae3c01f08253
Bryce Sampson <sampson.bryce@protonmail.com>
2020-07-06 09:41:16 +0200

    fix(schema-field): change null to undefined for custom web components when appropriate
    fix https://github.com/Lundalogik/lime-elements/issues/863

--- src/components/form/fields/schema-field.ts
+++ src/components/form/fields/schema-field.ts
@@ -4,6 +4,37 @@ import React from 'react';
 import { FieldProps } from './types';
 import { isEmpty, capitalize } from 'lodash-es';
 
+/**
+ * If given a value and schema, check if the value should be translated
+ * from null to undefined to avoid issues with validation
+ *
+ * This function needs to be used for several reasons:
+ * 1. CustomEvent does not allow `detail` to equal `undefined`, but we can call onChange with `undefined` in React
+ * 2. `null` is treated as a valid value in a jsonschema and with marshmallow and it has its own type (null)
+ * 3. Without changing `null` to `undefined` there would be no way to remove a field from
+ *    from the submitted form data once it had any data. (when POSTing, undefined is not posted since its not valid json)
+ * 4. The only applies to custom web components since widgets handle transforming null/'' to undefined depending on the widget
+ *    and its purpose.
+ *
+ * Example:
+ *    If I have an object { name?: string, email?: string }
+ *    that I am using a custom web component for `name`, then initially,
+ *    the formData will be {} which is fine because neither name or email are required.
+ *    Then if i input a value for name the formData will be { name: 'some_value' }
+ *    which is also fine. But then if I want to remove name from the form data I
+ *    would delete all the text from the name input field. Web components would emit
+ *    this empty value as '' or null. If the web component tries to emit `undefined`, null
+ *    would be emitted instead because CustomEvent has a default `detail` value of null
+ *
+ * @param {any} value the value associated with the schema
+ * @param {any} schema the schema for the value
+ *
+ * @returns {boolean} whether or not null should be changed to undefined
+ */
+const shouldChangeToUndefined = (value, schema): boolean => {
+    return value === null && !schema.type.includes('null');
+};
+
 const hasCustomComponent = (schema): boolean => {
     return Boolean(schema.lime?.component?.name);
 };
@@ -109,11 +140,18 @@ export class SchemaField extends React.Component<FieldProps> {
     }
 
     private handleChange(event) {
+        const { schema } = this.props;
         event.stopPropagation();
 
         this.setState({ modified: true });
 
-        this.props.onChange(event.detail);
+        let value = event.detail;
+
+        if (shouldChangeToUndefined(value, schema)) {
+            value = undefined;
+        }
+
+        this.props.onChange(value);
     }
 
     private buildCustomComponentProps() {
