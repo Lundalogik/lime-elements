314dfab434fb7a3d47a56b2b12aa8bb2a4e89a6e
Johan Groth <johan.groth@lime.tech>
2020-07-08 09:54:35 +0200

    feat(form): add support for a props factory for custom components
    connected to Lundalogik/lime-crm-components#107

--- src/components/form/fields/schema-field.spec.ts
+++ src/components/form/fields/schema-field.spec.ts
@@ -1,4 +1,4 @@
-import { resetDependentFields } from './schema-field';
+import { resetDependentFields, getFactoryProps } from './schema-field';
 
 describe('resetDependentFields()', () => {
     const schema = {
@@ -182,6 +182,41 @@ describe('resetDependentFields()', () => {
         ).toEqual({
             type: 'String',
             value: 'cheese',
+        });
+    });
+});
+
+describe('getFactoryProps', () => {
+    describe('when no factory is given', () => {
+        it('returns an empty object', () => {
+            const props = getFactoryProps({}, {});
+            expect(props).toEqual({});
+        });
+    });
+
+    describe('when a factory is set', () => {
+        describe('when the factory returns an object', () => {
+            it('returns the props from the factory', () => {
+                const props = getFactoryProps(
+                    {
+                        propsFactory: () => ({ foo: 'bar' }),
+                    },
+                    {}
+                );
+                expect(props).toEqual({ foo: 'bar' });
+            });
+        });
+
+        describe('when the factory not returns an object', () => {
+            it('returns an empty object', () => {
+                const props = getFactoryProps(
+                    {
+                        propsFactory: () => null,
+                    },
+                    {}
+                );
+                expect(props).toEqual({});
+            });
         });
     });
 });
