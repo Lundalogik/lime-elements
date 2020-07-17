d42c6195ab90e64312d124ff6a378a026571cdb9
Adrian Schmidt <adrian.schmidt@lime.tech>
2020-07-17 15:01:47 +0200

    style: make files comply with lint-rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,11 +1,15 @@
+/* eslint-env node */
+const ERROR = 2;
+const IGNORE = 0;
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'header-max-length': [2, 'always', 100],
-        'scope-case': [0, 'always', 'lower-case'],
-        'subject-case': [0, 'always', 'lower-case'],
+        // eslint-disable-next-line no-magic-numbers
+        'header-max-length': [ERROR, 'always', 100],
+        'scope-case': [IGNORE, 'always', 'lower-case'],
+        'subject-case': [IGNORE, 'always', 'lower-case'],
         'type-enum': [
-            2,
+            ERROR,
             'always',
             [
                 'feat',
