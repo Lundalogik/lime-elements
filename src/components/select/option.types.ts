98b456f911f2132eab2c4017df5f2d27677137c7
Adrian Schmidt <adrian.schmidt@lime.tech>
2020-10-05 14:23:46 +0200

    docs(select): improve docs for the `Option` type, clarifying that `value` must be a `string`
    fix #919

--- src/components/select/option.types.ts
+++ src/components/select/option.types.ts
@@ -1,5 +1,28 @@
+/**
+ * Describes an option for limel-select.
+ */
 export interface Option<T extends string = string> {
+    /**
+     * The name of the option as shown to the user.
+     */
     text: string;
+
+    /**
+     * The unique value of the option. Should always be the same for any given
+     * option, regardless of localization. The type `T` defaults to `string`,
+     * but can be set to any type that extends `string` (using `Option<type>`),
+     * for example an enum of specific strings.
+     *
+     * Note the value of this property *must* be a string!
+     * If the value you wish to use is, for example, numeric, convert it to a
+     * string before sending it to limel-select. Using numeric values does work
+     * in many desktop browsers, but breaks the select completely on some
+     * iOS devices.
+     */
     value: T;
+
+    /**
+     * Set to `true` to make this option disabled and not possible to select.
+     */
     disabled?: boolean;
 }
