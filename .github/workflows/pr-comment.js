c61fab1b821e622fee44968d55ced59cdbd20ee1
Johan Groth <johan.groth@lime.tech>
2021-02-23 16:19:20 +0100

    fixup! ci: add comment on PR for published docs

--- .github/workflows/pr-comment.js
+++ .github/workflows/pr-comment.js
@@ -1,27 +1,22 @@
 module.exports = async ({github, context}) => {
-    // Get a list of all issues created by the PR opener
-    // See: https://octokit.github.io/rest.js/#pagination
-    // const creator = context.payload.sender.login
-    // const opts = github.issues.listForRepo.endpoint.merge({
-    //     ...context.issue,
-    //     state: 'all'
-    // })
-    // const issues = await github.paginate(opts)
-
-    // for (const issue of issues) {
-    //     if (issue.number === context.issue.number) {
-    //         return;
-    //     }
-
-    //     if (issue.pull_request) {
-    //         return // Creator is already a contributor.
-    //     }
-    // }
-
-    await github.issues.createComment({
-        issue_number: context.issue.number,
+    const message = `Documentation has been published to https://lundalogik.github.io/lime-elements/versions/PR-${context.issue.number}/`;
+    const response = await github.issues.listComments({
         owner: context.repo.owner,
         repo: context.repo.repo,
-        body: `Documentation has been published to https://lundalogik.github.io/lime-elements/versions/PR-${context.issue.number}/`
+        issue_number: context.issue.number,
+    });
+
+    const comments = response.data;
+    for (const comment of comments) {
+        if (comment.body === message) {
+            return;
+        }
+    }
+
+    await github.issues.createComment({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        issue_number: context.issue.number,
+        body: message
     });
 }
