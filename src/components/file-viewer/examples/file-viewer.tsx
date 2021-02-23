472267b3bd9f28f61b0453b168f37751bdf49430
Kiarokh Moattar <kiarokh@icloud.com>
2021-02-23 15:27:54 +0100

    better example content

--- src/components/file-viewer/examples/file-viewer.tsx
+++ src/components/file-viewer/examples/file-viewer.tsx
@@ -11,28 +11,24 @@ export class FileViewerExample {
     public render() {
         return [
             <limel-file-viewer
-                url="https://www.lime-technologies.se/wp-content/uploads/2020/05/Product-logos-Preview.png"
-                alt="Something descriptive"
+                url="https://jobs.lime-technologies.com/wp-content/uploads/2020/04/Traineer-ad.jpg"
+                alt="Some of the people working at Lime Technologies"
                 type="image/jpg"
             />,
             <limel-file-viewer
-                url="https://www.skolverket.se/download/18.49f081e1610d887500ae7/1573116091500/information-np-ak9-engelska.pdf"
-                alt="Something descriptive"
+                url="https://file-examples-com.github.io/uploads/2017/10/file-sample_150kB.pdf"
                 type="application/pdf"
             />,
             <limel-file-viewer
                 url="https://filesamples.com/samples/document/txt/sample1.txt"
-                alt="Something descriptive"
                 type="text/plain"
             />,
             <limel-file-viewer
                 url="https://github.com/caseyohara/campfire-sounds/raw/gh-pages/sounds/trombone.mp3"
-                alt="Something descriptive"
                 type="audio/mpeg"
             />,
             <limel-file-viewer
                 url="https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_480_1_5MG.mp4"
-                alt="Something descriptive"
                 type="video/mp4"
             />,
         ];
