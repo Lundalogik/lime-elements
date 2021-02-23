8d1108d55667275f995e7041747edea3c903ede9
lime-ci <617355+lime-ci@users.noreply.github.com>
2021-02-23 13:59:28 +0000

    chore(docs): create docs PR-1150

---
+++ versions/PR-1150/assets/kompendium/collection/components/markdown/markdown-types.js
@@ -0,0 +1,7 @@
+let types = [];
+export function getTypes() {
+    return types;
+}
+export function setTypes(newTypes) {
+    types = newTypes;
+}
---
+++ versions/PR-1150/assets/kompendium/collection/kompendium/test/fixtures/basic.js
@@ -0,0 +1,18 @@
+/**
+ * The colors
+ */
+export var Color;
+(function (Color) {
+    /**
+     * The red color
+     */
+    Color["Red"] = "red";
+    /**
+     * Looks like green
+     */
+    Color["Green"] = "green";
+    /**
+     * Almost blue
+     */
+    Color["Blue"] = "blue";
+})(Color || (Color = {}));
