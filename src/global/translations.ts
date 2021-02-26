2b6ea587b91d56bac2e66c7e95c0fa66e36fa920
Johan Andersson <johan-spam@andersson.net>
2021-02-26 17:44:57 +0100

    feat: add dutch language

--- src/global/translations.ts
+++ src/global/translations.ts
@@ -1,10 +1,11 @@
 import da from '../translations/da';
 import en from '../translations/en';
 import fi from '../translations/fi';
+import nl from '../translations/nl';
 import no from '../translations/no';
 import sv from '../translations/sv';
 
-const allTranslations = { da: da, en: en, fi: fi, no: no, sv: sv };
+const allTranslations = { da: da, en: en, fi: fi, no: no, nl: nl, sv: sv };
 
 export class Translations {
     public get(key, language = 'en') {
---
+++ src/translations/nl.ts
@@ -0,0 +1,5 @@
+export default {
+    'date-picker.month.heading': 'Maand',
+    'date-picker.quarter.heading': 'Kwartaal',
+    'date-picker.year.heading': 'Jaar',
+};
