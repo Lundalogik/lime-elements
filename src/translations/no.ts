2cabc2c4d71e5e754cb77f65888d8853eb9b40cd
maltja <maltja1@gmail.com>
2021-02-26 15:27:41 +0100

    feat(chip-set): make clear all chips button's label translatable

--- src/translations/no.ts
+++ src/translations/no.ts
@@ -2,4 +2,5 @@ export default {
     'date-picker.month.heading': 'Måned',
     'date-picker.quarter.heading': 'Kvartal',
     'date-picker.year.heading': 'År',
+    'chip-set.clear-all': 'Fjern alle',
 };
