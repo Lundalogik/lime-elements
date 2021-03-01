87fcde4a6bd6b96ae192c6be83fa74efb51181ae
lime-ci <617355+lime-ci@users.noreply.github.com>
2021-03-01 15:26:33 +0000

    chore(docs): create docs PR-1166

---
+++ versions/PR-1166/build/limel-example-collapsible-section-with-slider.entry.js
@@ -0,0 +1,1 @@
+import{r as e,h as i}from"./index-9cbaf4f2.js";const l=class{constructor(i){e(this,i),this.percentage=34}render(){return i("limel-collapsible-section",{header:"With a limel-slider"},i("div",null,i("p",null,"This is primarily here for testing purposes."),i("limel-slider",{unit:"%",value:this.percentage})))}};export{l as limel_example_collapsible_section_with_slider}
