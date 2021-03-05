bea1a6860a94568b5bc2065dc28c5c58730df1fe
lime-ci <617355+lime-ci@users.noreply.github.com>
2021-03-05 12:58:07 +0000

    chore(docs): create docs PR-1176

---
+++ versions/PR-1176/assets/kompendium/markdown-code.ts
@@ -0,0 +1,52 @@
+import map from 'unist-util-map';
+
+export function kompendiumCode(): (tree) => any {
+    return transformer;
+}
+
+function transformer(tree) {
+    return map(tree, mapCodeNode);
+}
+
+function mapCodeNode(node) {
+    if (node.type !== 'element') {
+        return node;
+    }
+
+    if (node.tagName !== 'code') {
+        return node;
+    }
+
+    const language = getLanguage(node.properties);
+    if (!language) {
+        return node;
+    }
+
+    return Object.assign({}, node, {
+        type: 'element',
+        tagName: 'kompendium-code',
+        properties: {
+            language: language,
+        },
+        children: [],
+    });
+}
+
+function getLanguage(props: { className?: string[] }) {
+    if (!props) {
+        return;
+    }
+
+    if (!('className' in props)) {
+        return;
+    }
+
+    const languageClass = props.className.find((name) =>
+        name.startsWith('language-')
+    );
+    if (!languageClass) {
+        return;
+    }
+
+    return languageClass.replace('language-', '');
+}
