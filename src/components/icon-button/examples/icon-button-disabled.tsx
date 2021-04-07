1fb9145f2314959d2d5697548f370946cf6012f5
Adrian Schmidt <adrian.schmidt@lime.tech>
2021-04-07 15:12:21 +0200

    docs(icon-button): add missing example for `disabled` state

--- src/components/icon-button/examples/icon-button-disabled.tsx
+++ src/components/icon-button/examples/icon-button-disabled.tsx
@@ -4,7 +4,7 @@ import { Component, h } from '@stencil/core';
  * Disabled
  */
 @Component({
-    tag: 'limel-example-icon-button',
+    tag: 'limel-example-icon-button-disabled',
     shadow: true,
 })
 export class IconButtonDisabledExample {
