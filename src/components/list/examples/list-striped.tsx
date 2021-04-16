1bf833b0feaebb76645cb75fb8132c56e84631c2
Kiarokh Moattar <kiarokh@icloud.com>
2021-04-16 15:04:26 +0200

    docs(list): move custom styles to their respective example

--- src/components/list/examples/list-striped.tsx
+++ src/components/list/examples/list-striped.tsx
@@ -11,7 +11,14 @@ import { Component, h } from '@stencil/core';
  * item which is not `disabled` will display an elevated visual effect, giving
  * it more affordance and a solid feeling of interactivity.
  *
- * **Note:** to get both effects, you need to apply both of these classes.
+ * | Class name              | Description                                                                                     |
+ * | ----------------------- | ----------------------------------------------------------------------------------------------- |
+ * | `has-striped-rows`      | Adds distinct styling by which every other row (list item) gets a darker background.            |
+ * | `has-interactive-items` | Adds more affordance by applying an elevated visual effect on list item, when they are hovered. |
+ *
+ * :::note
+ * to get both effects, you need to apply both of these classes.
+ * :::
  */
 @Component({
     tag: 'limel-example-list-striped',
