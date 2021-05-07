9b52a1d96c161b70b1119444fbe8ecb3c407b26f
Johan Groth <johan.groth@lime.tech>
2021-05-07 16:34:12 +0200

    fix(portal): move portal content back on disconnect
    When the portal is disconnected, we need to move the portal content back from the container to the
    original parent element otherwise the content will be lost forever if the portal is connected to the
    DOM again
    
    fix Lundalogik/crm-feature#2022

---
+++ src/components/portal/examples/portal.tsx
@@ -0,0 +1,84 @@
+import { Component, h, Element, State } from '@stencil/core';
+
+/**
+ * Disconnect test
+ *
+ * This is an example to test that it works to remove a popover from the DOM,
+ * then add it back again without breaking it
+ */
+@Component({
+    tag: 'limel-example-portal',
+    shadow: true,
+})
+export class PortalExample {
+    @State()
+    private isOpen = false;
+
+    @Element()
+    private host: HTMLLimelExamplePortalElement;
+
+    @State()
+    private container: HTMLElement;
+
+    public render() {
+        return (
+            <div class="root">
+                <limel-button
+                    label="Disconnect from DOM"
+                    disabled={!!this.container}
+                    onClick={this.handleDisconnect}
+                />
+                <limel-button
+                    label="Connect to DOM"
+                    disabled={!this.container}
+                    onClick={this.handleConnect}
+                />
+                <div class="container">
+                    <limel-popover
+                        open={this.isOpen}
+                        onClose={this.onPopoverClose}
+                    >
+                        <limel-button
+                            slot="trigger"
+                            primary={true}
+                            label="Click me!"
+                            onClick={this.openPopover}
+                        />
+                        <div>CONTENT</div>
+                    </limel-popover>
+                </div>
+            </div>
+        );
+    }
+
+    private handleDisconnect = () => {
+        if (this.container) {
+            return;
+        }
+
+        const root = this.host.shadowRoot.querySelector('.root');
+        this.container = root.querySelector('.container');
+
+        root.removeChild(this.container);
+    };
+
+    private handleConnect = () => {
+        if (!this.container) {
+            return;
+        }
+
+        const root = this.host.shadowRoot.querySelector('.root');
+        root.appendChild(this.container);
+        this.container = null;
+    };
+
+    private openPopover = (event: MouseEvent) => {
+        event.stopPropagation();
+        this.isOpen = true;
+    };
+
+    private onPopoverClose = (event: CustomEvent) => {
+        event.stopPropagation();
+        this.isOpen = false;
+    };
+}
