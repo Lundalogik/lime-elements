6127a2ba6582a3cbfc4f8f9dd6b75b0c87d1c286
lime-ci <617355+lime-ci@users.noreply.github.com>
2021-05-17 09:24:14 +0000

    chore(docs): create docs PR-1239

---
+++ versions/PR-1239/build/limel-example-icon-button-disabled.entry.js
@@ -0,0 +1,1 @@
+import{r as e,h as i}from"./index-6a31bb18.js";const o=class{constructor(i){e(this,i)}render(){return[i("limel-icon-button",{label:"Add favourite",icon:"heart_outlined",disabled:!0,onClick:this.onClick})]}onClick(){console.log("This should never happen, since the button is disabled.")}};export{o as limel_example_icon_button_disabled}
