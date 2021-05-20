f2906b4715d0e4bc4bbee4ab2cec4a98faeeefa9
Johan Groth <johan.groth@lime.tech>
2021-05-20 14:23:28 +0200

    docs: add dynamic examples with component schema

--- src/components/menu/examples/menu-composite.tsx
+++ src/components/menu/examples/menu-composite.tsx
@@ -1,13 +1,4 @@
-import { ListItem, ListSeparator } from '@limetech/lime-elements';
-import { Component, h, State } from '@stencil/core';
-
-const icons = ['copy', 'cut', '', 'paste'];
-const iconColors = [
-    'rgb(var(--color-lime-light))',
-    'rgb(var(--color-red-light))',
-    '',
-    'rgb(var(--color-amber-default))',
-];
+import { Component, h, Prop, State } from '@stencil/core';
 
 /**
  * Composite
@@ -20,146 +11,103 @@ const iconColors = [
     shadow: true,
 })
 export class MenuCompositeExample {
-    @State()
-    private disabled = false;
+    @Prop()
+    public schema: any;
 
     @State()
-    private openLeft = false;
-
-    @State()
-    private showIcons = false;
-
-    @State()
-    private badgeIcons = false;
-
-    @State()
-    private showColors = false;
-
-    @State()
-    private items: Array<ListItem | ListSeparator> = [
-        { text: 'Copy' },
-        { text: 'Cut' },
-        { separator: true },
-        { text: 'Paste', disabled: true },
-    ];
+    private props = {
+        badgeIcons: true,
+        disabled: false,
+        fixed: false,
+        items: [
+            {
+                text: 'Copy',
+                icon: 'copy',
+                iconColor: 'rgb(var(--color-lime-light))',
+            },
+            {
+                text: 'Cut',
+                icon: 'cut',
+                iconColor: 'rgb(var(--color-red-light))',
+            },
+            { separator: true },
+            {
+                text: 'Paste',
+                disabled: true,
+                icon: 'paste',
+                iconColor: 'rgb(var(--color-amber-default))',
+            },
+        ],
+        open: false,
+        openDirection: 'right',
+    };
 
     private eventPrinter: HTMLLimelExampleEventPrinterElement;
 
-    constructor() {
-        this.handleSelect = this.handleSelect.bind(this);
-        this.handleCancel = this.handleCancel.bind(this);
-        this.handleDisabled = this.handleDisabled.bind(this);
-        this.handleOpenLeft = this.handleOpenLeft.bind(this);
-        this.handleShowIcons = this.handleShowIcons.bind(this);
-        this.handleBadgeIcons = this.handleBadgeIcons.bind(this);
-        this.handleShowColors = this.handleShowColors.bind(this);
+    public componentWillLoad() {
+        this.schema = {
+            ...this.schema,
+            lime: {
+                layout: {
+                    type: 'grid',
+                },
+            },
+        };
+
+        delete this.schema.properties.label;
     }
 
     public render() {
+        console.log(this.schema);
+
         return [
             <limel-menu
-                items={this.items}
-                disabled={this.disabled}
-                openDirection={this.openLeft ? 'left' : 'right'}
-                badgeIcons={this.badgeIcons}
+                items={this.props.items as any}
+                disabled={this.props.disabled}
+                openDirection={this.props.openDirection as any}
+                badgeIcons={this.props.badgeIcons}
+                fixed={this.props.fixed}
+                open={this.props.open}
                 onSelect={this.handleSelect}
                 onCancel={this.handleCancel}
             >
                 <limel-button
                     label="Menu"
-                    disabled={this.disabled}
+                    disabled={this.props.disabled}
                     slot="trigger"
                 />
             </limel-menu>,
-            <limel-example-controls>
-                <limel-switch
-                    label="Disabled"
-                    value={this.disabled}
-                    onChange={this.handleDisabled}
+            <limel-collapsible-section header="Settings">
+                <limel-form
+                    schema={this.schema}
+                    value={this.props}
+                    onChange={this.handleChange}
                 />
-                <limel-switch
-                    label="Open left"
-                    value={this.openLeft}
-                    onChange={this.handleOpenLeft}
-                />
-                <limel-switch
-                    label="Icons"
-                    value={this.showIcons}
-                    onChange={this.handleShowIcons}
-                />
-                <limel-switch
-                    label="Badge icons"
-                    value={this.badgeIcons}
-                    onChange={this.handleBadgeIcons}
-                />
-                <limel-switch
-                    label="Colors"
-                    value={this.showColors}
-                    onChange={this.handleShowColors}
-                />
-            </limel-example-controls>,
+            </limel-collapsible-section>,
             <limel-example-event-printer
                 ref={(el) => (this.eventPrinter = el)}
             />,
         ];
     }
 
-    private handleSelect(event: CustomEvent) {
+    private handleSelect = (event: CustomEvent) => {
         this.eventPrinter.writeEvent(event);
-        this.items = this.items.map((item) => {
+        this.props.items = this.props.items.map((item) => {
             if (!('separator' in item)) {
                 item.disabled = item.text === event.detail.text;
             }
 
             return item;
         });
-    }
 
-    private handleCancel(event: Event) {
+        this.props = { ...this.props };
+    };
+
+    private handleCancel = (event: Event) => {
         this.eventPrinter.writeEvent(event);
-    }
+    };
 
-    private handleDisabled(event: CustomEvent<boolean>) {
-        this.disabled = event.detail;
-    }
-
-    private handleOpenLeft(event: CustomEvent<boolean>) {
-        this.openLeft = event.detail;
-    }
-
-    private handleShowIcons(event: CustomEvent<boolean>) {
-        this.showIcons = event.detail;
-        if (this.showIcons) {
-            this.items = this.items.map((item, index) => {
-                return { ...item, icon: icons[index] };
-            });
-        } else {
-            this.items = this.items.map((item) => {
-                // eslint-disable-next-line @typescript-eslint/dot-notation
-                delete item['icon'];
-
-                return item;
-            });
-        }
-    }
-
-    private handleBadgeIcons(event: CustomEvent<boolean>) {
-        this.badgeIcons = event.detail;
-    }
-
-    private handleShowColors(event: CustomEvent<boolean>) {
-        this.showColors = event.detail;
-        if (this.showColors) {
-            this.items = this.items.map((item, index) => {
-                return { ...item, iconColor: iconColors[index] };
-            });
-        } else {
-            this.items = this.items.map((item) => {
-                // eslint-disable-next-line @typescript-eslint/dot-notation
-                delete item['iconColor'];
-
-                return item;
-            });
-        }
-    }
+    private handleChange = (event: CustomEvent) => {
+        this.props = event.detail;
+    };
 }
