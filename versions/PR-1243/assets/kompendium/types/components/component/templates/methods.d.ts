d5f8324b40fb5b3223c24d55c90c0a6c3b6e0e5b
lime-ci <617355+lime-ci@users.noreply.github.com>
2021-05-21 12:22:52 +0000

    chore(docs): remove PR-1243

--- versions/PR-1243/assets/kompendium/types/components/component/templates/methods.d.ts
+++
@@ -1,5 +0,0 @@
-import { JsonDocsMethod } from '../../../stencil-public-runtime';
-export declare function MethodList({ methods, id, }: {
-    id?: string;
-    methods: Partial<JsonDocsMethod>[];
-}): HTMLElement[];
