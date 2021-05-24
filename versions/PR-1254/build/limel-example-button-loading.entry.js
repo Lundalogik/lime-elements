f64242bd6e58cb2bc1ecea0244c632e46f63547f
lime-ci <617355+lime-ci@users.noreply.github.com>
2021-05-24 13:21:47 +0000

    chore(docs): create docs PR-1254

---
+++ versions/PR-1254/build/limel-example-button-loading.entry.js
@@ -0,0 +1,1 @@
+import{r as t,h as e}from"./index-6a31bb18.js";const l=class{constructor(e){t(this,e)}render(){return e("limel-button",{label:"My Button",disabled:!0,loading:!0})}};export{l as limel_example_button_loading}
