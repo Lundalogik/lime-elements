17fa992f765b14f0ec987e66e6aad78cbdaac42d
lime-ci <617355+lime-ci@users.noreply.github.com>
2021-05-26 12:57:48 +0000

    chore(docs): create docs PR-1258

---
+++ versions/PR-1258/build/limel-config.entry.js
@@ -0,0 +1,1 @@
+import{r as o}from"./index-6a31bb18.js";import{c as t}from"./config-f7362aeb.js";const s=class{constructor(t){o(this,t)}componentDidLoad(){this.setGlobalConfig()}componentDidUpdate(){this.setGlobalConfig()}setGlobalConfig(){Object.keys(this.config).forEach((o=>{t[o]=this.config[o]}))}render(){return null}};export{s as limel_config}
