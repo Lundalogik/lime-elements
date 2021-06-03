e6d660b5161718d99dfe29cc4ccd53ce8f863ac7
Adrian Schmidt <adrian.schmidt@lime.tech>
2021-06-03 10:22:08 +0200

    chore(docs): remove 30.15.2

--- versions/30.15.2/build/limel-example-linear-progress-indeterminate.entry.js
+++
@@ -1,1 +0,0 @@
-import{r as e,h as r}from"./index-9cbaf4f2.js";const s=class{constructor(r){e(this,r)}render(){return r("limel-linear-progress",{indeterminate:!0})}};export{s as limel_example_linear_progress_indeterminate}
