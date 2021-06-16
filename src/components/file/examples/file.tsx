1091a43e314b1fce07686ee2b3139216ad2a8e24
Kiarokh Moattar <kiarokh@icloud.com>
2021-06-16 14:23:37 +0200

    docs(file): improve arrow function expressions

--- src/components/file/examples/file.tsx
+++ src/components/file/examples/file.tsx
@@ -31,23 +31,26 @@ export class FileExample {
                 disabled={this.disabled}
                 readonly={this.readonly}
             />,
-            <limel-flex-container justify="end">
-                <limel-switch
-                    label="Required"
-                    value={this.required}
-                    onChange={this.handleRequiredChange}
-                />
-                <limel-switch
-                    label="Disabled"
-                    value={this.disabled}
-                    onChange={this.handleDisabledChange}
-                />
-                <limel-switch
-                    label="Readonly"
-                    value={this.readonly}
-                    onChange={this.handleReadonlyChange}
-                />
-            </limel-flex-container>,
+            <p>
+                <limel-flex-container justify="end">
+                    <limel-checkbox
+                        checked={this.disabled}
+                        label="Disabled"
+                        onChange={this.setDisabled}
+                    />
+                    <limel-checkbox
+                        checked={this.readonly}
+                        label="Readonly"
+                        onChange={this.setReadonly}
+                    />
+                    <limel-checkbox
+                        checked={this.required}
+                        label="Required"
+                        onChange={this.setRequired}
+                    />
+                </limel-flex-container>
+            </p>,
+            <limel-example-value value={this.value} />,
         ];
     }
 
@@ -56,15 +59,18 @@ export class FileExample {
         console.log('onChange', this.value);
     };
 
-    private handleRequiredChange = (event: CustomEvent<boolean>) => {
+    private setRequired = (event: CustomEvent<boolean>) => {
+        event.stopPropagation();
         this.required = !!event.detail;
     };
 
-    private handleDisabledChange = (event: CustomEvent<boolean>) => {
+    private setDisabled = (event: CustomEvent<boolean>) => {
+        event.stopPropagation();
         this.disabled = !!event.detail;
     };
 
-    private handleReadonlyChange = (event: CustomEvent<boolean>) => {
+    private setReadonly = (event: CustomEvent<boolean>) => {
+        event.stopPropagation();
         this.readonly = !!event.detail;
     };
 }
