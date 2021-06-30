c573a3763012e8cff9c21c26ea3a25b25fc139a2
Befkadu0 <befkassefa@gmail.com>
2021-06-30 17:07:14 +0200

    fix(chip-set): enable setting title attribute to the lime-icon

--- src/components/chip-set/chip-set.tsx
+++ src/components/chip-set/chip-set.tsx
@@ -636,6 +636,7 @@ export class ChipSet {
                 style={style}
                 size="small"
                 badge={true}
+                title={chip.iconTitle}
             />
         );
     }
