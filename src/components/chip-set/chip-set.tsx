3fb6d27385f6c89cc564b32d54bfdfdaef580f07
Adrian Schmidt <adrian.schmidt@lime.tech>
2021-07-12 11:18:17 +0200

    fix(chip-set): prevent tabbing to chips in disabled chip-set

--- src/components/chip-set/chip-set.tsx
+++ src/components/chip-set/chip-set.tsx
@@ -553,7 +553,7 @@ export class ChipSet {
             <span role="gridcell">
                 <a
                     role="button"
-                    tabindex="0"
+                    tabindex={this.disabled ? '-1' : '0'}
                     class="mdc-chip__text"
                     {...attributes}
                 >
@@ -583,7 +583,7 @@ export class ChipSet {
                 <span role="gridcell">
                     <span
                         role="checkbox"
-                        tabindex="0"
+                        tabindex={this.disabled ? '-1' : '0'}
                         aria-checked="false"
                         class="mdc-chip__text"
                     >
@@ -680,6 +680,10 @@ export class ChipSet {
     }
 
     private renderClearAllChipsButton() {
+        if (this.disabled || this.readonly) {
+            return;
+        }
+
         return (
             <a
                 href=""
