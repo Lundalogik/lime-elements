455e1fcea8d1f7bc83694636648b3d6117234c93
Kiarokh Moattar <kiarokh@icloud.com>
2021-09-29 14:34:15 +0200

    perf(linear-progress): remove `buffer-dots` and thus their always running animations
    The `mdc-linear-progress__buffer-dots` is from a feature that
    we do not use. However, their div was present, and their animation
    was always running, affecting the performance.

--- src/components/linear-progress/linear-progress.tsx
+++ src/components/linear-progress/linear-progress.tsx
@@ -72,7 +72,6 @@ export class LinearProgress {
             >
                 <div class="mdc-linear-progress__buffer">
                     <div class="mdc-linear-progress__buffer-bar"></div>
-                    <div class="mdc-linear-progress__buffer-dots"></div>
                 </div>
                 <div class="mdc-linear-progress__bar mdc-linear-progress__primary-bar">
                     <span class="mdc-linear-progress__bar-inner" />
