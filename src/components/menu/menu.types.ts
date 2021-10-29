3934e548ff586d9c3f383bc2f90b0d5d3586a543
Johan Groth <johan.groth@lime.tech>
2021-10-29 11:15:31 +0200

    fix(menu): make MenuItem generic

--- src/components/menu/menu.types.ts
+++ src/components/menu/menu.types.ts
@@ -2,7 +2,7 @@ import { ListItem } from '../list/list-item.types';
 
 export type OpenDirection = 'left' | 'right';
 
-export interface MenuItem extends ListItem {
+export interface MenuItem<T = any> extends ListItem<T> {
     /**
      * The additional supporting text is used for shortcut commands and displayed in the list item.
      */
