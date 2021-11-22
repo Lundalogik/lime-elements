548a9ebee78f79e340932eac14a3e114ad16043f
lime-ci <617355+lime-ci@users.noreply.github.com>
2021-11-22 09:13:00 +0000

    chore(docs): create docs NEXT-34.0.2-next.6

---
+++ versions/NEXT-34.0.2-next.6/build/limel-example-icon-button-elevated.entry.js
@@ -0,0 +1,1 @@
+import{r as e,h as t}from"./index-a7da85d2.js";let l=class{constructor(t){e(this,t)}render(){return t("limel-icon-button",{label:"Add favourite",icon:"heart_outlined",elevated:!0})}};export{l as limel_example_icon_button_elevated}
