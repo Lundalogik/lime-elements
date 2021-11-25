1431bd7d75ced06268d204f8d79a6f30783f5c9c
Kiarokh Moattar <kiarokh@icloud.com>
2021-11-25 16:20:20 +0100

    feat: add `limel-color-picker` component

---
+++ src/components/color-picker/examples/color-picker.tsx
@@ -0,0 +1,25 @@
+import { Component, h, State } from '@stencil/core';
+@Component({
+    tag: 'limel-example-color-picker',
+    shadow: true,
+})
+export class ColorPickerExample {
+    @State()
+    private value: string;
+
+    public render() {
+        return (
+            <limel-color-picker
+                value={this.value}
+                tooltipLabel="Click to pick a color"
+                helperText="You can also type a color name or value to preview it here"
+                label="Chosen color"
+                onChange={this.onChange}
+            />
+        );
+    }
+
+    private onChange = (event: CustomEvent<string>) => {
+        this.value = event.detail;
+    };
+}
