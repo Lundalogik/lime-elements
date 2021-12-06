e5a271983a64c148dd9ff9bda123c3bcbf5044c7
Johannes Eriksson <johannes.eriksson@hyperisland.se>
2021-12-06 14:17:55 +0100

    feat(form): make it possible to customize the default error messages

---
+++ src/components/form/examples/custom-error-message-form.tsx
@@ -0,0 +1,67 @@
+import { ValidationStatus, FormError } from '@limetech/lime-elements';
+import { Component, h, State } from '@stencil/core';
+import { schema } from './custom-error-message-schema';
+
+/**
+ * Form with custom error message
+ *
+ * @link custom-error-message-schema.ts
+ */
+@Component({
+    tag: 'limel-example-custom-error-message',
+    shadow: true,
+})
+export class CustomErrorMessageFormExample {
+    @State()
+    private formData: object = {
+        personalIdentityNumber: '',
+    };
+
+    @State()
+    private valid = true;
+
+    public render() {
+        return [
+            <limel-form
+                onValidate={this.handleFormValidate}
+                onChange={this.handleFormChange}
+                value={this.formData}
+                schema={schema}
+                transformErrors={this.transformErrors}
+            />,
+            <br />,
+            <limel-button
+                label="Submit"
+                primary={true}
+                disabled={!this.valid}
+                onClick={this.handleSubmit}
+            />,
+        ];
+    }
+
+    private handleFormChange = (event) => {
+        this.formData = event.detail;
+    };
+
+    private handleSubmit = () => {
+        const json = JSON.stringify(this.formData, null, '    ');
+        alert(`Sending information to villains...\n\n${json}`);
+    };
+
+    private handleFormValidate = (event: CustomEvent<ValidationStatus>) => {
+        this.valid = event.detail.valid;
+    };
+
+    private transformErrors = (errors: FormError[]): FormError[] => {
+        return errors.map((error) => {
+            if (
+                error.name === 'pattern' &&
+                error.property === '.personalIdentityNumber'
+            ) {
+                error.message = 'Invalid format, use YYYYMMDD-NNNN';
+            }
+
+            return error;
+        });
+    };
+}
--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -9,7 +9,7 @@ import {
 } from '@stencil/core';
 import React from 'react';
 import { render, unmountComponentAtNode } from 'react-dom';
-import JSONSchemaForm from '@rjsf/core';
+import JSONSchemaForm, { AjvError } from '@rjsf/core';
 import retargetEvents from 'react-shadow-dom-retarget-events';
 import { FormError, ValidationStatus } from './form.types';
 import {
@@ -34,6 +34,7 @@ import { isInteger } from './validators';
  * @exampleComponent limel-example-props-factory-form
  * @exampleComponent limel-example-form-layout
  * @exampleComponent limel-example-form-span-fields
+ * @exampleComponent limel-example-custom-error-message
  */
 @Component({
     tag: 'limel-form',
@@ -75,6 +76,12 @@ export class Form {
     public propsFactory?: (schema: Record<string, any>) => Record<string, any>;
 
     /**
+     * Custom function to customize the default error messages
+     */
+    @Prop()
+    public transformErrors?: (errors: FormError[]) => FormError[];
+
+    /**
      * Emitted when a change is made within the form
      */
     @Event()
@@ -96,6 +103,7 @@ export class Form {
 
     public constructor() {
         this.handleChange = this.handleChange.bind(this);
+        this.getCustomErrorMessages = this.getCustomErrorMessages.bind(this);
     }
 
     public connectedCallback() {
@@ -154,6 +162,7 @@ export class Form {
                     ArrayFieldTemplate: ArrayFieldTemplate as any,
                     ObjectFieldTemplate: ObjectFieldTemplate,
                     disabled: this.disabled,
+                    transformErrors: this.getCustomErrorMessages,
                     formContext: {
                         schema: this.modifiedSchema,
                         rootValue: this.value,
@@ -233,5 +242,39 @@ export class Form {
                 schemaPath: error.schemaPath,
             };
         });
+    }
+
+    private getCustomErrorMessages(originalErrors: AjvError[]): AjvError[] {
+        if (!this.transformErrors) {
+            return originalErrors;
+        }
+
+        const errors: FormError[] = originalErrors.map((error: AjvError) => {
+            return {
+                name: error.name,
+                property: error.property,
+                message: error.message,
+                // For some reason 'schemaPath' is missing from the AjvError type definition:
+                // https://github.com/rjsf-team/react-jsonschema-form/issues/2140
+                // eslint-disable-next-line @typescript-eslint/dot-notation
+                schemaPath: error['schemaPath'],
+            };
+        });
+
+        // Use `.call({}, …)` here to bind `this` to an empty object to prevent
+        // the consumer submitted `transformErrors` from getting access to our
+        // component's internals. /Ads
+        return this.transformErrors
+            .call({}, errors)
+            .map((transformedError: FormError) => {
+                const originalError = originalErrors.find((error: AjvError) => {
+                    return transformedError.property === error.property;
+                });
+
+                return {
+                    ...originalError,
+                    message: transformedError.message,
+                };
+            });
     }
 }
