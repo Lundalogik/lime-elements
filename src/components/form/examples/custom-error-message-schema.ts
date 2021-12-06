905d8075d8d46b144801e21c483e2cb0955f467c
Johannes Eriksson <johannes.eriksson@hyperisland.se>
2021-12-06 14:17:55 +0100

    feat(form): make it possible to customize the default error messages

---
+++ src/components/form/examples/custom-error-message-schema.ts
@@ -0,0 +1,14 @@
+export const schema = {
+    title: 'Personal identity number form',
+    description: 'Please enter your personal identity number',
+    type: 'object',
+    required: ['personalIdentityNumber'],
+    properties: {
+        personalIdentityNumber: {
+            type: 'string',
+            title: 'Personal identity number',
+            description: 'Enter your personal identity number',
+            pattern: '[0-9]{8}[-][0-9]{4}',
+        },
+    },
+};
