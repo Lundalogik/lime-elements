be07583b8ba656cb85905634fe4b3318b0260375
lime-ci <617355+lime-ci@users.noreply.github.com>
2021-12-10 09:24:05 +0000

    chore(docs): create docs PR-1495

---
+++ versions/PR-1495/build/limel-example-button-basic.entry.js
@@ -0,0 +1,1 @@
+import{r as e,h as l}from"./index-a7da85d2.js";let t=class{constructor(l){e(this,l)}render(){return l("limel-button",{label:"Click me!",onClick:this.onClick})}onClick(){console.log("Button clicked.")}};export{t as limel_example_button_basic}
