f5859cf996e2561e1b062231cc021667e668e5b2
lime-ci <617355+lime-ci@users.noreply.github.com>
2021-12-29 14:13:56 +0000

    chore(docs): create docs NEXT-34.1.0-next.27

---
+++ versions/NEXT-34.1.0-next.27/build/limel-example-color-picker.entry.js
@@ -0,0 +1,1 @@
+import{r as e,h as o}from"./index-a7da85d2.js";let l=class{constructor(o){e(this,o),this.onChange=e=>{this.value=e.detail}}render(){return o("limel-color-picker",{value:this.value,tooltipLabel:"Click to pick a color",helperText:"You can also type a color name or value to preview it here",label:"Chosen color",onChange:this.onChange})}};export{l as limel_example_color_picker}
