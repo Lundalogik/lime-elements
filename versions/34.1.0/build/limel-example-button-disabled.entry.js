0687a0dd4c517d20fea61e67fa5fe0be3afe37e5
lime-ci <617355+lime-ci@users.noreply.github.com>
2022-01-05 13:22:43 +0000

    chore(docs): create docs 34.1.0

---
+++ versions/34.1.0/build/limel-example-button-disabled.entry.js
@@ -0,0 +1,1 @@
+import{r as e,h as t}from"./index-a7da85d2.js";let l=class{constructor(t){e(this,t)}render(){return t("limel-button",{label:"My Button",disabled:!0,onClick:this.onClick})}onClick(){console.log("This should never happen, since the button is disabled.")}};export{l as limel_example_button_disabled}
