96334df982cdadb63191e34b9facf30321253616
lime-ci <617355+lime-ci@users.noreply.github.com>
2022-01-07 11:33:38 +0000

    chore(docs): create docs PR-1509

---
+++ versions/PR-1509/build/limel-example-collapsible-section.entry.js
@@ -0,0 +1,1 @@
+import{r as e,h as l}from"./index-a7da85d2.js";let s=class{constructor(l){e(this,l)}render(){return l("div",null,l("limel-collapsible-section",{header:"This text becomes the header"},l("p",null,"This element becomes the body.")))}};export{s as limel_example_collapsible_section}
