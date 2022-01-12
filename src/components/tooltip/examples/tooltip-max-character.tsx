4552bc0ccd847074b566742293c217154e16528b
Kiarokh Moattar <kiarokh@icloud.com>
2022-01-12 13:42:43 +0100

    feat(tooltip): break wide tooltips into two rows & allow overriding the break point

---
+++ src/components/tooltip/examples/tooltip-max-character.tsx
@@ -0,0 +1,45 @@
+import { Component, h } from '@stencil/core';
+
+/**
+ * Using 'maxlength' property
+ * To present an easy to read content, the tooltip's maximum text
+ * length is set to 50 characters, including spaces.
+ * When this threshold is reached, content will be rendered with line breaks.
+ * However, it is possible to override this value by specifying `maxlength`.
+ *
+ * :::note
+ * Tooltips are intended to display very brief information.
+ * Try not to place large amount of text in them.
+ * :::
+ */
+@Component({
+    tag: 'limel-example-tooltip-max-character',
+    shadow: true,
+    styleUrl: 'tooltip-max-character.scss',
+})
+export class TooltipExample {
+    public render() {
+        return [
+            <limel-icon-button icon="info" id="tooltip1" />,
+            <limel-tooltip
+                label="Short text"
+                helperLabel="less than 25ch"
+                elementId="tooltip1"
+                maxlength={25}
+            />,
+            <limel-icon-button icon="info" id="tooltip2" />,
+            <limel-tooltip
+                label="Long text"
+                helperLabel="The total amount of characters is more than 25"
+                elementId="tooltip2"
+                maxlength={25}
+            />,
+            <limel-icon-button icon="info" id="tooltip3" />,
+            <limel-tooltip
+                label="Very long text"
+                helperLabel="The total amount of characters is more than default max character length, which is 50ch. Note that there is no max character length specified here."
+                elementId="tooltip3"
+            />,
+        ];
+    }
+}
--- src/components/tooltip/tooltip-content.tsx
+++ src/components/tooltip/tooltip-content.tsx
@@ -15,7 +15,7 @@ export class TooltipContent {
      * Short descriptive text of the owner element.
      */
     @Prop({ reflect: true })
-    label: string;
+    label!: string;
 
     /**
      * Additional helper text for the element.
@@ -23,12 +23,33 @@ export class TooltipContent {
      * owner element.
      */
     @Prop({ reflect: true })
-    helperLabel: string;
+    helperLabel?: string;
+
+    /**
+     * The maximum amount of characters before rendering 'label' and 'helperLabel' in two rows.
+     */
+    @Prop({ reflect: true })
+    maxlength?: number;
 
     public render() {
+        let isLabelsTextLong = false;
+        if (this.helperLabel && this.maxlength) {
+            isLabelsTextLong =
+                this.label.length + this.helperLabel.length > this.maxlength;
+        }
+
+        const props: any = {};
+        if (this.maxlength) {
+            props.style = {
+                '--tooltip-max-width-of-text': `${this.maxlength}` + 'ch',
+            };
+        }
+
         return [
-            <div class="label">{this.label}</div>,
-            <div class="helper-label">{this.helperLabel}</div>,
+            <text class={{ 'has-column-layout': isLabelsTextLong }} {...props}>
+                <div class="label">{this.label}</div>
+                <div class="helper-label">{this.helperLabel}</div>
+            </text>,
         ];
     }
 }
