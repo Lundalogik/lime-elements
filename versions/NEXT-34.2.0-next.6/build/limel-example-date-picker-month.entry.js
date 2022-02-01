f1ef97e14f9832b7226b7954e93643acb4d6d9b6
lime-ci <617355+lime-ci@users.noreply.github.com>
2022-02-01 13:01:16 +0000

    chore(docs): create docs NEXT-34.2.0-next.6

---
+++ versions/NEXT-34.2.0-next.6/build/limel-example-date-picker-month.entry.js
@@ -0,0 +1,1 @@
+import{r as e,h as t}from"./index-a7da85d2.js";let l=class{constructor(t){e(this,t),this.value=new Date,this.handleChange=e=>{this.value=e.detail}}render(){return t("p",null,t("limel-date-picker",{type:"month",label:"month",value:this.value,onChange:this.handleChange}),t("limel-example-value",{value:this.value}))}};export{l as limel_example_date_picker_month}
