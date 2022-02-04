4fe67ba79a9f352ff7f7277e9a6c7d9d49b1e26b
lime-ci <617355+lime-ci@users.noreply.github.com>
2022-02-04 09:41:12 +0000

    chore(docs): create docs PR-1546

---
+++ versions/PR-1546/build/limel-example-date-picker-week.entry.js
@@ -0,0 +1,1 @@
+import{r as e,h as l}from"./index-a7da85d2.js";let t=class{constructor(l){e(this,l),this.value=new Date,this.handleChange=e=>{this.value=e.detail}}render(){return l("p",null,l("limel-date-picker",{type:"week",label:"week",value:this.value,onChange:this.handleChange}),l("limel-example-value",{value:this.value}))}};export{t as limel_example_date_picker_week}
