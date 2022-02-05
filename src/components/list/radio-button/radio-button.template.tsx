1141e04044ba2c402e74536f6630a48ca7c8bc87
Adrian Schmidt <adrian.schmidt@lime.tech>
2022-02-05 19:02:27 +0100

    style(lint): formatting changes by updated prettier

--- src/components/list/radio-button/radio-button.template.tsx
+++ src/components/list/radio-button/radio-button.template.tsx
@@ -8,35 +8,36 @@ interface RadioButtonTemplateProps {
     label?: string;
 }
 
-export const RadioButtonTemplate: FunctionalComponent<RadioButtonTemplateProps> =
-    (props) => {
-        return (
-            <div class="mdc-form-field">
-                <div
-                    class={`
+export const RadioButtonTemplate: FunctionalComponent<
+    RadioButtonTemplateProps
+> = (props) => {
+    return (
+        <div class="mdc-form-field">
+            <div
+                class={`
                         mdc-radio
                         ${props.disabled ? 'mdc-radio--disabled' : ''}
                     `}
-                >
-                    <input
-                        class="mdc-radio__native-control"
-                        type="radio"
-                        id={props.id}
-                        checked={props.checked}
-                        disabled={props.disabled}
-                        onChange={props.onChange}
-                    />
-                    <div class="mdc-radio__background">
-                        <div class="mdc-radio__outer-circle" />
-                        <div class="mdc-radio__inner-circle" />
-                    </div>
+            >
+                <input
+                    class="mdc-radio__native-control"
+                    type="radio"
+                    id={props.id}
+                    checked={props.checked}
+                    disabled={props.disabled}
+                    onChange={props.onChange}
+                />
+                <div class="mdc-radio__background">
+                    <div class="mdc-radio__outer-circle" />
+                    <div class="mdc-radio__inner-circle" />
                 </div>
-                <label
-                    class={`${props.disabled ? 'disabled' : ''}`}
-                    htmlFor={props.id}
-                >
-                    {props.label}
-                </label>
             </div>
-        );
-    };
+            <label
+                class={`${props.disabled ? 'disabled' : ''}`}
+                htmlFor={props.id}
+            >
+                {props.label}
+            </label>
+        </div>
+    );
+};
