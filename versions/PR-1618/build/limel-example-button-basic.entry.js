163e7521ad9d5e69a479e19684ba0eab8b6f8acd
lime-ci <617355+lime-ci@users.noreply.github.com>
2022-02-16 13:51:40 +0000

    chore(docs): remove PR-1618

--- versions/PR-1618/build/limel-example-button-basic.entry.js
+++
@@ -1,1 +0,0 @@
-import{r as e,h as l}from"./index-11aed7da.js";let t=class{constructor(l){e(this,l)}render(){return l("limel-button",{label:"Click me!",onClick:this.onClick})}onClick(){console.log("Button clicked.")}};export{t as limel_example_button_basic}
