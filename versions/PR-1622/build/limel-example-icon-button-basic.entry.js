675eb01cfe940971955a5ad72406bf69ba2a0684
lime-ci <617355+lime-ci@users.noreply.github.com>
2022-02-17 11:01:43 +0000

    chore(docs): create docs PR-1622

---
+++ versions/PR-1622/build/limel-example-icon-button-basic.entry.js
@@ -0,0 +1,1 @@
+import{r as e,h as o}from"./index-11aed7da.js";let t=class{constructor(o){e(this,o)}render(){return[o("limel-icon-button",{label:"Add favourite",icon:"heart_outlined",onClick:this.onClick})]}onClick(){console.log("Button clicked.")}};export{t as limel_example_icon_button_basic}
