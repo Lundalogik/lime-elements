4d53a7bf2b1c8c358894f8963fb43402ea599813
lime-ci <617355+lime-ci@users.noreply.github.com>
2022-02-18 11:03:40 +0000

    chore(docs): create docs PR-1627

---
+++ versions/PR-1627/build/limel-example-list.entry.js
@@ -0,0 +1,1 @@
+import{r as e,h as t}from"./index-11aed7da.js";let a=class{constructor(t){e(this,t),this.items=[{text:"King of Tokyo",value:1},{text:"Smash Up!",value:2},{text:"Pandemic",value:3},{separator:!0},{text:"Catan",value:4},{text:"Ticket to Ride",value:5}]}render(){return t("limel-list",{items:this.items})}};export{a as limel_example_list}
