633eba877525a73e2cfbfb79e79d1ebc5a92d675
Adrian Schmidt <adrian.schmidt@lime.tech>
2022-03-30 09:40:57 +0200

    chore(lint): remove now unnecessary disable-comments for `no-shadow`

--- src/components/form/fields/schema-field.ts
+++ src/components/form/fields/schema-field.ts
@@ -72,7 +72,6 @@ export function getFactoryProps(
     formContext: any,
     schema: any
 ): Record<string, any> {
-    // eslint-disable-next-line no-shadow
     const factory: (schema: any) => Record<string, any> =
         formContext.propsFactory;
     if (typeof factory !== 'function') {
