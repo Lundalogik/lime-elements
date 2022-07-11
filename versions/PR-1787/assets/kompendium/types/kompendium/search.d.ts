e089ea68d1d1e26eae3e929f3c07faa164fedbf6
lime-ci <617355+lime-ci@users.noreply.github.com>
2022-07-11 10:01:41 +0000

    chore(docs): remove PR-1787

--- versions/PR-1787/assets/kompendium/types/kompendium/search.d.ts
+++
@@ -1,3 +0,0 @@
-import { KompendiumData, KompendiumDocument, MenuItem } from '../types';
-export declare function createIndex(data: KompendiumData): any;
-export declare function createDocuments(items: MenuItem[], data: KompendiumData): Generator<KompendiumDocument>;
