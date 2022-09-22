3f1c53c1d2fbef46f1fc23c21ef1e617e672a2a2
lime-ci <617355+lime-ci@users.noreply.github.com>
2022-09-22 09:27:36 +0000

    chore(docs): create docs PR-1840

---
+++ versions/PR-1840/assets/kompendium/types/components/component/templates/methods.d.ts
@@ -0,0 +1,5 @@
+import { JsonDocsMethod } from '../../../stencil-public-runtime';
+export declare function MethodList({ methods, id, }: {
+  id?: string;
+  methods: Array<Partial<JsonDocsMethod>>;
+}): HTMLElement[];
