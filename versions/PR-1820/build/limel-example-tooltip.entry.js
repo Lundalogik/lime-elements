6c08f52649601e5f0a1fd4fef9b9e81bf93b7be9
lime-ci <617355+lime-ci@users.noreply.github.com>
2022-09-23 17:52:19 +0000

    chore(docs): create docs PR-1820

--- versions/PR-1820/build/limel-example-tooltip.entry.js
+++ versions/PR-1820/build/limel-example-tooltip.entry.js
@@ -1,1 +1,1 @@
-import{r as e,h as l}from"./index-7dccb886.js";const t=class{constructor(l){e(this,l)}render(){return[l("limel-button",{icon:"search",id:"tooltip-example"}),l("limel-tooltip",{label:"Search",helperLabel:"alt + F",elementId:"tooltip-example"})]}};export{t as limel_example_tooltip}
+import{r as e,h as l}from"./index-ab490ba1.js";const t=class{constructor(l){e(this,l)}render(){return[l("limel-button",{icon:"search",id:"tooltip-example"}),l("limel-tooltip",{label:"Search",helperLabel:"alt + F",elementId:"tooltip-example"})]}};export{t as limel_example_tooltip}
