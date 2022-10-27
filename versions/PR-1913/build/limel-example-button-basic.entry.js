6c0f8dbfb8105cee7631bca1a282ba43cd57b4b0
adrianschmidt <adrianschmidt@users.noreply.github.com>
2022-10-27 17:57:24 +0000

    chore(docs): create docs PR-1913

--- versions/PR-1913/build/limel-example-button-basic.entry.js
+++ versions/PR-1913/build/limel-example-button-basic.entry.js
@@ -1,1 +1,1 @@
-import{r as t,h as e}from"./index-ab490ba1.js";const l=class{constructor(e){t(this,e)}render(){return e("limel-button",{label:"Click me!",onClick:this.onClick})}onClick(){console.log("Button clicked.")}};export{l as limel_example_button_basic}
+import{r as t,h as e}from"./index-690c5d75.js";const l=class{constructor(e){t(this,e)}render(){return e("limel-button",{label:"Click me!",onClick:this.onClick})}onClick(){console.log("Button clicked.")}};export{l as limel_example_button_basic}
