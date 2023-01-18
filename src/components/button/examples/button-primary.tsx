812b4d4c40ad9d8493ba839ddab6498bd74a7107
Kiarokh Moattar <kiarokh@icloud.com>
2023-01-18 13:44:55 +0100

    docs(button): add guidelines for usage of `primary` & `outlined` styles
    Also correct the order of examples.

--- src/components/button/examples/button-primary.tsx
+++ src/components/button/examples/button-primary.tsx
@@ -2,6 +2,19 @@ import { Component, h } from '@stencil/core';
 
 /**
  * Primary
+ *
+ * Each screen (modal, or section with action buttons)
+ * should contain a single prominent button like this one,
+ * to emphasize the primary action.
+ *
+ * :::note
+ * Think twice before setting `primary={true}` on buttons.
+ * The arrangement of buttons and their colors should clearly
+ * communicate their importance and primariness or secondariness.
+ *
+ * See some examples at [our design guidelines for
+ * Action buttons](#/DesignGuidelines/action-buttons.md/).
+ * :::
  */
 @Component({
     tag: 'limel-example-button-primary',
