169915f69e3f97c369adad9b7ba647f08bdf5b1e
Kiarokh <Kiarokh@users.noreply.github.com>
2023-01-20 12:38:09 +0000

    chore(docs): remove undefined

--- versions/NEXT-36.3.0-next.6/build/limel-example-input-field-pattern.entry.js
+++
@@ -1,1 +0,0 @@
-import{r as t,h as e}from"./index-a55db97c.js";const i=class{constructor(e){t(this,e),this.handleChange=t=>{this.value=t.detail},this.value=void 0}render(){return e("limel-input-field",{label:"Personal identity number (YYYYMMDD-XXXX)",value:this.value,pattern:"[0-9]{8}[-][0-9]{4}",onChange:this.handleChange})}};export{i as limel_example_input_field_pattern}
