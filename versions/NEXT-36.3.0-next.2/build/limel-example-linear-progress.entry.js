45b3b3b6291805b6e319bcb4fbd044d22158b579
Kiarokh <Kiarokh@users.noreply.github.com>
2023-01-20 08:43:24 +0000

    chore(docs): remove undefined

--- versions/NEXT-36.3.0-next.2/build/limel-example-linear-progress.entry.js
+++
@@ -1,1 +0,0 @@
-import{r as e,h as l}from"./index-a55db97c.js";const s=class{constructor(l){e(this,l),this.handleChange=e=>{this.value=+e.detail/100},this.value=.7}render(){return[l("limel-input-field",{label:"Value",type:"number",value:(100*this.value).toFixed(0),onChange:this.handleChange}),l("p",null,l("limel-linear-progress",{value:this.value}))]}};export{s as limel_example_linear_progress}
