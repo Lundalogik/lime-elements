32adb48db85c41a221e17650ad60477d3859726c
lime-ci <617355+lime-ci@users.noreply.github.com>
2023-02-10 12:36:04 +0000

    chore(docs): remove PR-2118

--- versions/PR-2118/assets/kompendium/types/components/component/templates/examples.d.ts
+++
@@ -1,7 +0,0 @@
-import { JsonDocsComponent, JsonDocsTag } from '../../../stencil-public-runtime';
-export declare function ExampleList({ examples, id, schema, }: {
-  id: string;
-  examples: JsonDocsComponent[];
-  schema: Record<string, any>;
-}): HTMLElement[];
-export declare const isExampleTag: (name: string) => (tag: JsonDocsTag) => boolean;
