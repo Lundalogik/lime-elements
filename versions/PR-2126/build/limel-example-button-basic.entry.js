7ca9f33b8f0409ead873b05f94f14853fc048cc7
adrianschmidt <adrianschmidt@users.noreply.github.com>
2023-02-14 09:16:49 +0000

    chore(docs): create docs PR-2126

---
+++ versions/PR-2126/build/limel-example-button-basic.entry.js
@@ -0,0 +1,1 @@
+import{r as t,h as e}from"./index-a55db97c.js";const l=class{constructor(e){t(this,e)}render(){return e("limel-button",{label:"Click me!",onClick:this.onClick})}onClick(){console.log("Button clicked.")}};export{l as limel_example_button_basic}
