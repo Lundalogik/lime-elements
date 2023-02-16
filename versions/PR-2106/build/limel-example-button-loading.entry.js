365b157d6b567c60c727413304b3bf1922d7ceec
lime-ci <617355+lime-ci@users.noreply.github.com>
2023-02-16 15:31:06 +0000

    chore(docs): remove PR-2106

--- versions/PR-2106/build/limel-example-button-loading.entry.js
+++
@@ -1,1 +0,0 @@
-import{r as t,h as e}from"./index-a55db97c.js";const l=class{constructor(e){t(this,e)}render(){return e("limel-button",{label:"My Button",disabled:!0,loading:!0})}};export{l as limel_example_button_loading}
