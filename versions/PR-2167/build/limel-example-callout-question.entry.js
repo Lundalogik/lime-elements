81f2f78e2c6d747e6416eaf9547406f372e2430b
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2023-03-31 17:21:13 +0000

    chore(docs): create docs PR-2167

--- versions/PR-2167/build/limel-example-callout-question.entry.js
+++
@@ -1,1 +0,0 @@
-import{r as e,h as t}from"./index-a55db97c.js";const s=class{constructor(t){e(this,t)}render(){return t("limel-callout",{type:"question"},"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et euismod nulla. Curabitur feugiat, tortor non consequat finibus, justo purus auctor massa, nec semper lorem quam in massa.")}};export{s as limel_example_callout_question}
