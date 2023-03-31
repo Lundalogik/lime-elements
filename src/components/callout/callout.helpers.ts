de46de20e80a8e2b385802b60818583930a068a5
Adrian Schmidt <adrian.schmidt@lime.tech>
2023-03-31 17:07:22 +0200

    fixup! feat(callout): add new component

--- src/components/callout/callout.helpers.ts
+++ src/components/callout/callout.helpers.ts
@@ -9,7 +9,7 @@ const calloutIcons: Record<string, string> = {
 };
 
 export function getIcon(type: string = 'info'): string {
-    return calloutIcons[type] ?? calloutIcons.info;
+    return calloutIcons[type] ?? calloutIcons.note;
 }
 
 export function getHeading(
