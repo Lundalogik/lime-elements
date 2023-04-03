16aa45db128bcde2492c31ea83cb7022989c20be
Adrian Schmidt <adrian.schmidt@lime.tech>
2023-04-03 13:14:52 +0200

    Revert "fixup! feat(callout): add new component"
    This reverts commit 5c78618a75b7ae7cd269d6d98d71ac2f6cf538fb.
    
    Just reformatted where the line breaks are in the description.

--- src/components/callout/callout.tsx
+++ src/components/callout/callout.tsx
@@ -4,20 +4,21 @@ import { getHeading, getIcon } from './callout.helpers';
 import { Languages } from '@limetech/lime-elements';
 
 /**
- * Callouts—also known as Admonitions—are useful for including supportive or
- * special content within a large piece of text, or even inside a user
- * interface.
+ * Callouts—also known as Admonitions—are useful for including supportive
+ * or special content within a large piece of text,
+ * or even inside a user interface.
  *
  * When used in a document or text based user interface, the callout attracts
- * the reader's attention to a particular piece of information, without
- * significantly interrupting their flow of reading the document.
+ * the reader's attention to a particular piece of information,
+ * without significantly interrupting their flow of reading the document.
  *
- * In a user interface, a callout is more intrusive to the end-user. Still, it
- * could be a good choice when you intend to slightly disturb the user's
- * attention, and challenge them to pay extra attention to the information
- * presented. In such cases, a callout should not be used as a static and
- * constantly present element of the UI. Rather, it should be displayed when
- * something unusual or remarkable demands the user's attention.
+ * In a user interface, a callout is more intrusive to the
+ * end-user. Still, it could be a good choice when you intend to slightly
+ * disturb the user's attention, and challenge them to pay extra attention
+ * to the information presented. In such cases, a callout should not
+ * be used as a static and constantly present element of the UI. Rather, it should
+ * be displayed when something unusual or remarkable demands
+ * the user's attention.
  *
  * @exampleComponent limel-example-callout-note
  * @exampleComponent limel-example-callout-important
