769deb77ee4c5f9e58a12a1d58aa93af227fcb12
Kiarokh Moattar <kiarokh@icloud.com>
2023-05-02 16:48:31 +0200

    docs(snackbar): add basic usage guidelines

--- src/components/snackbar/examples/snackbar.tsx
+++ src/components/snackbar/examples/snackbar.tsx
@@ -2,6 +2,19 @@ import { Component, Element, h, State } from '@stencil/core';
 
 /**
  * Basic example
+ *
+ * Snackbars should not necessarily require a deliberate action from the user to dismiss them.
+ * This is why the component has a default `timeout` and will disappear even if the user
+ * does not interact with it.
+ *
+ * As you see in this example, there is no `timeout` specified.
+ * Therefore the snackbar will automatically disappear after a few seconds.
+ *
+ * :::important
+ * Make sure to set a proper timeout, based on the length of the text.
+ * An average user must be able to read the full message within the given
+ * time!
+ * :::
  */
 @Component({
     tag: 'limel-example-snackbar',
