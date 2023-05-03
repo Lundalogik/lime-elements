f91dce9902f469bea72de03f1a543df50607e0b1
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2023-05-03 08:01:57 +0000

    chore(docs): remove undefined

--- versions/NEXT-36.3.1-next.1/build/limel-example-input-field-suffix.entry.js
+++
@@ -1,1 +0,0 @@
-import{r as i,h as t}from"./index-a55db97c.js";const e=class{constructor(t){i(this,t),this.handleChange=i=>{this.value=i.detail},this.value="50"}render(){return t("limel-input-field",{label:"Quantity",suffix:"pcs",value:this.value,type:"number",onChange:this.handleChange})}};export{e as limel_example_input_field_suffix}
