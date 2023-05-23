537aa0f41326b55859622f3e06cd13e022b326e2
Lucy Chyzhova <lucy.chyzhova@lime.tech>
2023-05-23 10:31:25 +0200

    feat(action-bar): add new component
    Fixes Lundalogik/crm-feature#3379
    Co-authored-by: Kiarokh Moattar <kiarokh@icloud.com>
    Co-authored-by: Adrian Schmidt <adrian.schmidt@lime.tech>
    Co-authored-by: Per Fryking <per.fryking@lime.tech>
    Co-authored-by: Laurens Borst <laurens.borst@lime.tech>
    Co-authored-by: Johan Groth <johan.groth@lime.tech>

---
+++ src/components/action-bar/action-bar-item/action-bar-overflow-menu.tsx
@@ -0,0 +1,65 @@
+import { Component, Prop, h, Event, EventEmitter } from '@stencil/core';
+import {
+    MenuItem,
+    LimelMenuCustomEvent,
+    ListSeparator,
+    ActionBarItem,
+    OpenDirection,
+} from '@limetech/lime-elements';
+
+/**
+ * @private
+ */
+@Component({
+    tag: 'limel-action-bar-overflow-menu',
+    shadow: false,
+})
+export class ActionBarOverflowMenu {
+    /**
+     * List of the items that should be rendered in the overflow menu.
+     */
+    @Prop()
+    public items: Array<MenuItem | ListSeparator>;
+
+    /**
+     * Defines the location that the content of the overflow menu
+     * appears, in relation to its trigger.
+     * It defaults to `bottom-end`, since in normal scenarios
+     * (for example when the action bar is not floating at the bottom of the screen)
+     * this menu is the right-most item in the user interface of the component.
+     */
+    @Prop({ reflect: true })
+    public openDirection: OpenDirection = 'bottom-end';
+
+    @Event()
+    public select: EventEmitter<ActionBarItem>;
+
+    public render() {
+        return [
+            <limel-menu
+                openDirection={this.openDirection}
+                items={this.items}
+                onSelect={this.handleSelect}
+            >
+                <button slot="trigger">{this.countOverflowedItems()}</button>
+            </limel-menu>,
+        ];
+    }
+
+    private countOverflowedItems = () => {
+        return `+${this.numberOfMenuItems}`;
+    };
+
+    private handleSelect = (event: LimelMenuCustomEvent<MenuItem>) => {
+        event.stopPropagation();
+        this.select.emit(event.detail);
+    };
+
+    private get numberOfMenuItems() {
+        return this.items.filter((item) => this.isMenuItem(item)).length;
+    }
+
+    private isMenuItem(item: MenuItem | ListSeparator): item is MenuItem {
+        return !('separator' in item);
+    }
+}
