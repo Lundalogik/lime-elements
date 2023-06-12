bdaf181b0dc656c66226b8e639e14aceb932414b
Johan Groth <johan.groth@lime.tech>
2023-06-12 12:48:04 +0200

    feat(icon): use browser request cache to cache icons
    The request cache is more persistent than the in-memory implementation we have today. Icons should
    now only be cached once until the cache is cleared, instead of once per page load

--- src/global/icon-cache.ts
+++ src/global/icon-cache.ts
@@ -1,13 +1,16 @@
+const CACHE_NAME = '@limetech/lime-elements/icons';
+
 export class IconCache {
     /*
      * Cache of all loaded SVGs
      */
-    private cache = {};
+    private cache: Promise<Cache>;
 
-    /*
-     * Contains resolve functions for all unresolved promises that are waiting for SVG data.
-     */
-    private resolveFunctions = {};
+    private promises: Record<string, Promise<void>> = {};
+
+    constructor() {
+        this.cache = caches.open(CACHE_NAME);
+    }
 
     /**
      * Get icon data from the cache
@@ -15,39 +18,34 @@ export class IconCache {
      * @param {string} path path on the server where the assets are located
      * @returns {string} svg markup
      */
-    public async get(name, path = '') {
-        if (!this.cache[name]) {
-            this.cache[name] = await this.getIcon(name, path);
+    public async get(name: string, path: string = ''): Promise<string> {
+        const cache = await this.cache;
+        const url = this.getUrl(name, path);
+
+        let response = await cache.match(url);
+        if (!response) {
+            response = await this.fetchData(url, cache);
         }
 
-        return this.cache[name];
+        return this.getIcon(response);
+    }
+
+    private async fetchData(url: string, cache: Cache): Promise<Response> {
+        let requestPromise = this.promises[url];
+        if (requestPromise === undefined) {
+            requestPromise = cache.add(url);
+            this.promises[url] = requestPromise;
+        }
+
+        await requestPromise;
+
+        return cache.match(url);
     }
 
     /*
-     * Creates and returns a promise that will be resolved when SVG data is available
+     * Get icon data from a response
      */
-    private getIcon(name, path) {
-        return new Promise((resolve) => {
-            if (!this.resolveFunctions[name]) {
-                this.resolveFunctions[name] = [];
-                this.fetchData(name, path);
-            }
-
-            this.resolveFunctions[name].push(resolve);
-        });
-    }
-
-    /*
-     * Fetch SVG data from the server
-     */
-    private async fetchData(name, path) {
-        let iconPath = path || '';
-        if (path && !path.endsWith('/')) {
-            iconPath = `${path}/`;
-        }
-
-        const response = await fetch(`${iconPath}assets/icons/${name}.svg`);
-
+    private async getIcon(response: Response): Promise<string> {
         let svgData = await response.text();
 
         // Some of the icons in the Icons8 library have hard coded black color on some of the paths.
@@ -58,7 +56,7 @@ export class IconCache {
             throw new Error('Invalid SVG');
         }
 
-        this.resolvePromises(name, svgData);
+        return svgData;
     }
 
     /*
@@ -71,19 +69,17 @@ export class IconCache {
         return svgDoc.documentElement.tagName.toLowerCase() === 'svg';
     }
 
-    /*
-     * Resolve all promises waiting for data for a specific icon
-     */
-    private resolvePromises(name, svgData) {
-        const resolves = this.resolveFunctions[name];
-        resolves.forEach((resolve) => {
-            resolve(svgData);
-        });
-        this.resolveFunctions[name] = null;
+    private getUrl(name: string, path: string): string {
+        let iconPath = path || '';
+        if (path && !path.endsWith('/')) {
+            iconPath = `${path}/`;
+        }
+
+        return `${iconPath}assets/icons/${name}.svg`;
     }
 }
 
-const cache = new IconCache();
+const iconCache = new IconCache();
 export default (() => {
-    return cache;
+    return iconCache;
 })();
