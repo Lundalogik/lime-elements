60578ba0501c9a9cd8d0de10bd1bbfbc207c49af
Adrian Schmidt <adrian.schmidt@lime.tech>
2023-06-27 09:39:39 +0200

    chore: update StencilJS from v2 to v3
    - stencil.config.ts:
      Remove redundant config option `sourceMap`.
      This option already defaults to `true` in Stencil v3.
    
    - interface.d.ts:
      Export only what is not already exported from components.d.ts.
    
    BREAKING CHANGE:
    We have updated StencilJS to v3. In this new version of Stencil,
    `input` events has been strongly typed. This means that your event
    handlers for `input` events may need to be updated.
    
    Example:
    
    ```diff
    -    private handleInput = (event: CustomEvent<string>) => {
    -        this.textValue = event.detail;
    +    private handleInput = (
    +        event: LimelChipSetCustomEvent<string> | InputEvent
    +    ) => {
    +        if (event instanceof CustomEvent) {
    +            this.textValue = event.detail;
    +        }
         };
    ```
    
    Here, we not only update the type of the custom event we want to
    listen for from the generic `CustomEvent<string>` to the more
    specific `LimelChipSetCustomEvent<string>`, we also update the
    type of the event to include the type of the native `input` event.
    We then check the actual type of the event, and act on it only if
    it is the kind we are interested in (`CustomEvent`).
    
    This is an improvement of the event handler, since the earlier form
    would have caught any native `input` events too, and tried to act
    on it, with unexpected results.

--- src/interface.d.ts
+++ src/interface.d.ts
@@ -1,24 +1,15 @@
 // Components interfaces
-export * from './global/shared-types/link.types';
 export * from './components/button/button.types';
-export * from './components/chip-set/chip.types';
 export * from './components/collapsible-section/action';
-export * from './components/date-picker/date.types';
-export * from './components/dialog/dialog.types';
-export * from './components/file/file.types';
-export * from './components/flex-container/flex-container.types';
 export * from './components/form/form.types';
 export * from './components/icon/icon.types';
 export * from './components/input-field/input-field.types';
-export * from './components/list/list-item.types';
-export * from './components/list/list.types';
-export * from './components/menu/menu.types';
-export * from './components/menu-list/menu-list.types';
-export * from './components/picker/searcher.types';
+export {
+    ListSeparator,
+    ListComponent,
+} from './components/list/list-item.types';
+export { OpenDirection } from './components/menu/menu.types';
 export * from './components/progress-flow/progress-flow.types';
-export * from './components/select/option.types';
-export * from './components/spinner/spinner.types';
-export * from './components/tab-bar/tab.types';
 export * from './components/tab-panel/tab-panel.types';
 export * from './components/table/table.types';
 export * from './components/dock/dock.types';
