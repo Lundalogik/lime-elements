0d3dd1a6fdda952a47d64dcc4dd9586f7795744e
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2023-07-10 08:31:53 +0000

    chore(docs): remove undefined

--- versions/NEXT-37.0.0-next.1/build/limel-example-callout-caution.entry.js
+++
@@ -1,2 +0,0 @@
-import{r as t,h as e}from"./index-2626b3b7.js";const o=class{constructor(e){t(this,e)}render(){return e("limel-callout",{type:"caution"},"I hope you read this.")}};export{o as limel_example_callout_caution};
-//# sourceMappingURL=limel-example-callout-caution.entry.js.map
