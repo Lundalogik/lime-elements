a1b708d842bae48f8a9f46488abf3377b784038d
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2023-07-21 12:57:57 +0000

    chore(docs): create docs NEXT-37.1.0-next.6

---
+++ versions/NEXT-37.1.0-next.6/build/limel-example-tooltip.entry.js
@@ -0,0 +1,2 @@
+import{r as e,h as l}from"./index-6156b4fd.js";const t=class{constructor(l){e(this,l)}render(){return[l("limel-button",{icon:"search",id:"tooltip-example"}),l("limel-tooltip",{label:"Search",helperLabel:"alt + F",elementId:"tooltip-example"})]}};export{t as limel_example_tooltip};
+//# sourceMappingURL=limel-example-tooltip.entry.js.map
