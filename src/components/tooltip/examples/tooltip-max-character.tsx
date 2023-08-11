754be1458c134c2cf5aea6be36c57fca32132106
Adrian Schmidt <adrian.schmidt@lime.tech>
2023-08-11 10:16:10 +0200

    chore(tooltip): make tooltip position relative to target element rather than tooltip host element
    fix: #1408

--- src/components/tooltip/examples/tooltip-max-character.tsx
+++ src/components/tooltip/examples/tooltip-max-character.tsx
@@ -1,7 +1,7 @@
 import { Component, h } from '@stencil/core';
 
 /**
- * Using 'maxlength' property
+ * Using `maxlength` property
  * To present an easy to read content, the tooltip's maximum text
  * length is set to 50 characters, including spaces.
  * When this threshold is reached, content will be rendered with line breaks.
@@ -21,20 +21,20 @@ export class TooltipExample {
     public render() {
         return [
             <limel-icon-button icon="info" id="tooltip1" />,
+            <limel-icon-button icon="info" id="tooltip2" />,
+            <limel-icon-button icon="info" id="tooltip3" />,
             <limel-tooltip
                 label="Short text"
                 helperLabel="less than 25ch"
                 elementId="tooltip1"
                 maxlength={25}
             />,
-            <limel-icon-button icon="info" id="tooltip2" />,
             <limel-tooltip
                 label="Long text"
                 helperLabel="The total amount of characters is more than 25"
                 elementId="tooltip2"
                 maxlength={25}
             />,
-            <limel-icon-button icon="info" id="tooltip3" />,
             <limel-tooltip
                 label="Very long text"
                 helperLabel="The total amount of characters is more than default max character length, which is 50ch. Note that there is no max character length specified here."
