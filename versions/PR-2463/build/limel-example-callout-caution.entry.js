f64472d9fa2b24b67e06251a8c68ff19920df7b5
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2023-08-29 14:41:01 +0000

    chore(docs): remove PR-2463

--- versions/PR-2463/build/limel-example-callout-caution.entry.js
+++
@@ -1,2 +0,0 @@
-import{r as t,h as e}from"./index-6156b4fd.js";const o=class{constructor(e){t(this,e)}render(){return e("limel-callout",{type:"caution"},"I hope you read this.")}};export{o as limel_example_callout_caution};
-//# sourceMappingURL=limel-example-callout-caution.entry.js.map
