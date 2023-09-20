dfda46666d7ca8767dc6ba20954eae903015a78d
Johan Groth <johan.groth@lime.tech>
2023-09-20 12:56:18 +0200

    fix(form): make form compatible with Ajv version 8
    fix Lundalogik/crm-feature#3625

--- src/components/form/validators/format.ts
+++ src/components/form/validators/format.ts
@@ -1,3 +1,7 @@
 export function isInteger(data: string): boolean {
     return Number.isInteger(Number(data));
 }
+
+export function isNumber(data: string): boolean {
+    return Number.isFinite(Number(data));
+}
