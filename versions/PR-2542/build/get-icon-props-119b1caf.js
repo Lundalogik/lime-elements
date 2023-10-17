bb60511df4054a30b409d9d7d459de51863674b8
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2023-10-17 11:33:15 +0000

    chore(docs): create docs PR-2542

--- versions/PR-2542/build/get-icon-props-119b1caf.js
+++ versions/PR-2542/build/get-icon-props-119b1caf.js
@@ -1,2 +1,2 @@
 function r(r){if(typeof r==="string"){return r}return r.name}function t(r,t){if(typeof r==="string"){return t}return r.color}export{t as a,r as g};
-//# sourceMappingURL=get-icon-props-6e5204de.js.map
+//# sourceMappingURL=get-icon-props-119b1caf.js.map
