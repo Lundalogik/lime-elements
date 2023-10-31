da2b16f415a731f937c50f77f0c147cb9a8b220a
Adrian Schmidt <adrian.schmidt@lime.tech>
2023-10-31 13:28:02 +0100

    chore(docs): remove PR-1152

--- versions/PR-1152/assets/kompendium/types/components/component/templates/examples.d.ts
+++
@@ -1,6 +0,0 @@
-import { JsonDocsComponent, JsonDocsTag } from '../../../stencil-public-runtime';
-export declare function ExampleList({ examples, id, }: {
-    id: string;
-    examples: JsonDocsComponent[];
-}): HTMLElement[];
-export declare const isExampleTag: (name: string) => (tag: JsonDocsTag) => boolean;
