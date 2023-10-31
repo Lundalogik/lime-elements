fab6f2ad9675465826264da34444e9ba8be97562
Adrian Schmidt <adrian.schmidt@lime.tech>
2023-10-31 13:51:38 +0100

    chore(docs): remove PR-1*

--- versions/PR-1318/build/limel-example-date-picker-quarter.entry.js
+++
@@ -1,1 +0,0 @@
-import{r as e,h as t}from"./index-64ccb4a0.js";const r=class{constructor(t){e(this,t),this.value=new Date,this.handleChange=e=>{this.value=e.detail}}render(){return t("p",null,t("limel-date-picker",{type:"quarter",label:"quarter",value:this.value,onChange:this.handleChange}),t("limel-example-value",{value:this.value}))}};export{r as limel_example_date_picker_quarter}
--- versions/PR-1547/build/table.types-4d3e33d9.js
+++
@@ -1,1 +0,0 @@
-var a;!function(a){a.Average="avg",a.Maximum="max",a.Minimum="min",a.Sum="sum",a.Count="count"}(a||(a={}));export{a as C}
