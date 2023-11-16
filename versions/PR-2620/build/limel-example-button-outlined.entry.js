e57a66a2a27ee5866dd20c904d1130239289c35b
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2023-11-16 16:23:37 +0000

    chore(docs): create docs PR-2620

---
+++ versions/PR-2620/build/limel-example-button-outlined.entry.js
@@ -0,0 +1,2 @@
+import{r as t,h as e}from"./index-6156b4fd.js";const o=class{constructor(e){t(this,e)}render(){return e("limel-button",{label:"My Button",outlined:true})}};export{o as limel_example_button_outlined};
+//# sourceMappingURL=limel-example-button-outlined.entry.js.map
