825da4897592b0ba572e1f1631af857bed136861
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2024-01-02 11:10:13 +0000

    chore(docs): create docs NEXT-37.1.0-next.79

---
+++ versions/NEXT-37.1.0-next.79/assets/kompendium/types/components/component/templates/examples.d.ts
@@ -0,0 +1,9 @@
+import { JsonDocsComponent, JsonDocsTag } from '../../../stencil-public-runtime';
+import { PropsFactory } from '../../playground/playground.types';
+export declare function ExampleList({ examples, id, schema, propsFactory, }: {
+  id: string;
+  examples: JsonDocsComponent[];
+  schema: Record<string, any>;
+  propsFactory?: PropsFactory;
+}): HTMLElement[];
+export declare const isExampleTag: (name: string) => (tag: JsonDocsTag) => boolean;
