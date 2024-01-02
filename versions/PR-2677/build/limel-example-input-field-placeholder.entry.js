37edd86fdb79648afcd2c9a8d74335703b8380f9
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2024-01-02 10:37:14 +0000

    chore(docs): create docs PR-2677

---
+++ versions/PR-2677/build/limel-example-input-field-placeholder.entry.js
@@ -0,0 +1,2 @@
+import{r as e,h as t}from"./index-6156b4fd.js";const r=class{constructor(t){e(this,t);this.handleChange=e=>{this.value=e.detail};this.value=undefined}render(){return t("limel-input-field",{label:"Your personal identity number",placeholder:"example: 19990101-1234",helperText:"Use correct format (12 digits, and a dash after your birth date)",value:this.value,onChange:this.handleChange})}};export{r as limel_example_input_field_placeholder};
+//# sourceMappingURL=limel-example-input-field-placeholder.entry.js.map
