4bd5ae4eec4987a1e2d8433fab723a539118820d
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2024-01-12 15:15:28 +0000

    chore(docs): create docs PR-2707

---
+++ versions/PR-2707/build/limel-example-chip-badge.entry.js
@@ -0,0 +1,2 @@
+import{r as a,h as e}from"./index-6156b4fd.js";const t=class{constructor(e){a(this,e)}render(){return[e("limel-chip",{id:"a",text:"Batman",icon:"batman_old",badge:2005}),e("span",null," "),e("limel-chip",{id:"b",text:"Batman",icon:"batman_new",badge:"NEW"})]}};export{t as limel_example_chip_badge};
+//# sourceMappingURL=limel-example-chip-badge.entry.js.map
