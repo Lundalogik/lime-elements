f0a1af8d47bf357925792e292a8b6d57377d4827
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2024-01-23 14:03:40 +0000

    chore(docs): create docs PR-2733

---
+++ versions/PR-2733/build/limel-text-editor.entry.js
@@ -0,0 +1,2 @@
+import{r as t}from"./index-6156b4fd.js";const r="";const e=class{constructor(r){t(this,r);this.value=undefined}render(){return[]}};e.style=r;export{e as limel_text_editor};
+//# sourceMappingURL=limel-text-editor.entry.js.map
