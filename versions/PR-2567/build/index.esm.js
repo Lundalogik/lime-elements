3aa4fbc5b92ab96ea137dc862988976936338cbd
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2024-02-02 21:30:38 +0000

    chore(docs): create docs PR-2567

--- versions/PR-2567/build/index.esm.js
+++ versions/PR-2567/build/index.esm.js
@@ -1,2 +1,2 @@
-export{C as ColumnAggregatorType}from"./table.types-bafb7a24.js";
+export{F as FormLayoutType}from"./form.types-e39f98a4.js";export{C as ColumnAggregatorType}from"./table.types-bafb7a24.js";
 //# sourceMappingURL=index.esm.js.map
