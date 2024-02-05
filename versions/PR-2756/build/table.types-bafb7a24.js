8cbc3f8fef05625f784ee4035ae9c11990ea507a
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2024-02-05 08:37:58 +0000

    chore(docs): remove PR-2756

--- versions/PR-2756/build/table.types-bafb7a24.js
+++
@@ -1,2 +0,0 @@
-var a;(function(a){a["Average"]="avg";a["Maximum"]="max";a["Minimum"]="min";a["Sum"]="sum";a["Count"]="count"})(a||(a={}));export{a as C};
-//# sourceMappingURL=table.types-bafb7a24.js.map
