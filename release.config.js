6d61747e10486df78b5f3edcee3252962e8f84b2
Adrian Schmidt <adrian.schmidt@lime.tech>
2024-02-05 15:36:49 +0100

    Revert "ci(release.config.js): temporarily re-add release-config for branch `next`"
    This reverts commit 3aa906589e66f1e95ec59a21de97e9d97d0d4e17.

--- release.config.js
+++ release.config.js
@@ -6,7 +6,6 @@ module.exports = {
     branches: [
         'main',
         '+([0-9])?(.{+([0-9]),x}).x',
-        { name: 'next', prerelease: true },
         { name: 'dev', prerelease: true },
         { name: 'beta', prerelease: true },
         { name: 'alpha', prerelease: true },
