8f76fd2dc828360e0efd69b9af027b38f7488878
Adrian Schmidt <adrian.schmidt@lime.tech>
2024-02-12 14:41:45 +0100

    docs(config): add some simple docs for limel-config
    `limel-config` is marked as private, but is linked to from the docs
    about how to provide your own icon library.

--- src/components/config/config.tsx
+++ src/components/config/config.tsx
@@ -2,6 +2,18 @@ import { Component, Prop } from '@stencil/core';
 import { Config, globalConfig } from '../../global/config';
 
 /**
+ * Component used to set global configuration for Lime Elements.
+ *
+ * :::warning
+ * **Building something for Lime CRM?** Then you should _NOT_ use this component.
+ * Lime CRM already uses this component to set the global configuration for
+ * Lime Elements. No matter what problem you are facing at the moment, using
+ * this component will not help, and might cause other problems.
+ * :::
+ *
+ * Building your own software, which is using Lime Elements?
+ * Then you _might_ need to use this component.
+ *
  * @private
  */
 @Component({
