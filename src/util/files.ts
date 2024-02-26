834f57b4ed267b667e8065d1929d4443053fd767
Per Fryking <per.fryking@lime.tech>
2024-02-26 18:34:50 +0100

    feat(files): add helper function
    Adds a helper function to create `FileInfo` from a `File` and another
    function to check if a `FileInfo` complies with a accept string.

---
+++ src/util/files.ts
@@ -0,0 +1,51 @@
+import { FileInfo } from '../global/shared-types/file.types';
+import {
+    getFileBackgroundColor,
+    getFileColor,
+    getFileExtensionTitle,
+    getFileIcon,
+} from './file-metadata';
+
+export function createFileInfo(file: File): FileInfo {
+    const limeFile: FileInfo = {
+        id: crypto.randomUUID(),
+        filename: file.name,
+        contentType: file.type,
+        size: file.size,
+        fileContent: file,
+    };
+    limeFile.icon = {
+        name: getFileIcon(limeFile),
+        title: getFileExtensionTitle(limeFile),
+        color: getFileColor(limeFile),
+        backgroundColor: getFileBackgroundColor(limeFile),
+    };
+
+    return limeFile;
+}
+
+export function isTypeAccepted(file: FileInfo, accept?: string): boolean {
+    if (accept === undefined || accept === '*') {
+        return true;
+    }
+
+    const acceptList = accept.split(',').map((raw) => raw.trim());
+
+    return acceptList.some((acceptedType: string) => {
+        if (acceptedType === file.contentType) {
+            return true;
+        }
+
+        if (acceptedType.endsWith('/*')) {
+            const baseType = acceptedType.split('/')[0];
+
+            return file.contentType.startsWith(`${baseType}/`);
+        }
+
+        if (acceptedType.startsWith('.')) {
+            const fileType = acceptedType.split('.')[1];
+
+            return file.contentType.endsWith(`/${fileType}`);
+        }
+    });
+}
