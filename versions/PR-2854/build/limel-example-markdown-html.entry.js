94cf0c0c37d7a6523f20bbb78557933cf3bf0b57
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2024-03-19 16:05:09 +0000

    chore(docs): create docs PR-2854

---
+++ versions/PR-2854/build/limel-example-markdown-html.entry.js
@@ -0,0 +1,2 @@
+import{r as d,h as e}from"./index-6156b4fd.js";const t=`\n<dl>\n  <dt>A Definition list</dt>\n  <dd>is something people sometimes use.</dd>\n\n  <dt>Markdown in HTML</dt>\n  <dd>does *not* work **very** well.</dd>\n\n  <dt>What to do?</dt>\n  <dd>It's better to use HTML <em>tags</em>.</dd>\n\n  <dt>Can you use text colors?</dt>\n  <dd style="color:red">Yes, since I'm red!</dd>\n</dl>\n`;const n=class{constructor(e){d(this,e)}render(){return e("limel-markdown",{value:t})}};export{n as limel_example_markdown_html};
+//# sourceMappingURL=limel-example-markdown-html.entry.js.map
