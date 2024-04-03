314d7c9b300b959f97515c86a88b28c2937a5437
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2024-04-03 09:13:16 +0000

    chore(docs): create docs PR-2878

---
+++ versions/PR-2878/assets/kompendium/collection/kompendium/test/fixtures/basic.js
@@ -0,0 +1,44 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+  var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+  if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+  else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+  return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+/**
+ * Use this!
+ *
+ * @param {DecoratorConfig} _config how to use it
+ * @returns {ClassDecorator} the decorator
+ */
+const CustomDecorator = (_config) => {
+  return () => { };
+};
+/**
+ * The colors
+ */
+export var Color;
+(function (Color) {
+  /**
+   * The red color
+   */
+  Color["Red"] = "red";
+  /**
+   * Looks like green
+   */
+  Color["Green"] = "green";
+  /**
+   * Almost blue
+   */
+  Color["Blue"] = "blue";
+})(Color || (Color = {}));
+/**
+ * The Zap class
+ *
+ * @deprecated
+ */
+let Zap = class Zap {
+};
+Zap = __decorate([
+  CustomDecorator({ name: 'gg' })
+], Zap);
+export { Zap };
