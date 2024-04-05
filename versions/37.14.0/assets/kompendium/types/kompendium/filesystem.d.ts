90ddcd56e2ee7e906e13eb8fe3c0833701d1f53d
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2024-04-05 12:06:09 +0000

    chore(docs): remove undefined

--- versions/37.14.0/assets/kompendium/types/kompendium/filesystem.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="node" />
-import fs from 'fs';
-export declare function exists(path: string): Promise<boolean>;
-export declare function mkdir(path: string, options?: any): Promise<string>;
-export declare function readFile(path: string, options?: any): Promise<string>;
-export declare function writeFile(path: string, data: string, options?: any): Promise<void>;
-export declare function stat(path: string): Promise<fs.Stats>;
