27b6f67f7df51db473365d8e07349cd206bfac51
Laurens Borst <laurens.borst@lime.tech>
2024-04-08 15:32:19 +0200

    feat(text-editor): add basic text editor (private component in beta, at the moment)
    This implements a basic version of the ProseMirror text editor.
    
    Co-authored-by: Lucy Chyzhova <lucy.chyzhova@lime.tech>
    Co-authored-by: Kiarokh Moattar <kiarokh@icloud.com>
    Co-authored-by: Adrian Schmidt <adrian.schmidt@lime.tech>

---
+++ src/components/text-editor/text-editor.tsx
@@ -0,0 +1,76 @@
+import {
+    Component,
+    Element,
+    Event,
+    EventEmitter,
+    State,
+    h,
+} from '@stencil/core';
+import { EditorState } from 'prosemirror-state';
+import { EditorView } from 'prosemirror-view';
+import { Schema, DOMParser } from 'prosemirror-model';
+import { schema } from 'prosemirror-schema-basic';
+import { exampleSetup } from 'prosemirror-example-setup';
+
+/**
+ * This editor offers a rich text editing experience with markdown support,
+ * in the sense that you can easily type markdown syntax and see the rendered
+ * result as rich text in real-time. For instance, you can type `# Hello, world!`
+ * and see it directly turning to a heading 1 (an `<h1>` HTML element).
+ *
+ * Naturally, you can use standard keyboard hotkeys such as <kbd>Ctrl</kbd> + <kbd>B</kbd>
+ * to toggle bold text, <kbd>Ctrl</kbd> + <kbd>I</kbd> to toggle italic text, and so on.
+ *
+ * @exampleComponent limel-example-text-editor-basic
+ * @beta
+ * @private
+ */
+@Component({
+    tag: 'limel-text-editor',
+    shadow: true,
+    styleUrl: 'text-editor.scss',
+})
+export class TextEditor {
+    @Element()
+    private host: HTMLLimelTextEditorElement;
+
+    @State()
+    private view: EditorView;
+
+    /**
+     * Dispatched when a change is made to the editor
+     */
+    @Event()
+    private change: EventEmitter<{ html: string }>;
+
+    public componentWillLoad() {}
+
+    public render() {
+        return <div id="editor" />;
+    }
+
+    public componentDidLoad() {
+        const mySchema = new Schema({
+            nodes: schema.spec.nodes,
+            marks: schema.spec.marks,
+        });
+
+        this.view = new EditorView(
+            this.host.shadowRoot.querySelector('#editor'),
+            {
+                state: EditorState.create({
+                    doc: DOMParser.fromSchema(mySchema).parse(
+                        this.host.shadowRoot.querySelector('#editor'),
+                    ),
+                    plugins: exampleSetup({ schema: mySchema }),
+                }),
+                dispatchTransaction: (transaction) => {
+                    const newState = this.view.state.apply(transaction);
+                    this.view.updateState(newState);
+
+                    this.change.emit({ html: this.view.dom.innerHTML });
+                },
+            },
+        );
+    }
+}
