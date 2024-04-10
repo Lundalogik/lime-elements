be5e062f68f40583f21281787fe58180bd019953
Adrian Schmidt <adrian.schmidt@lime.tech>
2024-04-10 13:48:48 +0200

    refactor(tooltip): extract timer based logic to separate file

---
+++ src/components/tooltip/tooltipTimer.ts
@@ -0,0 +1,30 @@
+const DEFAULT_DELAY_FOR_SHOWING = 500;
+
+export class TooltipTimer {
+    private timerHandle: number | null = null;
+    private showCallback: Function;
+    private hideCallback: Function;
+    private delayForShowing: number;
+
+    constructor(
+        showCallback: Function,
+        hideCallback: Function,
+        delayForShowing: number = DEFAULT_DELAY_FOR_SHOWING,
+    ) {
+        this.showCallback = showCallback;
+        this.hideCallback = hideCallback;
+        this.delayForShowing = delayForShowing;
+    }
+
+    showAfterDelay(): void {
+        this.timerHandle = window.setTimeout(
+            this.showCallback,
+            this.delayForShowing,
+        );
+    }
+
+    hide(): void {
+        window.clearTimeout(this.timerHandle);
+        this.hideCallback();
+    }
+}
