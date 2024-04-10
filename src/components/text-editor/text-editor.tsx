07d02a538a92840d397c4048ac18e204dfa349a6
Laurens Borst <laurens.borst@lime.tech>
2024-04-10 16:08:59 +0200

    feat(text-editor): add buttons for lists

--- src/components/text-editor/text-editor.tsx
+++ src/components/text-editor/text-editor.tsx
@@ -10,6 +10,7 @@ import { EditorState } from 'prosemirror-state';
 import { EditorView } from 'prosemirror-view';
 import { Schema, DOMParser } from 'prosemirror-model';
 import { schema } from 'prosemirror-schema-basic';
+import { addListNodes } from 'prosemirror-schema-list';
 import { exampleSetup } from 'prosemirror-example-setup';
 
 /**
@@ -51,7 +52,7 @@ export class TextEditor {
 
     public componentDidLoad() {
         const mySchema = new Schema({
-            nodes: schema.spec.nodes,
+            nodes: addListNodes(schema.spec.nodes, 'paragraph block*', 'block'),
             marks: schema.spec.marks,
         });
 
