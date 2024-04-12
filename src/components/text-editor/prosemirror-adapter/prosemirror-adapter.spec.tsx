d5e789a5d699d9732d75b2a4a194930cb8ed15ae
Adrian Schmidt <adrian.schmidt@lime.tech>
2024-04-12 16:29:01 +0200

    test(text-editor): prevent console.warn logging expected warning to test output

--- src/components/text-editor/prosemirror-adapter/prosemirror-adapter.spec.tsx
+++ src/components/text-editor/prosemirror-adapter/prosemirror-adapter.spec.tsx
@@ -8,10 +8,20 @@ describe('prosemirror-adapter', () => {
     let editor: HTMLElement;
 
     beforeEach(async () => {
+        // Mock console.warn to avoid the warning message in the test output
+        // The warning happens because we initialize the editor in the
+        // componentDidLoad lifecycle method, which triggers extra rerenders.
+        // TODO: Can we avoid setting up the editor in componentDidLoad?
+        const consoleWarnSpy = jest
+            .spyOn(console, 'warn')
+            .mockImplementation(() => {});
         page = await newSpecPage({
             components: [ProsemirrorAdapter],
             template: () => <limel-prosemirror-adapter />,
         });
+        // Fail if there are unexpected warnings
+        expect(consoleWarnSpy).toHaveBeenCalledTimes(1);
+        jest.restoreAllMocks();
 
         editor = page.root.shadowRoot.querySelector('#editor');
     });
