fdfb9debee6ae5eb1b2da1eab1882fe65a2103b5
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2024-04-17 11:06:18 +0000

    chore(docs): create docs PR-2917

--- versions/PR-2917/build/index.esm.js
+++ versions/PR-2917/build/index.esm.js
@@ -1,2 +1,2 @@
-export{g as getIconBackgroundColorForFile,a as getIconFillColorForFile,b as getIconForFile}from"./icons-95e44bd9.js";export{C as ColumnAggregatorType,F as FormLayoutType}from"./table.types-a8f98572.js";
+export{g as getIconBackgroundColorForFile,a as getIconFillColorForFile,b as getIconForFile}from"./icons-d4830de7.js";export{C as ColumnAggregatorType,F as FormLayoutType}from"./table.types-a8f98572.js";
 //# sourceMappingURL=index.esm.js.map
