aa8cc73cb23e58ce6857ad94895d6b5c9a1fea5a
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2024-04-17 20:44:30 +0000

    chore(docs): create docs PR-2907

---
+++ versions/PR-2907/build/limel-example-prosemirror-adapter-basic.entry.js
@@ -0,0 +1,2 @@
+import{r,h as e}from"./index-6156b4fd.js";const s=class{constructor(e){r(this,e);this.handleChange=r=>{r.stopPropagation();this.text=r.detail};this.text={html:""}}render(){return[e("limel-prosemirror-adapter",{onChange:this.handleChange}),e("limel-example-value",{value:this.text})]}};export{s as limel_example_prosemirror_adapter_basic};
+//# sourceMappingURL=limel-example-prosemirror-adapter-basic.entry.js.map
--- versions/PR-2907/build/limel-example-text-editor-with-custom-menu.entry.js
+++
@@ -1,2 +0,0 @@
-import{r as e,h as t}from"./index-6156b4fd.js";const i=class{constructor(t){e(this,t);this.handleChange=e=>{e.stopPropagation();this.text=e.detail};this.text={html:""}}render(){const e=["strong","emphasis","heading 1","heading 2","heading 3","undo","redo","bullet list","ordered list","block quote","join above","lift out"];return[t("limel-text-editor",{onChange:this.handleChange,menuItems:e}),t("limel-example-value",{value:this.text})]}};export{i as limel_example_text_editor_with_custom_menu};
-//# sourceMappingURL=limel-example-text-editor-with-custom-menu.entry.js.map
