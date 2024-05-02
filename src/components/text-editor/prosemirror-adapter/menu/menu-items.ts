4cc1c3e1e2dd333959e71c14fa41dd056f938895
John Traas <johntraas@lime-n4qd4x9f7t.localdomain>
2024-05-02 10:02:42 +0200

    feat(text editor): add new icons and mod/option key display depending on user system

--- src/components/text-editor/prosemirror-adapter/menu/menu-items.ts
+++ src/components/text-editor/prosemirror-adapter/menu/menu-items.ts
@@ -1,55 +1,75 @@
 import { ActionBarItem } from 'src/components/action-bar/action-bar.types';
 import { ListSeparator } from 'src/components/list/list-item.types';
 
+const getCommandSymbols = (): {
+    mod: string;
+    option: string;
+    shift: string;
+} => {
+    const macUserAgent = /Macintosh|MacIntel|MacPPC|Mac68K/;
+    if (navigator.userAgent.match(macUserAgent)) {
+        return { mod: '⌘', option: '⌥', shift: '⇧' };
+    }
+
+    return { mod: 'Ctrl', option: 'Alt', shift: 'Shift' };
+};
+
+const { mod, shift } = getCommandSymbols();
+
 export const textEditorMenuItems: Array<ActionBarItem | ListSeparator> = [
     {
+        value: 'strong',
         text: 'Bold',
-        commandText: '⌘ B',
-        icon: 'bold',
+        commandText: `${mod} B`,
+        icon: '-lime-text-bold',
         iconOnly: true,
     },
     {
+        value: 'em',
         text: 'Italic',
-        commandText: '⌘ I',
-        icon: 'italic',
-        iconOnly: true,
-    },
-    {
-        text: 'Add or remove link',
-        commandText: '⌘ shift U',
-        icon: 'link',
+        commandText: `${mod} I`,
+        icon: '-lime-text-italic',
         iconOnly: true,
     },
     { separator: true },
     {
+        value: 'headerlevel1',
         text: 'Header Level 1',
-        icon: 'header_1',
+        commandText: `${mod} ${shift} 1`,
+        icon: '-lime-text-h-heading-1',
         iconOnly: true,
     },
     {
+        value: 'headerlevel2',
         text: 'Header Level 2',
-        icon: 'header_2',
+        commandText: `${mod} ${shift} 2`,
+        icon: '-lime-text-h-heading-2',
         iconOnly: true,
     },
     {
+        value: 'headerlevel3',
         text: 'Header Level 3',
-        icon: 'header_3',
+        commandText: `${mod} ${shift} 3`,
+        icon: '-lime-text-h-heading-3',
         iconOnly: true,
     },
     { separator: true },
     {
-        text: 'List',
-        icon: 'list',
+        value: 'bullet_list',
+        text: 'Bullet list',
+        icon: '-lime-text-bulleted-list',
         iconOnly: true,
     },
     {
+        value: 'ordered_list',
         text: 'Numbered list',
-        icon: 'numbered_list',
+        icon: '-lime-text-ordered-list',
         iconOnly: true,
     },
     {
+        value: 'blockquote',
         text: 'Blockquote',
-        icon: 'quote_right',
+        icon: '-lime-text-blockquote',
         iconOnly: true,
     },
 ];
