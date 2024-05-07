b828a104e775ea7e223ba74f272458159c85e15e
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2024-05-07 15:29:55 +0000

    chore(docs): create docs 37.32.5

---
+++ versions/37.32.5/assets/kompendium/types/components/markdown/examples/markdown.d.ts
@@ -0,0 +1,8 @@
+/**
+ * This is a simple example of how the `kompendium-markdown` component is used
+ *
+ * @link markdown-example.ts
+ */
+export declare class MarkdownExample {
+  render(): HTMLElement;
+}
