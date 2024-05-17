9d066c4d0068c3c0ff636e1c383ac1c447c5dcaa
Kiarokh Moattar <kiarokh@icloud.com>
2024-05-17 16:14:44 +0200

    fix(chip): construct a better label for the remove button

--- src/translations/sv.ts
+++ src/translations/sv.ts
@@ -10,7 +10,6 @@ export default {
     'date-picker.quarter.heading': 'Kvartal',
     'date-picker.year.heading': 'År',
     'chip-set.clear-all': 'Rensa alla',
-    'chip-set.remove-chip': 'Ta bort chip',
     'snackbar.dismiss': 'Stäng',
     'file.drag-and-drop-tips':
         'Dra och släpp filen här eller klicka om du vill bläddra.',
