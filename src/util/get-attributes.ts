74c6f0358094efb7bad48614c95c8e5cecd8ea52
Laurens Borst <laurens.borst@lime.tech>
2024-05-20 11:04:56 +0200

    test(breadcrumbs): abstract the getIDs function
    The abstraction allows you to not only gets IDs, but any type
    of HTML attribute recursively
    
    Intended to be used on attributes that are randomly generated
    and otherwise hard to test

---
+++ src/util/get-attributes.ts
@@ -0,0 +1,32 @@
+/**
+ * Some attributes are randomly generated and we do not have access to them as they
+ * are generated e.g., inside the breadcrumbs component. But we can get them this way.
+ * Retrieves such attributes as ids via inorder traversal
+ *
+ * Useful for testing
+ *
+ * @param node - base node
+ * @param attributes - array that you wish to populate that contains the found attributes
+ * @param attribute - the name of the type of attribute you are looking for
+ *
+ * @internal
+ */
+export function getAttributesRecursively(
+    node: Element,
+    attributes: string[],
+    attribute: string = 'id',
+) {
+    if (node.children && node.getAttribute(attribute)) {
+        attributes.push(node.getAttribute(attribute));
+    }
+
+    if (node.shadowRoot) {
+        for (const child of Array.from(node.shadowRoot.children)) {
+            getAttributesRecursively(child, attributes, attribute);
+        }
+    }
+
+    for (const child of Array.from(node.children)) {
+        getAttributesRecursively(child, attributes, attribute);
+    }
+}
