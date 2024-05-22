712ed03fb202c99ac2b63627b2a2620f39f837f8
Kiarokh Moattar <kiarokh@icloud.com>
2024-05-22 14:01:48 +0200

    fix(chip): ensure the ellipsis menu changes color in dark mode

--- src/components/chip/chip.tsx
+++ src/components/chip/chip.tsx
@@ -346,7 +346,7 @@ export class Chip implements ChipInterface {
         }
 
         const svgData =
-            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" xml:space="preserve"><circle cx="16" cy="16" r="2"/><circle cx="16" cy="24" r="2"/><circle cx="16" cy="8" r="2"/></svg>';
+            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" xml:space="preserve"><circle fill="currentColor" cx="16" cy="16" r="2"/><circle fill="currentColor" cx="16" cy="24" r="2"/><circle fill="currentColor" cx="16" cy="8" r="2"/></svg>';
 
         const menuItems = this.getMenuItems();
 
