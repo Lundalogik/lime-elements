541ea98284ced90c255391b5ec3caf9635a9e76d
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2024-05-27 09:02:23 +0000

    chore(docs): create docs 37.44.1

---
+++ versions/37.44.1/assets/kompendium/types/components/darkmode-switch/darkmode-switch.d.ts
@@ -0,0 +1,18 @@
+/**
+ * @private
+ */
+export declare class DarkmodeSwitch {
+  private theme;
+  private systemSettingIsDark;
+  private checkbox;
+  private colorSchemeMediaQuery;
+  constructor();
+  connectedCallback(): void;
+  disconnectedCallback(): void;
+  componentWillLoad(): void;
+  render(): HTMLElement;
+  private getSelectRef;
+  private handleSystemThemeChange;
+  private handleThemeChange;
+  private setTheme;
+}
