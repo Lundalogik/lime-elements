7c6b79f44533f76f1acfbf43c54b2219e9699f0f
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2024-09-09 12:53:53 +0000

    chore(docs): create docs PR-3154

---
+++ versions/PR-3154/build/allowed-css-properties-2dc31137.js
@@ -0,0 +1,2 @@
+const t=["background-color","color","font-style","font-weight","text-decoration-color","text-decoration-line","text-decoration-skip-ink","text-decoration-style","text-decoration-thickness","text-decoration","width","border","border-radius"];export{t as a};
+//# sourceMappingURL=allowed-css-properties-2dc31137.js.map
