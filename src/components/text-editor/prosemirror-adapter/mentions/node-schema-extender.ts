9893b310500d88772aef0e2546d428c4ec8bda6f
John Traas <johntraas@lime-n4qd4x9f7t.localdomain>
2024-09-11 09:00:16 +0200

    feat(text editor mentions): add node spec

---
+++ src/components/text-editor/prosemirror-adapter/mentions/node-schema-extender.ts
@@ -0,0 +1,35 @@
+import { NodeSpec } from 'prosemirror-model';
+
+export const mention: NodeSpec = {
+    inline: true,
+    group: 'inline',
+    selectable: true,
+    atom: false,
+
+    attrs: {
+        type: {},
+        objectid: {},
+        descriptive: {},
+    },
+
+    toDOM: (node) => [
+        'limebb-mention',
+        {
+            class: 'mention',
+            style: 'color: blue',
+            type: node.attrs.type,
+            objectid: node.attrs.objectid,
+            descriptive: node.attrs.descriptive,
+        },
+    ],
+    parseDOM: [
+        {
+            tag: 'limebb-mention[type][objectid]',
+            getAttrs: (dom) => ({
+                type: dom.getAttribute('type'),
+                objectid: dom.getAttribute('objectid'),
+                descriptive: dom.getAttribute('descriptive'),
+            }),
+        },
+    ],
+};
