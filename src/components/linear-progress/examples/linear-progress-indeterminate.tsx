bdf913047740cd7c04f66193c2ea955f92c256a6
Kiarokh Moattar <kiarokh@icloud.com>
2024-09-11 15:59:15 +0200

    docs(linear-progress): improve documentations & examples

--- src/components/linear-progress/examples/linear-progress-indeterminate.tsx
+++ src/components/linear-progress/examples/linear-progress-indeterminate.tsx
@@ -1,7 +1,12 @@
 import { Component, h } from '@stencil/core';
 
 /**
- * Indeterminate progress bar
+ * Indeterminate progress
+ * The component can be put in an indeterminate state,
+ * where the progress bar will animate without a specific value.
+ *
+ * This is useful for instance when the progress is not known,
+ * but the user should be aware that something is happening.
  */
 @Component({
     shadow: true,
