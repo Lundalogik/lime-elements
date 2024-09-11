bc78c84084b0ad7381f7c3aabe352da717419635
John Traas <johntraas@lime-n4qd4x9f7t.localdomain>
2024-09-11 09:00:45 +0200

    feat(text editor mentions): serialize mention from Markdown

--- src/components/text-editor/utils/markdown-converter.ts
+++ src/components/text-editor/utils/markdown-converter.ts
@@ -1,14 +1,25 @@
 import { ContentTypeConverter } from './content-type-converter';
 import { EditorView } from 'prosemirror-view';
+import { Node as ProseMirrorNode } from 'prosemirror-model';
 import {
     MarkdownSerializer,
+    MarkdownSerializerState,
     defaultMarkdownSerializer,
 } from 'prosemirror-markdown';
 import { markdownToHTML } from '../../markdown/markdown-parser';
 
+const mentionSerializer = {
+    mention: (state: MarkdownSerializerState, node: ProseMirrorNode) => {
+        state.write(
+            `<limebb-mention type="${node.attrs.type}" objectid="${node.attrs.objectid}" descriptive="${node.attrs.descriptive}"></limebb-mention>`,
+        );
+    },
+};
+
 const customMarkdownSerializer = new MarkdownSerializer(
     {
         ...defaultMarkdownSerializer.nodes,
+        mention: mentionSerializer.mention,
     },
     {
         ...defaultMarkdownSerializer.marks,
