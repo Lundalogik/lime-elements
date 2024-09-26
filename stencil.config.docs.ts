6ed801737f07359a653ea681fc49cce2881c2e38
Adrian Schmidt <adrian.schmidt@lime.tech>
2024-09-26 12:45:48 +0200

    chore: minor fixes required by upgrade to Stencil 4

--- stencil.config.docs.ts
+++ stencil.config.docs.ts
@@ -29,7 +29,7 @@ export const config: Config = {
                 },
                 {
                     src: 'favicon.svg',
-                    dest: 'favicon.svg',
+                    dest: '.',
                 },
             ],
         },
