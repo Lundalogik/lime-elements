8cf8620988b17cc0a5303c72c069e4d33259e666
Tomas Eketorp <58422324+eketorp@users.noreply.github.com>
2024-10-08 12:27:28 +0200

    fix(L10n): fix broken support for French (`fr`) and German (`de`)

--- src/global/translations.ts
+++ src/global/translations.ts
@@ -1,11 +1,22 @@
 import da from '../translations/da';
+import de from '../translations/de';
 import en from '../translations/en';
 import fi from '../translations/fi';
+import fr from '../translations/fr';
 import nl from '../translations/nl';
 import no from '../translations/no';
 import sv from '../translations/sv';
 
-const allTranslations = { da: da, en: en, fi: fi, no: no, nl: nl, sv: sv };
+const allTranslations = {
+    da: da,
+    de: de,
+    en: en,
+    fi: fi,
+    fr: fr,
+    no: no,
+    nl: nl,
+    sv: sv,
+};
 
 export class Translations {
     public get(key, language = 'en') {
