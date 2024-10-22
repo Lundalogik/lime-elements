15a1242954691ae05c5a140eb11c9e5066b6088f
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2024-10-22 12:54:42 +0000

    chore(docs): create docs PR-3258

---
+++ versions/PR-3258/build/limel-example-input-field-suffix.entry.js
@@ -0,0 +1,2 @@
+import{r as i,h as t}from"./index-6156b4fd.js";const e=class{constructor(t){i(this,t);this.handleChange=i=>{this.value=i.detail};this.value="50"}render(){return t("limel-input-field",{label:"Quantity",suffix:"pcs",value:this.value,type:"number",onChange:this.handleChange})}};export{e as limel_example_input_field_suffix};
+//# sourceMappingURL=limel-example-input-field-suffix.entry.js.map
