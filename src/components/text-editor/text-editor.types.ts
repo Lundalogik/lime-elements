29d464a14e60d9a0e59682e2c538777f99fb630e
Per Fryking <per.fryking@lime.tech>
2024-11-01 13:02:41 +0100

    feat(text-editor): add types
    Add types for trigger functionality

---
+++ src/components/text-editor/text-editor.types.ts
@@ -0,0 +1,83 @@
+import { CustomElement } from '../../global/shared-types/custom-element.types';
+
+/**
+ * @alpha
+ */
+export type TriggerCharacter =
+    | '@'
+    | '#'
+    | '$'
+    | '!'
+    | '?'
+    | '&'
+    | '*'
+    | '%'
+    | '+'
+    | '-'
+    | '='
+    | '/'
+    | '\\'
+    | '^'
+    | '~'
+    | '`'
+    | ':'
+    | ';'
+    | '|'
+    | '.'
+    | ','
+    | '<'
+    | '>'
+    | '['
+    | ']'
+    | '{'
+    | '}'
+    | '('
+    | ')'
+    | "'";
+
+/**
+ * @alpha
+ */
+export type TextEditorNode = {
+    /**
+     * The top node
+     */
+    node: CustomElement | string;
+
+    /**
+     * One more more children under the top node
+     */
+    children?: Array<TextEditorNode | string>;
+};
+
+/**
+ * @alpha
+ */
+export interface TextEditor {
+    /**
+     * Method to insert either text or a node at the cursor position
+     *
+     */
+    insert: (input: TextEditorNode | string) => void;
+}
+
+/**
+ * @alpha
+ */
+export interface TriggerEventDetail {
+    /**
+     * The trigger that triggered this event
+     *
+     */
+    trigger: TriggerCharacter;
+
+    /**
+     * The text editor
+     */
+    textEditor: TextEditor;
+
+    /**
+     * Current value of the trigger
+     */
+    value: string;
+}
--- src/global/shared-types/custom-element.types.ts
+++ src/global/shared-types/custom-element.types.ts
@@ -9,3 +9,15 @@ export interface CustomElementDefinition {
     tagName: string;
     attributes: string[];
 }
+
+/**
+ * Custom Element
+ *
+ * @alpha
+ */
+export type CustomElement = Omit<CustomElementDefinition, 'attributes'> & {
+    /**
+     * Record of attributes and values to apply to the node
+     */
+    attributes: Record<string, any>;
+};
