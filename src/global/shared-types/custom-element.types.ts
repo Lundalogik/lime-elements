545aa9c7174a5813e015cbe59ead0b400a1f58d3
Per Fryking <per.fryking@lime.tech>
2024-11-01 10:33:24 +0100

    fix(custom-element): change name to CustomElementDefinition
    Change the name to `CustomElementDefinition` to better reflect the
    meaning of this type and how it's supposed to be used.

--- src/global/shared-types/custom-element.types.ts
+++ src/global/shared-types/custom-element.types.ts
@@ -1,9 +1,11 @@
 /**
- * Custom Element
+ * Custom Element definition
+ *
+ * Used to define a Custom Element
  *
  * @alpha
  */
-export interface CustomElement {
+export interface CustomElementDefinition {
     tagName: string;
     attributes: string[];
 }
