be1b2348fa250f0d6bef2fec3de6a294161a02a1
Fredrik Wallström <21986391+FredrikWallstrom@users.noreply.github.com>
2024-11-21 15:07:52 +0100

    feat(text-editor): add insertHtml function to properly parse html string
    Co-authored-by: Adrian Schmidt <adrian.schmidt@lime.tech>

---
+++ src/components/text-editor/prosemirror-adapter/plugins/trigger/create-html-inserter.ts
@@ -0,0 +1,25 @@
+import { Node, DOMParser, Fragment } from 'prosemirror-model';
+import { EditorView } from 'prosemirror-view';
+import { ContentTypeConverter } from '../../../utils/content-type-converter';
+
+export const createHtmlInserter = (
+    view: EditorView,
+    contentConverter: ContentTypeConverter,
+    startPos: number,
+    dispatchTransaction: (
+        view: EditorView,
+        startPos: number,
+        fragment: Fragment | Node,
+    ) => void,
+): ((input: string) => Promise<void>) => {
+    const schema = view.state.schema;
+
+    return async (input: string): Promise<void> => {
+        const container = document.createElement('span');
+        container.innerHTML = await contentConverter.parseAsHTML(input, schema);
+
+        const fragment = DOMParser.fromSchema(schema).parse(container).content;
+
+        dispatchTransaction(view, startPos, fragment);
+    };
+};
