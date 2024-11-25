d21ae4b65e31a64345deb4db0e5805f595d256d1
Adrian Schmidt <adrian.schmidt@lime.tech>
2024-11-25 11:31:30 +0100

    fix(text-editor): close XSS vulnerability
    fix: Lundalogik/crm-feature#4490

--- src/components/text-editor/utils/html-converter.ts
+++ src/components/text-editor/utils/html-converter.ts
@@ -1,14 +1,20 @@
 import { ContentTypeConverter } from './content-type-converter';
 import { EditorView } from 'prosemirror-view';
+import { markdownToHTML } from '../../markdown/markdown-parser';
+import { CustomElementDefinition } from '../../../interface';
 
 /**
  * @private
  */
 export class HTMLConverter implements ContentTypeConverter {
+    private customNodes: CustomElementDefinition[];
+
+    constructor(plugins: CustomElementDefinition[]) {
+        this.customNodes = plugins;
+    }
+
     public parseAsHTML = (text: string): Promise<string> => {
-        return new Promise((resolve) => {
-            resolve(text);
-        });
+        return markdownToHTML(text, { whitelist: this.customNodes });
     };
 
     public serialize = (view: EditorView): string => {
