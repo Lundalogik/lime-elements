25e5f9655cd143268df06aaeef4b59f246c85a92
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2024-12-04 13:07:03 +0000

    chore(docs): create docs 37.74.0

---
+++ versions/37.74.0/build/limel-example-input-field-search.entry.js
@@ -0,0 +1,2 @@
+import{r as e,h as i}from"./index-6156b4fd.js";const s=class{constructor(i){e(this,i);this.handleChange=e=>{this.value=e.detail};this.value=undefined}render(){return i("limel-input-field",{label:"Search",type:"search",leadingIcon:"search",value:this.value,onChange:this.handleChange})}};export{s as limel_example_input_field_search};
+//# sourceMappingURL=limel-example-input-field-search.entry.js.map
