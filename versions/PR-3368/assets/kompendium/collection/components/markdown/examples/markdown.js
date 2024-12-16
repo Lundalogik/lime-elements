29238d9e8f6e1e751a61a5cc37ea8a811557f4ac
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2024-12-16 14:39:04 +0000

    chore(docs): create docs PR-3368

---
+++ versions/PR-3368/assets/kompendium/collection/components/markdown/examples/markdown.js
@@ -0,0 +1,13 @@
+import { Component, h } from '@stencil/core';
+import { markdownExample } from './markdown-example';
+/**
+ * This is a simple example of how the `kompendium-markdown` component is used
+ * @sourceFile markdown-example.ts
+ */
+export class MarkdownExample {
+  render() {
+    return h("kompendium-markdown", { text: markdownExample });
+  }
+  static get is() { return "kompendium-example-markdown"; }
+  static get encapsulation() { return "shadow"; }
+}
