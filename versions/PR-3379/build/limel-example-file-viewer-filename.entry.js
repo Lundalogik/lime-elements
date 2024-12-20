27a98ebea7e374c58b32715d3eb248d366e60cda
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2024-12-20 10:23:47 +0000

    chore(docs): create docs PR-3379

--- versions/PR-3379/build/limel-example-file-viewer-filename.entry.js
+++ versions/PR-3379/build/limel-example-file-viewer-filename.entry.js
@@ -1,2 +1,2 @@
-import{r as e,h as r}from"./index-6156b4fd.js";const t=class{constructor(r){e(this,r)}render(){return r("limel-file-viewer",{url:"https://avatars.githubusercontent.com/u/2682464?s=200&v=4",alt:"Lime's avatar on Github",filename:"lime-logo.png"})}};export{t as limel_example_file_viewer_filename};
+import{r as e,h as r}from"./index-6156b4fd.js";const i=class{constructor(r){e(this,r)}render(){return r("limel-file-viewer",{url:"https://unsplash.it/1280/720/?random",alt:"Some random picture form Unsplash",filename:"random-image.jpg"})}};export{i as limel_example_file_viewer_filename};
 //# sourceMappingURL=limel-example-file-viewer-filename.entry.js.map
