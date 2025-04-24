fe2c7766aa6bc687d4fde008bace77c86bd80225
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2025-04-24 07:35:42 +0000

    chore(docs): create docs PR-3539

---
+++ versions/PR-3539/build/limel-example-button-icon.entry.js
@@ -0,0 +1,2 @@
+import{r as e,h as t}from"./index-2714248e.js";const o=class{constructor(t){e(this,t)}render(){return t("limel-button",{icon:"filled_message"})}};export{o as limel_example_button_icon};
+//# sourceMappingURL=limel-example-button-icon.entry.js.map
