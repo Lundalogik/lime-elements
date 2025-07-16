b4955016c40d3d852d59717ae259c8a12172ff7b
Adrian Schmidt <adrian.schmidt@lime.tech>
2025-07-16 17:31:59 +0200

    ci(eslint.config.mjs): ignore `docsDist` folder

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig, globalIgnores } from 'eslint/config';
 import config from '@limetech/eslint-config';
 
 export default defineConfig([
-    globalIgnores(['.kompendium', 'api-extractor.json']),
+    globalIgnores(['.kompendium', 'api-extractor.json', 'docsDist']),
     ...config,
     {
         files: ['**/*.{ts,tsx}'],
