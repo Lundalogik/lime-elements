8141044e830b4257b92b60697193b2341cc14eda
Kiarokh Moattar <kiarokh@icloud.com>
2025-07-31 12:11:00 +0200

    refactor(slider):  use `limel-notched-outline` for rendering

--- src/components/slider/slider.tsx
+++ src/components/slider/slider.tsx
@@ -162,60 +162,68 @@ export class Slider {
 
         return (
             <Host class={this.getContainerClassList()}>
-                <label
-                    class="slider__label mdc-floating-label mdc-floating-label--float-above"
-                    id={this.labelId}
+                <limel-notched-outline
+                    labelId={this.labelId}
+                    label={this.label}
+                    required={this.required}
+                    invalid={this.invalid}
+                    disabled={this.disabled}
+                    readonly={this.readonly}
+                    hasValue={!!this.value}
+                    hasFloatingLabel={true}
                 >
-                    {this.label}
-                </label>
-                <div class="slider__content-range-container">
-                    <span class="slider__content-min-label">
-                        {this.multiplyByFactor(this.valuemin)}
-                        {this.unit}
-                    </span>
-                    <span class="slider__content-max-label">
-                        {this.multiplyByFactor(this.valuemax)}
-                        {this.unit}
-                    </span>
-                </div>
-                <div
-                    class={{
-                        'mdc-slider': true,
-                        'mdc-slider--discrete': true,
-                        'mdc-slider--disabled': this.disabled || this.readonly,
-                    }}
-                >
-                    <input
-                        class="mdc-slider__input"
-                        type="range"
-                        min={this.multiplyByFactor(this.valuemin)}
-                        max={this.multiplyByFactor(this.valuemax)}
-                        value={this.multiplyByFactor(this.value)}
-                        name="volume"
-                        aria-labelledby={this.labelId}
-                        aria-controls={this.helperTextId}
-                        {...inputProps}
-                    />
-                    <div class="mdc-slider__track">
-                        <div class="mdc-slider__track--inactive"></div>
-                        <div class="mdc-slider__track--active">
-                            <div class="mdc-slider__track--active_fill"></div>
+                    <div slot="content">
+                        <div class="slider__content-range-container">
+                            <span class="slider__content-min-label">
+                                {this.multiplyByFactor(this.valuemin)}
+                                {this.unit}
+                            </span>
+                            <span class="slider__content-max-label">
+                                {this.multiplyByFactor(this.valuemax)}
+                                {this.unit}
+                            </span>
                         </div>
-                    </div>
-                    <div class="mdc-slider__thumb">
                         <div
-                            class="mdc-slider__value-indicator-container"
-                            aria-hidden="true"
+                            class={{
+                                'mdc-slider': true,
+                                'mdc-slider--discrete': true,
+                                'mdc-slider--disabled':
+                                    this.disabled || this.readonly,
+                            }}
                         >
-                            <div class="mdc-slider__value-indicator">
-                                <span class="mdc-slider__value-indicator-text">
-                                    {this.multiplyByFactor(this.value)}
-                                </span>
+                            <input
+                                class="mdc-slider__input"
+                                type="range"
+                                min={this.multiplyByFactor(this.valuemin)}
+                                max={this.multiplyByFactor(this.valuemax)}
+                                value={this.multiplyByFactor(this.value)}
+                                name="volume"
+                                aria-labelledby={this.labelId}
+                                aria-controls={this.helperTextId}
+                                {...inputProps}
+                            />
+                            <div class="mdc-slider__track">
+                                <div class="mdc-slider__track--inactive"></div>
+                                <div class="mdc-slider__track--active">
+                                    <div class="mdc-slider__track--active_fill"></div>
+                                </div>
+                            </div>
+                            <div class="mdc-slider__thumb">
+                                <div
+                                    class="mdc-slider__value-indicator-container"
+                                    aria-hidden="true"
+                                >
+                                    <div class="mdc-slider__value-indicator">
+                                        <span class="mdc-slider__value-indicator-text">
+                                            {this.multiplyByFactor(this.value)}
+                                        </span>
+                                    </div>
+                                </div>
+                                <div class="mdc-slider__thumb-knob"></div>
                             </div>
                         </div>
-                        <div class="mdc-slider__thumb-knob"></div>
                     </div>
-                </div>
+                </limel-notched-outline>
                 {this.renderHelperLine()}
             </Host>
         );
