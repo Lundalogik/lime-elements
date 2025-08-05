7f3d65035a086b80126f5e509431010d5c867acb
Kiarokh Moattar <kiarokh@icloud.com>
2025-08-05 16:54:38 +0200

    chore(checkbox): get rid of imported MDC's styles
    As a first step of getting rid of MDC, we need to make sure that
    the component renders correctly, without relying on any styles
    applied by MDC.

--- src/components/checkbox/checkbox.template.tsx
+++ src/components/checkbox/checkbox.template.tsx
@@ -50,52 +50,37 @@ export const CheckboxTemplate: FunctionalComponent<CheckboxTemplateProps> = (
     }
 
     return [
-        <div class="mdc-form-field ">
-            <div
-                class={{
-                    'mdc-checkbox': true,
-                    'mdc-checkbox--invalid': props.invalid,
-                    'mdc-checkbox--disabled': props.disabled,
-                    'mdc-checkbox--required': props.required,
-                    'mdc-checkbox--indeterminate': props.indeterminate,
-                    'lime-checkbox--readonly': props.readonly,
-                }}
-            >
-                <input
-                    type="checkbox"
-                    class="mdc-checkbox__native-control"
-                    id={props.id}
-                    checked={props.checked}
-                    disabled={props.disabled || props.readonly}
-                    required={props.required}
-                    onChange={props.onChange}
-                    aria-controls={props.helperTextId}
-                    aria-describedby={props.helperTextId}
-                    {...inputProps}
-                />
-                <div class="mdc-checkbox__background">
-                    <svg class="mdc-checkbox__checkmark" viewBox="0 0 24 24">
-                        <path
-                            class="mdc-checkbox__checkmark-path"
-                            fill="none"
-                            d="M1.73,12.91 8.1,19.28 22.79,4.59"
-                        />
-                    </svg>
-                    <div class="mdc-checkbox__mixedmark" />
-                </div>
+        <div
+            class={{
+                'mdc-form-field': true, // required by MDC to work
+                'mdc-checkbox': true, // required by MDC to work
+                checkbox: true,
+                checked: props.checked,
+                invalid: props.invalid,
+                disabled: props.disabled,
+                required: props.required,
+                indeterminate: props.indeterminate,
+                readonly: props.readonly,
+            }}
+        >
+            <input
+                type="checkbox"
+                class="mdc-checkbox__native-control" // required by MDC to work
+                id={props.id}
+                checked={props.checked}
+                disabled={props.disabled || props.readonly}
+                required={props.required}
+                onChange={props.onChange}
+                aria-controls={props.helperTextId}
+                aria-describedby={props.helperTextId}
+                {...inputProps}
+            />
+            <div class="box">
+                <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+                    <path fill="none" d="M1.73,12.91 8.1,19.28 22.79,4.59" />
+                </svg>
             </div>
-            <label
-                class={{
-                    'mdc-checkbox--invalid': props.invalid,
-                    'mdc-checkbox--disabled': props.disabled,
-                    'mdc-checkbox--required': props.required,
-                    'mdc-checkbox--indeterminate': props.indeterminate,
-                    'lime-checkbox--readonly': props.readonly,
-                }}
-                htmlFor={props.id}
-            >
-                {props.label}
-            </label>
+            <label htmlFor={props.id}>{props.label}</label>
         </div>,
         <HelperText
             text={props.helperText}
