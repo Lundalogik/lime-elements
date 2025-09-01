d67b9b7ea9ba9f4adb5fb5eb71af869c700a9a61
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2025-09-01 13:41:56 +0000

    chore(docs): remove PR-3660

--- versions/PR-3660/build/limel-example-chip-image.entry.js
+++
@@ -1,2 +0,0 @@
-import{r as e,h as o}from"./index-2714248e.js";const t=class{constructor(o){e(this,o)}render(){const e={name:"filled_star",color:"rgb(var(--color-yellow-default))"};const t={src:"https://lundalogik.github.io/lime-elements/780af2a6-d3d1-4593-8642-f03210d09271.png",alt:"A picture of Lucy Chyzhova, UX designer at Lime Technologies"};return o("limel-chip",{text:"Lucy Chyzhova",icon:e,image:t})}};export{t as limel_example_chip_image};
-//# sourceMappingURL=limel-example-chip-image.entry.js.map
