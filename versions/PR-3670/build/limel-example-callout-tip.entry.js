7b08225548228fd9f58f68933d4756c68cf1a8b1
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2025-09-08 08:01:07 +0000

    chore(docs): create docs PR-3670

---
+++ versions/PR-3670/build/limel-example-callout-tip.entry.js
@@ -0,0 +1,2 @@
+import{r as t,h as e}from"./index-2714248e.js";const r=class{constructor(e){t(this,e)}render(){return e("limel-callout",{type:"tip"},"You want to read this.")}};export{r as limel_example_callout_tip};
+//# sourceMappingURL=limel-example-callout-tip.entry.js.map
