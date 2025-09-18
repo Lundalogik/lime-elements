78583ef810ac911a86dc0c258a3201baddbcb65a
John Traas <john.traas@lime.tech>
2025-09-18 16:06:33 +0200

    chore(text-editor test suite): export all from test suite

---
+++ src/components/text-editor/test-setup/editor-test-utils.ts
@@ -0,0 +1,7 @@
+export * from './schema-builder';
+export * from './editor-state-builder';
+export * from './editor-view-builder';
+export * from './content-generator';
+export * from './command-tester';
+export * from './event-simulator';
+export * from './mock-factories';
