4121c6da94e354c91026e27dc684163ae87a0d49
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2025-10-03 08:35:37 +0000

    chore(docs): create docs 38.24.5

---
+++ versions/38.24.5/build/limel-example-markdown-images.entry.js
@@ -0,0 +1,2 @@
+import{r as e,h as t}from"./index-2714248e.js";const o=`\nHere's our logo (hover to see the title text):\n\nInline-style:\n![alt text](https://lundalogik.github.io/lime-elements/versions/latest/favicon.svg "Logo Title Text 1")\n\nReference-style:\n![alt text][logo]\n\n[logo]: https://lundalogik.github.io/lime-elements/versions/latest/favicon.svg "Logo Title Text 2"\n`;const n=class{constructor(t){e(this,t)}render(){return t("limel-markdown",{value:o})}};export{n as limel_example_markdown_images};
+//# sourceMappingURL=limel-example-markdown-images.entry.js.map
