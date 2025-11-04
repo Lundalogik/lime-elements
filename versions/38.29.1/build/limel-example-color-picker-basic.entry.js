02446e914dba18e03841eba1316320cc4e879925
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2025-11-04 13:36:55 +0000

    chore(docs): remove undefined

--- versions/38.29.1/build/limel-example-color-picker-basic.entry.js
+++
@@ -1,2 +0,0 @@
-import{r as e,h as o}from"./index-2714248e.js";const r=class{constructor(o){e(this,o);this.onChange=e=>{this.value=e.detail};this.value=undefined}render(){return o("limel-color-picker",{value:this.value,tooltipLabel:"Click to pick a color",helperText:"You can also type a color name or value to preview it here",label:"Chosen color",onChange:this.onChange})}};export{r as limel_example_color_picker_basic};
-//# sourceMappingURL=limel-example-color-picker-basic.entry.js.map
