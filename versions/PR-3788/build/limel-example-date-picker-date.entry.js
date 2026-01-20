4c65996c91a66df81f85e539c2c7780c82e89148
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-01-20 19:46:25 +0000

    chore(docs): create docs PR-3788

---
+++ versions/PR-3788/build/limel-example-date-picker-date.entry.js
@@ -0,0 +1,2 @@
+import{r as e,h as t}from"./index-0c4503aa.js";const a=class{constructor(t){e(this,t);this.handleChange=e=>{this.value=e.detail};this.value=new Date}render(){return t("p",null,t("limel-date-picker",{type:"date",label:"date",value:this.value,onChange:this.handleChange}),t("limel-example-value",{value:this.value}))}};export{a as limel_example_date_picker_date};
+//# sourceMappingURL=limel-example-date-picker-date.entry.js.map
