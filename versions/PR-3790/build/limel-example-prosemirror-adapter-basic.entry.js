0ca1a69875052f27415671ae1d43ab88cf9ce21e
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-01-21 11:27:34 +0000

    chore(docs): remove PR-3790

--- versions/PR-3790/build/limel-example-prosemirror-adapter-basic.entry.js
+++
@@ -1,2 +0,0 @@
-import{r,h as e}from"./index-0c4503aa.js";const s=class{constructor(e){r(this,e);this.handleChange=r=>{r.stopPropagation();this.text=r.detail};this.text=""}render(){return[e("limel-prosemirror-adapter",{onChange:this.handleChange}),e("limel-example-value",{value:this.text})]}};export{s as limel_example_prosemirror_adapter_basic};
-//# sourceMappingURL=limel-example-prosemirror-adapter-basic.entry.js.map
