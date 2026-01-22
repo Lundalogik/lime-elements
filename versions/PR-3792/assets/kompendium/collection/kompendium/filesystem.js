45755db1536bca1b6c0f01aa847982bf4953ad4c
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-01-22 08:34:08 +0000

    chore(docs): remove PR-3792

--- versions/PR-3792/assets/kompendium/collection/kompendium/filesystem.js
+++
@@ -1,9 +0,0 @@
-import { access, constants } from "fs";
-export function exists(path) {
-    return new Promise((resolve) => {
-        access(path, constants.F_OK, (error) => {
-            resolve(!error);
-        });
-    });
-}
-//# sourceMappingURL=filesystem.js.map
