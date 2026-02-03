9ff88ba4d5bb77c4f20a18e175bf2fe02e4a19e0
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-02-03 12:53:33 +0000

    chore(docs): create docs PR-3817

--- versions/PR-3817/build/dispatch-resize-event-96cdbaf8.js
+++
@@ -1,2 +0,0 @@
-const e=()=>{const e=new UIEvent("resize",{view:window,detail:0});window.dispatchEvent(e)};const n=()=>{e()};export{e as d,n as r};
-//# sourceMappingURL=dispatch-resize-event-96cdbaf8.js.map
