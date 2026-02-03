2299857de89c6b4b4eb0109b38ec582d55404970
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-02-03 17:28:47 +0000

    chore(docs): create docs PR-3823

---
+++ versions/PR-3823/build/limel-example-header.entry.js
@@ -0,0 +1,2 @@
+import{r as e,h as a}from"./index-0c4503aa.js";const r=class{constructor(a){e(this,a)}render(){return a("limel-header",{icon:"brake_warning",heading:"Useful information",subheading:"Note",supportingText:"Data couldn't be loaded!"})}};export{r as limel_example_header};
+//# sourceMappingURL=limel-example-header.entry.js.map
