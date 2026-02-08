b71434a744630484574d9c8f925f59b129c3ffdd
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-02-08 15:19:07 +0000

    chore(docs): create docs PR-3829

---
+++ versions/PR-3829/build/limel-example-callout-note.entry.js
@@ -0,0 +1,2 @@
+import{r as t,h as e}from"./index-0c4503aa.js";const o=class{constructor(e){t(this,e)}render(){return e("limel-callout",{type:"note"},"You might read this, you might not.")}};export{o as limel_example_callout_note};
+//# sourceMappingURL=limel-example-callout-note.entry.js.map
