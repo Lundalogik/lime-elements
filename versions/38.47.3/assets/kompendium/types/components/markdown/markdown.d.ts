1235bfc1d8e92deed8399f8a5da6e7b15a164f81
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-02-09 17:46:28 +0000

    chore(docs): create docs 38.47.3

---
+++ versions/38.47.3/assets/kompendium/types/components/markdown/markdown.d.ts
@@ -0,0 +1,15 @@
+/**
+ * This component renders markdown
+ * @exampleComponent kompendium-example-markdown
+ */
+export declare class Markdown {
+    /**
+     * The text to render
+     */
+    text: string;
+    private host;
+    protected componentDidLoad(): void;
+    protected componentDidUpdate(): void;
+    private renderMarkdown;
+    render(): HTMLElement;
+}
