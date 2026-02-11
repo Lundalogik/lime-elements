0f01e87ee006955270f0531a0eec2fb8619362d0
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-02-11 07:40:25 +0000

    chore(docs): create docs PR-3778

---
+++ versions/PR-3778/build/allowed-css-properties-K3v8WSkP.js
@@ -0,0 +1,1 @@
+const t=["background-color","color","font-style","font-weight","text-decoration-color","text-decoration-line","text-decoration-skip-ink","text-decoration-style","text-decoration-thickness","text-decoration","width","height","min-width","min-height"];export{t as a}
