677f59bf0d910084fff4bf6dcae057259d92ce1b
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-02-16 14:39:04 +0000

    chore(docs): remove PR-3849

--- versions/PR-3849/build/allowed-css-properties-K3v8WSkP.js
+++
@@ -1,1 +0,0 @@
-const t=["background-color","color","font-style","font-weight","text-decoration-color","text-decoration-line","text-decoration-skip-ink","text-decoration-style","text-decoration-thickness","text-decoration","width","height","min-width","min-height"];export{t as a}
