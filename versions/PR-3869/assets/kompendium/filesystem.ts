822db4fc63191cd19331bb64ce7f56ecf49dbbda
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-02-18 11:37:31 +0000

    chore(docs): remove PR-3869

--- versions/PR-3869/assets/kompendium/filesystem.ts
+++
@@ -1,9 +0,0 @@
-import { access, constants } from 'fs';
-
-export function exists(path: string): Promise<boolean> {
-    return new Promise((resolve) => {
-        access(path, constants.F_OK, (error) => {
-            resolve(!error);
-        });
-    });
-}
