1e7c496eb5bd0bad9766dbbdd779f2706fdd8f71
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-03-16 10:10:04 +0000

    chore(docs): create docs 39.7.1

---
+++ versions/39.7.1/assets/kompendium/types/components/navigation/navigation.d.ts
@@ -0,0 +1,35 @@
+import { MenuItem } from '../../types';
+/**
+ * @private
+ */
+export declare class Navigation {
+    /**
+     * The menu to display
+     */
+    menu: MenuItem[];
+    /**
+     * Title to display at the top of the navigation
+     */
+    header: string;
+    /**
+     * Optional logo to display instead of the header
+     */
+    logo: string;
+    /**
+     * Index containing searchable documents
+     */
+    index: any;
+    private route;
+    private displayNavPanel;
+    constructor();
+    protected connectedCallback(): void;
+    protected disconnectedCallback(): void;
+    private setRoute;
+    render(): HTMLElement[];
+    private renderHeader;
+    private toggleMenu;
+    private renderChapters;
+    private renderMenuItem;
+    private isRouteActive;
+    private stopPropagationOfNavClick;
+}
