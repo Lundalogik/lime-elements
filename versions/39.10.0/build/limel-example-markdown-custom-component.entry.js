d8dd6455fccfebcf165b64f4244331eb38d257e5
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-03-31 08:23:47 +0000

    chore(docs): remove old patch version 39.10.0

--- versions/39.10.0/build/limel-example-markdown-custom-component.entry.js
+++
@@ -1,1 +0,0 @@
-import{r as e,h as t}from"./index-DBTJNfo7.js";const r=class{constructor(t){e(this,t)}render(){return t("limel-markdown",{key:"2fe416b257abfc89e0e340788190871bf4f402a9",value:'This is 20% <meter value="0.2"></meter> and this is 100% <meter value="1" />',whitelist:[{tagName:"meter",attributes:["value"]}]})}};export{r as limel_example_markdown_custom_component}
