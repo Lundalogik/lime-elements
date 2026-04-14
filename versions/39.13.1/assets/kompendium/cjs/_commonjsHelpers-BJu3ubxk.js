34762723c7bc2fae9ba5b23790ed0361d2599cfb
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-04-14 13:55:07 +0000

    chore(docs): create docs 39.13.1

---
+++ versions/39.13.1/assets/kompendium/cjs/_commonjsHelpers-BJu3ubxk.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var commonjsGlobal = typeof globalThis !== 'undefined' ? globalThis : typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : {};
+
+function getDefaultExportFromCjs (x) {
+	return x && x.__esModule && Object.prototype.hasOwnProperty.call(x, 'default') ? x['default'] : x;
+}
+
+exports.commonjsGlobal = commonjsGlobal;
+exports.getDefaultExportFromCjs = getDefaultExportFromCjs;
+//# sourceMappingURL=_commonjsHelpers-BJu3ubxk.js.map
+
+//# sourceMappingURL=_commonjsHelpers-BJu3ubxk.js.map
