95659b989bc857f2911f986449f43f27ce342ec8
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-04-22 14:31:06 +0000

    chore(docs): remove PR-4041

--- versions/PR-4041/assets/kompendium/types/components/component/templates/style.d.ts
+++
@@ -1,5 +0,0 @@
-import { JsonDocsStyle } from '../../../stencil-public-runtime';
-export declare function StyleList({ styles, id, }: {
-    id: string;
-    styles: JsonDocsStyle[];
-}): HTMLElement[];
