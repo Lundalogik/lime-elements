798cea56a444a8f7c54aeb75f16dbed78f117c3c
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-04-23 13:03:15 +0000

    chore(docs): remove old patch version 39.16.2

--- versions/39.16.2/build/limel-example-header-basic.entry.js
+++
@@ -1,1 +0,0 @@
-import{r as e,h as a}from"./index-DBTJNfo7.js";const r=class{constructor(a){e(this,a)}render(){return a("limel-header",{key:"502505e8ea925e3262abc6a58b803eafc49f7782",icon:"brake_warning",heading:"Useful information",subheading:"Note",supportingText:"Data couldn't be loaded!"})}};export{r as limel_example_header_basic}
