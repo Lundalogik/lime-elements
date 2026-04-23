f5c8c971027a717fc288c4a247782dcd71dd0102
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-04-23 13:03:14 +0000

    chore(docs): create docs 39.16.3

---
+++ versions/39.16.3/assets/kompendium/collection/kompendium/markdown-nodes.js
@@ -0,0 +1,10 @@
+export function isParent(node) {
+    return 'children' in node;
+}
+export function isTextNode(node) {
+    return node.type === 'text';
+}
+export function isElement(node) {
+    return node.type === 'element' && 'tagName' in node;
+}
+//# sourceMappingURL=markdown-nodes.js.map
