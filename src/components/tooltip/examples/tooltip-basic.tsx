ff1cddfd6ae450dd1c57d1b41a12fb3f4e2126f5
Kiarokh Moattar <kiarokh@icloud.com>
2026-04-27 10:47:02 +0200

    docs(tooltip): imporve documentations

--- src/components/tooltip/examples/tooltip-basic.tsx
+++ src/components/tooltip/examples/tooltip-basic.tsx
@@ -2,6 +2,18 @@ import { Component, h } from '@stencil/core';
 
 /**
  * Basic example
+ * In order to display the tooltip, the tooltip element and its trigger element
+ * must be within the same document or document fragment (the same `shadowRoot`).
+ * Often, it's easiest to just place them next to each other like in the example
+ * below, but if you need to, you can place them differently.
+ *
+ * Since `limel-tooltip` is absolutely positioned, it will not occupy any
+ * space in the layout.
+ *
+ * ```html
+ * <limel-button icon="search" id="tooltip-example" />
+ * <limel-tooltip label="Search" elementId="tooltip-example" />
+ * ```
  */
 @Component({
     tag: 'limel-example-tooltip-basic',
