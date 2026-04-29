1ba97c2908b9d3c0108646f245820dc0c17a607d
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-04-29 09:04:11 +0000

    chore(docs): remove PR-4054

--- versions/PR-4054/build/limel-example-icon-button-elevated.entry.js
+++
@@ -1,1 +0,0 @@
-import{r as e,h as t}from"./index-DBTJNfo7.js";const o=class{constructor(t){e(this,t)}render(){return t("limel-icon-button",{key:"c2d349c0ff73412f37fae22530ae1a954c460490",label:"Add favourite",icon:"heart_outlined",elevated:!0})}};export{o as limel_example_icon_button_elevated}
--- versions/PR-4054/build/limel-example-linear-progress-indeterminate.entry.js
+++
@@ -1,1 +0,0 @@
-import{r as e,h as r}from"./index-DBTJNfo7.js";const s=class{constructor(r){e(this,r)}render(){return r("limel-linear-progress",{key:"0fe9114c16b70603e8479ed5c344dd7f39e61778",indeterminate:!0})}};export{s as limel_example_linear_progress_indeterminate}
