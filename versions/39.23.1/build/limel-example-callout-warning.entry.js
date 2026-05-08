4593d8d586866c4c129ed6f063c49b8fb8928016
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-05-08 13:36:40 +0000

    chore(docs): create docs 39.23.1

---
+++ versions/39.23.1/build/limel-example-callout-warning.entry.js
@@ -0,0 +1,1 @@
+import{r as e,h as r}from"./index-DBTJNfo7.js";const t=class{constructor(r){e(this,r)}render(){return r("limel-callout",{key:"59dc85b7698f6435fac681b7e79c4cb2307c6eb9",type:"warning"},"You need to read this.")}};export{t as limel_example_callout_warning}
