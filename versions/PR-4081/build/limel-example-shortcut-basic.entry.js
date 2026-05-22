c935e8e1c0ca78093add57a3297eff59c60703c6
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-05-22 08:24:46 +0000

    chore(docs): create docs PR-4081

---
+++ versions/PR-4081/build/limel-example-shortcut-basic.entry.js
@@ -0,0 +1,1 @@
+import{r as i,h as e}from"./index-DBTJNfo7.js";const t=class{constructor(e){i(this,e)}render(){return e("limel-shortcut",{key:"919cf3810d269157cdd7314f2327496ebbcb9c49",icon:"wikipedia",label:"Wikipedia",link:{href:"https://www.wikipedia.org/",title:"Open Wikipedia in a new tab.",target:"_blank"}})}};t.style="limel-shortcut{width:20%}";export{t as limel_example_shortcut_basic}
