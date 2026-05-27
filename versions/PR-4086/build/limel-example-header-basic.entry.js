046982e0b3d504a1d6f04c1893c8800119544817
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-05-27 07:57:40 +0000

    chore(docs): create docs PR-4086

---
+++ versions/PR-4086/build/limel-example-header-basic.entry.js
@@ -0,0 +1,1 @@
+import{r as e,h as a}from"./index-DBTJNfo7.js";const r=class{constructor(a){e(this,a)}render(){return a("limel-header",{key:"dba93c64469e3a89437449182cb35f84f09fe3a3",icon:"brake_warning",heading:"Useful information",subheading:"Note",supportingText:"Data couldn't be loaded!"})}};export{r as limel_example_header_basic}
