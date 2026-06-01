fe3047abc360a97f009531f78b8406f9a74f592b
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-06-01 18:03:25 +0000

    chore(docs): remove PR-4085

--- versions/PR-4085/build/limel-example-icon-button-elevated.entry.js
+++
@@ -1,1 +0,0 @@
-import{r as e,h as t}from"./index-DBTJNfo7.js";const o=class{constructor(t){e(this,t)}render(){return t("limel-icon-button",{key:"9a9de3485c7c202b2bdb506e54ad111820e88089",label:"Add favourite",icon:"heart_outlined",elevated:!0})}};export{o as limel_example_icon_button_elevated}
