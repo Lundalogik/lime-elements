8fc8a6c4816ac50e48808e71d71c253b40d78902
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-06-03 11:08:22 +0000

    chore(docs): create docs PR-4104

---
+++ versions/PR-4104/build/limel-example-markdown-custom-component.entry.js
@@ -0,0 +1,1 @@
+import{r as e,h as t}from"./index-DBTJNfo7.js";const r=class{constructor(t){e(this,t)}render(){return t("limel-markdown",{key:"53bd5edf7a63bae32fc050f12668e933431946dd",value:'This is 20% <meter value="0.2"></meter> and this is 100% <meter value="1" />',whitelist:[{tagName:"meter",attributes:["value"]}]})}};export{r as limel_example_markdown_custom_component}
