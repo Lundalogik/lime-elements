8464f775cbc2308faa181aa29103c6787b1345d7
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-06-09 13:30:29 +0000

    chore(docs): remove PR-4122

--- versions/PR-4122/build/limel-example-shortcut-basic.entry.js
+++
@@ -1,1 +0,0 @@
-import{r as i,h as e}from"./index-DBTJNfo7.js";const t=class{constructor(e){i(this,e)}render(){return e("limel-shortcut",{key:"bf10fdb8880234afe1e8775f8cf69aa460617f41",icon:"wikipedia",label:"Wikipedia",link:{href:"https://www.wikipedia.org/",title:"Open Wikipedia in a new tab.",target:"_blank"}})}};t.style="limel-shortcut{width:20%}";export{t as limel_example_shortcut_basic}
