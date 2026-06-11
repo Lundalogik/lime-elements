1788ae8cd2cd0ec27c7e1ce8cebdb52b583aa4de
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-06-11 13:24:49 +0000

    chore(docs): create docs 39.33.2

---
+++ versions/39.33.2/build/limel-example-icon-button-disabled.entry.js
@@ -0,0 +1,1 @@
+import{r as e,h as o}from"./index-DBTJNfo7.js";const i=class{constructor(o){e(this,o)}render(){return[o("limel-icon-button",{key:"748800cfc5b640d449e5a9d19315f61694d2950e",label:"Add favourite",icon:"heart_outlined",disabled:!0,onClick:this.onClick})]}onClick(){console.log("This should never happen, since the button is disabled.")}};export{i as limel_example_icon_button_disabled}
