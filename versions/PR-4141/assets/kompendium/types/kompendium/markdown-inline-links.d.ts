7dab9bbae4555bdd28907a2cac03e1ae1d0eb7ca
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-06-18 12:56:47 +0000

    chore(docs): remove PR-4141

--- versions/PR-4141/assets/kompendium/types/kompendium/markdown-inline-links.d.ts
+++
@@ -1,6 +0,0 @@
-import type { Node } from 'unist';
-export type LinkResolver = (target: string) => string | null;
-export declare function normalizeInlineLinkUrls(text: string): string;
-export declare function inlineLinks(options?: {
-    resolve?: LinkResolver;
-}): (tree: Node) => void;
