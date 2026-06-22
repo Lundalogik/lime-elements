fb119193f705673f84ff1c7ee955dcb6d0055831
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-06-22 12:51:21 +0000

    chore(docs): create docs PR-4056

--- versions/PR-4056/build/image.template-zwhzWBQq.js
+++
@@ -1,1 +0,0 @@
-import{h as r}from"./index-BGxJfR2f.js";const e=({image:e})=>{var i;return r("img",Object.assign({src:e.src,alt:e.alt,loading:null!==(i=e.loading)&&void 0!==i?i:"lazy"},e.referrerpolicy?{referrerpolicy:e.referrerpolicy}:{}))};export{e as I}
