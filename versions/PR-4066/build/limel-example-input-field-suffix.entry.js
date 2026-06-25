f6293c695d8e635da7f8a3879a7192b984c47d75
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-06-25 07:35:26 +0000

    chore(docs): remove PR-4066

--- versions/PR-4066/build/limel-example-input-field-suffix.entry.js
+++
@@ -1,1 +0,0 @@
-import{r as e,h as i}from"./index-DBTJNfo7.js";const t=class{constructor(i){e(this,i),this.value="50",this.handleChange=e=>{this.value=e.detail}}render(){return i("limel-input-field",{key:"699598f3077b32907006571fb80f02b4005698b5",label:"Quantity",suffix:"pcs",value:this.value,type:"number",onChange:this.handleChange})}};export{t as limel_example_input_field_suffix}
