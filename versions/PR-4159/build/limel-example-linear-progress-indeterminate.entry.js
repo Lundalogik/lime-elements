a0fc4adae5104b7d955b2d1f553b809c3b712285
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-07-02 20:57:48 +0000

    chore(docs): create docs PR-4159

---
+++ versions/PR-4159/build/limel-example-linear-progress-indeterminate.entry.js
@@ -0,0 +1,1 @@
+import{r as e,h as r}from"./index-BGxJfR2f.js";const s=class{constructor(r){e(this,r)}render(){return r("limel-linear-progress",{key:"f6b18fe1374fa3bae532539f18c9485314db9930",indeterminate:!0})}};export{s as limel_example_linear_progress_indeterminate}
