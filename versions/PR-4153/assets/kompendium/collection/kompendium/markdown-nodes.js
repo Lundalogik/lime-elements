737ef5671f2ca1948df9a798519ed7bac5bc1336
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-07-02 10:06:04 +0000

    chore(docs): remove PR-4153

--- versions/PR-4153/assets/kompendium/collection/kompendium/markdown-nodes.js
+++
@@ -1,9 +0,0 @@
-export function isParent(node) {
-    return 'children' in node;
-}
-export function isTextNode(node) {
-    return node.type === 'text';
-}
-export function isElement(node) {
-    return node.type === 'element' && 'tagName' in node;
-}
--- versions/PR-4153/build/limel-example-input-field-suffix.entry.js
+++
@@ -1,1 +0,0 @@
-import{r as e,h as i}from"./index-BGxJfR2f.js";const t=class{constructor(i){e(this,i),this.value="50",this.handleChange=e=>{this.value=e.detail}}render(){return i("limel-input-field",{key:"6df363e1f78a558505be02d71d9cb24b3368bbb0",label:"Quantity",suffix:"pcs",value:this.value,type:"number",onChange:this.handleChange})}};export{t as limel_example_input_field_suffix}
