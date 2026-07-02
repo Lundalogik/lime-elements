9fab418f6b71524e052dba926e2529c897193ec0
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-07-02 08:11:51 +0000

    chore(docs): remove PR-4151

--- versions/PR-4151/assets/kompendium/types/components/component/templates/events.d.ts
+++
@@ -1,6 +0,0 @@
-import { JsonDocsEvent } from '../../../stencil-public-runtime';
-export declare function EventList({ events, id, slugId, }: {
-    id?: string;
-    slugId?: string;
-    events: JsonDocsEvent[];
-}): HTMLElement[];
