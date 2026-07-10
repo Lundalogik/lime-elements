06dd306c3afb4f80dd18a00a3f1a4ae8a8c91af2
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-07-10 12:17:24 +0000

    chore(docs): remove PR-4181

--- versions/PR-4181/build/limel-example-open-direction.entry.js
+++
@@ -1,1 +0,0 @@
-import{r as e,h as r}from"./index-BGxJfR2f.js";const i=class{constructor(r){e(this,r)}render(){return r("limel-help",{key:"1c35d2106bd79604954e1c85cdc35a13a136459e",openDirection:"right",value:"This popover is opened on the right side of the trigger."})}};export{i as limel_example_open_direction}
