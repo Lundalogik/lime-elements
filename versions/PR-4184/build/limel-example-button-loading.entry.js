28a5645c53ae8e3acca6df8ca7dcfb916014412e
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-07-14 08:37:30 +0000

    chore(docs): remove PR-4184

--- versions/PR-4184/build/limel-example-button-loading.entry.js
+++
@@ -1,1 +0,0 @@
-import{r as e,h as t}from"./index-BGxJfR2f.js";const l=class{constructor(t){e(this,t)}render(){return t("limel-button",{key:"2de9f45315b57f487e02a61858d9f23c39e99512",label:"My Button",disabled:!0,loading:!0})}};export{l as limel_example_button_loading}
