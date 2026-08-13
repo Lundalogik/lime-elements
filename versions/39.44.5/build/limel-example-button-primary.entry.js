a7460286821b7ff43dc5d3babbd18a39c2a71adf
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-08-13 09:34:22 +0000

    chore(docs): remove old patch version 39.44.5

--- versions/39.44.5/build/limel-example-button-primary.entry.js
+++
@@ -1,1 +0,0 @@
-import{r,h as a}from"./index-BGxJfR2f.js";const t=class{constructor(a){r(this,a)}render(){return a("limel-button",{key:"6c0748aad0de8ca990600c8816adf207dd10daa5",label:"My Button",primary:!0})}};export{t as limel_example_button_primary}
