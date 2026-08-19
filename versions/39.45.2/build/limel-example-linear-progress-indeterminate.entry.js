d2b031f219aea0e0c841309b69fb7f49ee6e5ef1
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-08-19 09:09:35 +0000

    chore(docs): remove old patch version 39.45.2

--- versions/39.45.2/build/limel-example-linear-progress-indeterminate.entry.js
+++
@@ -1,1 +0,0 @@
-import{r as e,h as r}from"./index-BGxJfR2f.js";const s=class{constructor(r){e(this,r)}render(){return r("limel-linear-progress",{key:"5b38805ffa12d849e4335840efabca669ec58e92",indeterminate:!0})}};export{s as limel_example_linear_progress_indeterminate}
