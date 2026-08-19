3ee0d2aa561b5f935a88bdd0e3d1afeaf2653644
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-08-19 09:09:34 +0000

    chore(docs): create docs 39.45.3

---
+++ versions/39.45.3/build/limel-example-icon-name.entry.js
@@ -0,0 +1,1 @@
+import{r as e,h as a}from"./index-BGxJfR2f.js";const r=class{constructor(a){e(this,a)}render(){return a("limel-icon",{key:"adb8825c41e66a66c3050b56beab60864d810534",name:"happy"})}};export{r as limel_example_icon_name}
---
+++ versions/39.45.3/build/limel-example-input-field-suffix.entry.js
@@ -0,0 +1,1 @@
+import{r as e,h as i}from"./index-BGxJfR2f.js";const t=class{constructor(i){e(this,i),this.value="50",this.handleChange=e=>{this.value=e.detail}}render(){return i("limel-input-field",{key:"cd743d34c2002916e36de48d113e2c9cbf58f703",label:"Quantity",suffix:"pcs",value:this.value,type:"number",onChange:this.handleChange})}};export{t as limel_example_input_field_suffix}
