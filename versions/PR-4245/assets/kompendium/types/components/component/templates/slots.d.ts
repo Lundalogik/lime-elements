9ec497e2ce1034ca88fbe3a29f7424cb12df6b1c
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-08-19 09:29:39 +0000

    chore(docs): create docs PR-4245

---
+++ versions/PR-4245/assets/kompendium/types/components/component/templates/slots.d.ts
@@ -0,0 +1,6 @@
+import { JsonDocsSlot } from '../../../stencil-public-runtime';
+export declare function SlotList({ slots, id, slugId, }: {
+    id?: string;
+    slugId?: string;
+    slots: JsonDocsSlot[];
+}): HTMLElement[];
