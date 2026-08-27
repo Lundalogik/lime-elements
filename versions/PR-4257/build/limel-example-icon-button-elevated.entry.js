368356ca974273c7ae29d0a170c7e508c636299f
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-08-27 14:20:18 +0000

    chore(docs): create docs PR-4257

---
+++ versions/PR-4257/build/limel-example-icon-button-elevated.entry.js
@@ -0,0 +1,1 @@
+import{r as e,h as t}from"./index-BGxJfR2f.js";const a=class{constructor(t){e(this,t)}render(){return t("limel-icon-button",{key:"410aaa5eca3ffc39689120f4ac9ff8f3718d127b",label:"Add favourite",icon:"heart_outlined",elevated:!0})}};export{a as limel_example_icon_button_elevated}
