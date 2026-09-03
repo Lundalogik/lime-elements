f72e0d9465b096b12c8a9edc84d21923b709906f
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-09-03 14:04:45 +0000

    chore(docs): create docs PR-4262

--- versions/PR-4262/build/limel-example-chart-max-value.entry.js
+++ versions/PR-4262/build/limel-example-chart-max-value.entry.js
@@ -1,1 +1,1 @@
-import{r as a,h as e}from"./index-BGxJfR2f.js";import{c as r}from"./chart-items-stack-BdUh9la4.js";const t=class{constructor(e){a(this,e)}render(){return e("limel-chart",{key:"68eaa94f2216c49579681cdc1951383a2d18f5a0",items:r,maxValue:512})}};export{t as limel_example_chart_max_value}
+import{r as e,h as a}from"./index-BGxJfR2f.js";import{c as r}from"./chart-items-stack-BdUh9la4.js";const t=class{constructor(a){e(this,a)}render(){return a("limel-chart",{key:"78aab444275e81e77f27dfe1c365f7511b396b14",items:r,maxValue:512})}};export{t as limel_example_chart_max_value}
