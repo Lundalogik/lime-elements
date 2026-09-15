e817f2f6bcd5f652e01ba23cb5d82937e06481d8
Lime Technologies OSS <93315277+lime-opensource@users.noreply.github.com>
2026-09-15 15:53:02 +0000

    chore(docs): remove old patch version 40.2.4

--- versions/40.2.4/assets/kompendium/cjs/types-SjA7Kcy_.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-const THEME_EVENT_NAME = 'kompendiumTheme';
-
-exports.THEME_EVENT_NAME = THEME_EVENT_NAME;
--- versions/40.2.4/build/limel-example-callout-tip.entry.js
+++
@@ -1,1 +0,0 @@
-import{r as e,h as t}from"./index-BGxJfR2f.js";const r=class{constructor(t){e(this,t)}render(){return t("limel-callout",{key:"d84ef422830ae62bcdbe1d18bbfe3c976047ccd9",type:"tip"},"You want to read this.")}};export{r as limel_example_callout_tip}
